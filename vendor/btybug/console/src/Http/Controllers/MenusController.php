2709cd2ad8d7e2fd99486df5fa7022be3504f1bb
sahakavatar <hakobyan.sahak88@gmail.com>
2017-11-21 15:58:37 +0400

    update

--- vendor/btybug/console/src/Http/Controllers/MenusController.php
+++ vendor/btybug/console/src/Http/Controllers/MenusController.php
@@ -61,19 +61,27 @@ class MenusController extends Controller
     }
 
     public function getView(
-        $id, $slug,
+        $id, $slug,$flag=null,
         MenuRepository $menuRepository,
         AdminPagesRepository $adminPagesRepository,
         RoleRepository $roleRepository,
         StructureService $structureService
     )
     {
-        $children = ($id != 1);
-        $menu = $menuRepository->findOrFail($id);
-        $page = $adminPagesRepository->first();
-        $pageGrouped = $adminPagesRepository->getGroupedWithModule();
-        $role = $roleRepository->findBy('slug', $slug);
-        $data = $structureService->getMenuItems($menu, $role);
+        if($flag=='hook'){
+            $core_menus = \Config::get('admin_menus',[]);
+            if(!isset($core_menus[$id]))abort(404);
+            $menu=$core_menus[$id];
+            return view('console::structure.menus.hook_menus',compact('menu'));
+        }else{
+            $children = ($id != 1);
+            $menu = $menuRepository->findOrFail($id);
+            $page = $adminPagesRepository->first();
+            $pageGrouped = $adminPagesRepository->getGroupedWithModule();
+            $role = $roleRepository->findBy('slug', $slug);
+            $data = $structureService->getMenuItems($menu, $role);
+        }
+
         return view('console::structure.menus.view', compact(['pageGrouped', 'page', 'slug', 'children', 'data', 'menu']));
     }
 
---
+++ vendor/btybug/console/src/Resources/Views/structure/menus/hook_menus.blade.php
@@ -0,0 +1,83 @@
+@extends('btybug::layouts.admin')
+@section('content')
+
+    <div class="bb-menu-container">
+        <div class="container-fluid">
+            <!-- Settings -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading menu-panel">
+                            <h3 class="panel-title pull-left">
+                                <label>{!! $menu['title'] !!}</label>
+                            </h3>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- Menu -->
+            <div class="row">
+                <div class="col-md-10">
+                    <ol class="bb-sortable-static">
+                        <li data-id="1" id="menu-item-1">
+                            <div class="bb-menu-item">
+                                <div class="bb-menu-item-title">
+                                    <i></i>
+                                    <span>{!! $menu['title'] !!}</span>
+
+                                    <div class="bb-menu-actions pull-right">
+                                        <a href="javascript:" class="bb-menu-delete">
+                                            <i class="fa fa-close"></i>
+                                        </a>
+                                        <a href="javascript:" class="bb-menu-collapse">
+                                            <i class="fa fa-caret-down"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        @foreach($menu['children'] as $child)
+                        <li data-id="2" id="menu-item-2" class="level-1">
+                            <div class="bb-menu-item">
+                                <div class="bb-menu-item-title">
+                                    <i></i>
+                                    <span>{!! $child['title'] !!}</span>
+
+                                    <div class="bb-menu-actions pull-right">
+                                        <a href="javascript:" class="bb-menu-delete">
+                                            <i class="fa fa-close"></i>
+                                        </a>
+                                        <a href="javascript:" class="bb-menu-collapse">
+                                            <i class="fa fa-caret-down"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        @endforeach
+                    </ol>
+                    {{--<ol class="bb-sortable-static bb-sortable-group">--}}
+                       {{--{!! dd($menu) !!}--}}
+                    {{--</ol>--}}
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
+{{--@include('tools::common_inc')--}}
+@section('CSS')
+    {{--{!! HTML::style('public/css/bootstrap/css/bootstrap-switch.min.css') !!}--}}
+    {!! HTML::style('public/css/font-awesome/css/fontawesome-iconpicker.min.css') !!}
+    {!! HTML::style('public/css/menus.css') !!}
+
+@stop
+
+@section('JS')
+    {{--{!! HTML::script('public/js/jquery.mjs.nestedSortable.js') !!}--}}
+    {{--{!! HTML::script('public/css/bootstrap/js/bootstrap-switch.min.js') !!}--}}
+    {!! HTML::script('public/css/font-awesome/js/fontawesome-iconpicker.min.js') !!}
+    {{--{!! HTML::script('public/js/menus.js') !!}--}}
+@stop

b72800ae429eb925e2bbcd452d8eafd4dd1cd34e
Eduard <edo.terakyan@gmail.com>
2017-11-29 17:17:11 +0400

    classify service

--- vendor/btybug/frontsite/src/Services/ClassifierService.php
+++ vendor/btybug/frontsite/src/Services/ClassifierService.php
@@ -19,7 +19,7 @@ class ClassifierService extends GeneralService
     private $classifierItemRepository;
     private $classifierItemPageRepository;
 
-    public function __construct(
+    public function __construct (
         ClassifierRepository $classifierRepository,
         ClassifierItemRepository $classifierItemRepository,
         ClassifierItemPageRepository $classifierItemPageRepository
@@ -34,18 +34,20 @@ class ClassifierService extends GeneralService
     public function save (array $data)
     {
         $response = $this->classifierRepository->create($data + ['id' => uniqid()]);
+
         return ($response) ? $response->id : null;
     }
 
     public function delete ($id)
     {
         $deleted = false;
-        $response = $this->classifierRepository->findBy('id',$id);
-        if($response) $deleted = $response->delete();
+        $response = $this->classifierRepository->findBy('id', $id);
+        if ($response) $deleted = $response->delete();
+
         return $deleted;
     }
 
-    public function getClassifierPageRelations(int $pageId)
+    public function getClassifierPageRelations (int $pageId)
     {
         return $this->classifierItemPageRepository
             ->model()
@@ -54,7 +56,7 @@ class ClassifierService extends GeneralService
             ->get();
     }
 
-    public function classifierItems()
+    public function classifierItems ()
     {
         return $this->classifierRepository
             ->model()
@@ -63,47 +65,54 @@ class ClassifierService extends GeneralService
             ->toArray();
     }
 
-    public function generateItems ($id,$data)
+    public function generateItems ($id, $data)
     {
-        $model = $this->classifierRepository->findBy('id',$id);
-        if($model && $data){
+        $model = $this->classifierRepository->findBy('id', $id);
+        if ($model && $data) {
             $this->cleanItems($model->id);
-            $this->addItems(json_decode($data,true), 0, $model->id);
+            $this->addItems(json_decode($data, true), 0, $model->id);
         }
     }
 
-    private function addItems($data,$parent,$classifier_id){
-        if(count($data)){
-            foreach ($data as $item){
-                $new = $this->classifierItemRepository->create([
-                    'id' => uniqid(),
-                    'title' => $item['title'],
-                    'icon' => $item['icon'],
-                    'parent_id' => $parent,
-                    'classifier_id' => $classifier_id,
-                    'informative' => issetReturn($item, 'informative',0),
-                    'listing' => issetReturn($item, 'listing',0),
-                    'tagged' => issetReturn($item, 'tagged',0),
-                ]);
+    private function cleanItems ($classifier_id)
+    {
+        $this->classifierItemRepository->deleteByCondition('classifier_id', $classifier_id);
+    }
 
-                if(isset($item['children']) && $new){
-                    $this->addItems($item['children'], $new->id, $classifier_id);
-                }
+    private function addItems ($data, $parent, $classifier_id, $i = 0)
+    {
+        if (count($data)) {
+            $item = $data[$i];
+            $new = $this->classifierItemRepository->create([
+                'id'            => uniqid(),
+                'title'         => $item['title'],
+                'icon'          => $item['icon'],
+                'parent_id'     => $parent,
+                'classifier_id' => $classifier_id,
+                'informative'   => issetReturn($item, 'informative', 0),
+                'listing'       => issetReturn($item, 'listing', 0),
+                'tagged'        => issetReturn($item, 'tagged', 0),
+            ]);
+
+            if (isset($item['children']) && $new) {
+                $this->addItems($item['children'], $new->id, $classifier_id, 0);
+            }
+
+            $i++;
+            if (isset($data[$i])) {
+                $this->addItems($data, $parent, $classifier_id, $i);
             }
         }
     }
 
-    private function cleanItems($classifier_id){
-        $this->classifierItemRepository->deleteByCondition('classifier_id', $classifier_id);
-    }
-
-    public function loadItems ($id,bool $json = false,array $columns = ['*']) {
+    public function loadItems ($id, bool $json = false, array $columns = ['*'])
+    {
         $data = $this->classifierItemRepository->model()->select($columns)->
         with('children')
             ->where('parent_id', 0)
             ->where('classifier_id', $id)
             ->get();
 
-        return ($json) ? json_encode($data,true) : $data;
+        return ($json) ? json_encode($data, true) : $data;
     }
 }

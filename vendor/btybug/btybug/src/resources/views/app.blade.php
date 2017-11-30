b411dfbb7fa9aa0f7cd284928df5932cf55585b5
sahakavatar <hakobyan.sahak88@gmail.com>
2017-11-30 17:01:42 +0400

    up

--- vendor/btybug/btybug/src/resources/views/app.blade.php
+++ vendor/btybug/btybug/src/resources/views/app.blade.php
@@ -1,18 +1,11 @@
-{!! HTML::style('custom/css/'.$page->slug.'.css') !!}
-<body>
+
 @php
     if(!isset($page)){
         $page = \Btybug\btybug\Services\RenderService::getFrontPageByURL();
     }
-$settings=json_decode($page->settings,true);
-if(isset($settings['file_path']) && \File::exists(module_path($settings['file_path']))){
-include module_path($settings['file_path']);
-}
+            $settings=json_decode($page->settings,true);
+            if(isset($settings['file_path']) && View::exists($settings['file_path'])){
+            $path=$settings['file_path'];
+          }
 @endphp
-
-
-{{--{!! BBRenderUnits($page->template) !!}--}}
-
-
-
-<body>
+@include($path)

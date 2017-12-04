1948e5f11e9d9848bca2cc7445ff43a1e18fb9ef
sahakavatar <hakobyan.sahak88@gmail.com>
2017-12-04 17:25:54 +0400

    update

--- resources/views/ContentLayouts/backend_two_cols/tpl.blade.php
+++ resources/views/ContentLayouts/backend_two_cols/tpl.blade.php
@@ -4,5 +4,19 @@
     @endif
 </div>
 <div class="col-md-9">
+
+    @if(Session::has('message'))
+        <div style="position: absolute; width: 60%;left: 27%; z-index: 999999999;" class="m-t-10 alert {{ Session::get('alert-class', 'alert-info') }} alert-dismissible"
+             role="alert">
+            <button type="button" class="close" data-dismiss="alert"
+                    aria-label="Close"><span aria-hidden="true">&times;</span>
+            </button>
+            {!! Session::get('message') !!}
+            @php
+                Session::forget('message');
+                Session::forget('alert-class');
+            @endphp
+        </div>
+    @endif
     @yield('content')
 </div>

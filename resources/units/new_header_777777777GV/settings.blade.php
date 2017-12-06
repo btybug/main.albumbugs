da59c469ca5d649108af5d33228f3127902b5026
sahakavatar <hakobyan.sahak88@gmail.com>
2017-12-06 03:26:05 -0800

    GV header

---
+++ resources/units/new_header_777777777GV/settings.blade.php
@@ -0,0 +1,20 @@
+<div id="settings_div111">
+
+    <div class="form-group my_rows1">
+        <label for="newcontainer" class="col-sm-4 labelTitle">User Area</label>
+        <div class="col-sm-8">
+            {!! BBbutton2('unit','user_area','user-unit','Select Unit',['class'=>'form-control input-md','model'=>$settings]) !!}
+        </div>
+    </div>
+    <div class="form-group my_rows1">
+        <label for="newcontainer" class="col-sm-4 labelTitle">Menu</label>
+        <div class="col-sm-8">
+            {!! BBbutton2('menus','menu_area','frontend','Select Menu',['class'=>'form-control input-md','model'=>$settings]) !!}
+        </div>
+    </div>
+</div>
+
+
+
+
+
---
+++ vendor/btybug/user/src/Models/FormSettings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Comp1
+ * Date: 12/5/2016
+ * Time: 4:39 PM
+ */
+
+namespace Btybug\User\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class FormSettings extends Model
+{
+    protected $table = 'form_settings';
+    protected $guarded = ['id'];
+
+}

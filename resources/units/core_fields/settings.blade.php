cd25b9989d06a8d1ed8b3e9db91a84066ec301c7
terakyan <edo.terakyan@gmail.com>
2017-12-07 10:12:59 -0800

    unit

--- resources/units/core_fields/settings.blade.php
+++ resources/units/core_fields/settings.blade.php
@@ -1,32 +1,20 @@
-{{--<div id="settings_div111">--}}
-    {{--<div class="form-group my_rows1">--}}
-        {{--<label for="newcontainer" class="col-sm-4 labelTitle">Area 1</label>--}}
-        {{--<div class="col-sm-8">--}}
-            {{--{!! BBbutton('units','area1','Select Unit',['class'=>'form-control input-md','data-sub' => 'general','data-type'=>'backend','model'=>$settings]) !!}--}}
-        {{--</div>--}}
-    {{--</div>--}}
-
-    {{--<div class="form-group my_rows1">--}}
-        {{--<label for="newcontainer" class="col-sm-4 labelTitle">Area 2</label>--}}
-        {{--<div class="col-sm-8">--}}
-            {{--{!! BBbutton2('unit',"area2","footer_units","Change",['class'=>'btn btn-default change-layout','data-type'=>'frontend_sidebar','model'=>$settings]) !!}--}}
-        {{--</div>--}}
-    {{--</div>--}}
-
-    {{--<div class="form-group my_rows1">--}}
-        {{--<label for="newcontainer" class="col-sm-4 labelTitle">Area 3</label>--}}
-        {{--<div class="col-sm-8">--}}
-            {{--{!! BBbutton('units','area3','Select Unit',['class'=>'form-control input-md','data-sub' => 'general','data-type'=>'backend','model'=>$settings]) !!}--}}
-        {{--</div>--}}
-    {{--</div>--}}
-
-    {{--<div class="form-group my_rows1">--}}
-        {{--<label for="newcontainer" class="col-sm-4 labelTitle">Area 3</label>--}}
-        {{--<div class="col-sm-8">--}}
-            {{--{!! BBbutton('units','area3','Select Unit',['class'=>'form-control input-md','data-sub' => 'general','data-type'=>'backend','model'=>$settings]) !!}--}}
-        {{--</div>--}}
-    {{--</div>--}}
-{{--</div>--}}
+<div id="settings_div111">
+    <div class="form-group my_rows1">
+        <label for="newcontainer" class="col-sm-4 labelTitle">Select Input Type</label>
+        <div class="col-sm-8">
+            {!! Form::select('input_type',
+                [
+                    '' => 'Select',
+                    'text' => 'Text input',
+                    'textarea' => 'Textarea',
+                    'select' => 'Select',
+                    'checkbox' => 'Checkbox',
+                    'radio' => 'Radio'
+                ],
+                 null,['class' => 'form-control']) !!}
+        </div>
+    </div>
+</div>
 
 
 
--- resources/units/core_fields/tpl.blade.php
+++ resources/units/core_fields/tpl.blade.php
@@ -1,7 +1,32 @@
-<input class="bty-input-label-7" type="text" placeholder="Your name?">
-<label>Name</label>
-<label><i class="fa fa-question" aria-hidden="true"></i></label>
-<p>Lorem ipsum Lorem</p>
+@if(isset($settings['input_type']))
+    @if($settings['input_type'] == 'text')
+        <input class="bty-input-label-7" type="text" placeholder="Your name?">
+        <label>Name</label>
+        <label><i class="fa fa-question" aria-hidden="true"></i></label>
+        <p>Lorem ipsum Lorem</p>
+    @elseif($settings['input_type'] == 'textarea')
+        <textarea placeholder="This is an placeholder box" class="bty-textarea-1"></textarea>
+    @elseif($settings['input_type'] == 'select')
+        <div class="bty-input-select-3">
+            <select>
+                <option>Choose Option</option>
+                <option>Option 1</option>
+                <option>Option 2</option>
+                <option>Option 3</option>
+                <option>Option 4</option>
+            </select>
+        </div>
+    @elseif($settings['input_type'] == 'radio')
+        <input type="radio" class="bty-input-radio-5" id="bty-radio-5">
+        <label for="bty-radio-5"><i class="fa fa-check" aria-hidden="true"></i></label>
+    @elseif($settings['input_type'] == 'checkbox')
+        <input type="checkbox" class="bty-input-checkbox-2" id="bty-checkbox-2">
+        <label for="bty-checkbox-2">checkbox</label>
+    @else
+        No Html
+    @endif
+@endif
+
 
 
 {!! BBstyle($_this->path.DS.'css'.DS.'styles.css') !!}

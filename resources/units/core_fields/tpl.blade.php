26a69ddde42a57baf1aeb637d257877a107eefde
terakyan <edo.terakyan@gmail.com>
2017-12-07 09:25:38 -0800

    field unit

---
+++ resources/units/core_fields/tpl.blade.php
@@ -0,0 +1,8 @@
+<input class="bty-input-label-7" type="text" placeholder="Your name?">
+<label>Name</label>
+<label><i class="fa fa-question" aria-hidden="true"></i></label>
+<p>Lorem ipsum Lorem</p>
+
+
+{!! BBstyle($_this->path.DS.'css'.DS.'styles.css') !!}
+{!! BBscript($_this->path.DS.'js'.DS.'script.js') !!}

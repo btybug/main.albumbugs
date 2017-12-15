a84555a70799666ce3fc1337fdecf18230a339d4
sahakavatar <hakobyan.sahak88@gmail.com>
2017-12-15 20:04:59 +0400

    up

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,7 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Headers', 'X-Custom-Header')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,X-Custom-Header')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }

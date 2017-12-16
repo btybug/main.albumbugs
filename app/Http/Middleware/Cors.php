d7e021bc28bec8ea50957d3d1af71fba7484049c
sahakavatar <hakobyan.sahak88@gmail.com>
2017-12-16 21:48:00 +0400

    update

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -14,12 +14,9 @@ class Cors
 
     public function handle($request, Closure $next)
     {
-        if ($request->getMethod() === "OPTIONS") {
-            return response('');
-        }
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,X-Custom-Header')
-            ->header('Access-Control-Allow-Methods', 'POST');
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }

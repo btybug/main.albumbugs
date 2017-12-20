d2c9d39627a7bd90f82a92bf6f2048334a140ed5
terakyan <edo.terakyan@gmail.com>
2017-12-20 07:28:37 -0800

    include

--- vendor/btybug/console/src/Resources/Views/structure/developers/forms/form_layout.blade.php
+++ vendor/btybug/console/src/Resources/Views/structure/developers/forms/form_layout.blade.php
@@ -1,4 +1,4 @@
 {!! Form::open(['class' => 'bty-form-5']) !!}
     {!! Form::hidden('form_id',$form->id) !!}
-    {{--@include('forms.'.$form->slug,['form' => $form])--}}
+    @include('forms.'.$form->slug,['form' => $form])
 {!! Form::close() !!}

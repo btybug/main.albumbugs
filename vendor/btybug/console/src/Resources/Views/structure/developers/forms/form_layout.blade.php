7745d1e2245167e85f0a16c6fdc4d1764a71d4e2
terakyan <edo.terakyan@gmail.com>
2017-12-20 09:46:01 -0800

    form layout

--- vendor/btybug/console/src/Resources/Views/structure/developers/forms/form_layout.blade.php
+++ vendor/btybug/console/src/Resources/Views/structure/developers/forms/form_layout.blade.php
@@ -1,4 +1,28 @@
-{!! Form::open(['class' => 'bty-form-5']) !!}
-    {!! Form::hidden('form_id',$form->id) !!}
+{!! Form::open(['class' => 'bty-form-5', 'id' => 'form_id_'.$form->id]) !!}
+{!! Form::hidden('form_id',$form->id) !!}
     @include('forms.'.$form->slug,['form' => $form])
 {!! Form::close() !!}
+
+@if(isset($form->settings['is_ajax']) && $form->settings['is_ajax'] == 'yes')
+    <script>
+        $(document).ready(function () {
+            $("body").on('click', '#form_id_{!! $form->id !!} input[type="submit"], #form_id_{!! $form->id !!} button[type="submit"]', function (e) {
+                e.preventDefault();
+                var data = $("#form_id_{!! $form->id !!}").serialize();
+                $.ajax({
+                    type: "POST",
+                    datatype: "JSON",
+                    url: '',
+                    data: data,
+                    headers: {
+                        'X-CSRF-TOKEN': $("input[name='_token']").val()
+                    },
+                    success: function (data) {
+                        document.getElementById("form_id_{!! $form->id !!}").reset();
+                        alert(data.message)
+                    }
+                });
+            });
+        })
+    </script>
+@endif

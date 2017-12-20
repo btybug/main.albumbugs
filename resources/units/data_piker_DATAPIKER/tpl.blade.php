f0224e8006b7398a5a5eaeb22e2251b39b460e48
sahakavatar <hakobyan.sahak88@gmail.com>
2017-12-20 22:29:45 +0400

    up

--- resources/units/data_piker_DATAPIKER/tpl.blade.php
+++ resources/units/data_piker_DATAPIKER/tpl.blade.php
@@ -1,1 +1,10 @@
-<input type="color" id="html5colorpicker" onchange="clickColor(0, -1, -1, 5)" value="#ff0000" style="width:85%;">
+<input data-toggle="datepicker" class="form-control input-sm">
+{!! BBstyle($_this->path.DS.'css'.DS.'themes'.DS.'datepicker.css') !!}
+{!! BBscript($_this->path.DS.'js'.DS.'datepicker.js') !!}
+{!! BBscript($_this->path.DS.'js'.DS.'picker.date.js') !!}
+<script>
+$(function () {
+    $('[data-toggle="datepicker"]').datepicker();
+})
+
+</script>

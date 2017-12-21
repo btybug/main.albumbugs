6313088abfae8abbf9fdd3d05c9c0141c0c63e39
Sahak <hakobyan.sahak88@gmail.com>
2017-12-21 10:19:46 +0400

    field data name

--- resources/units/data_piker_DATAPIKER/tpl.blade.php
+++ resources/units/data_piker_DATAPIKER/tpl.blade.php
@@ -1,4 +1,4 @@
-<input data-toggle="datepicker" class="form-control input-sm">
+<input data-toggle="datepicker" class="form-control input-sm" name="{!! isset($settings['field_id'])?get_field_data($settings['field_id'],'name'):''!!}">
 {!! BBstyle($_this->path.DS.'css'.DS.'themes'.DS.'datepicker.css') !!}
 {!! BBscript($_this->path.DS.'js'.DS.'datepicker.js') !!}
 {!! BBscript($_this->path.DS.'js'.DS.'picker.date.js') !!}

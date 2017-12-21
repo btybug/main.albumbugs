99bd54bd0b91d1db5d090b1be31687c77a74402e
Sahak <hakobyan.sahak88@gmail.com>
2017-12-21 10:21:59 +0400

    color piker name

--- resources/units/color_piker_3T4IJ9809j/tpl.blade.php
+++ resources/units/color_piker_3T4IJ9809j/tpl.blade.php
@@ -1,1 +1,1 @@
-<input type="color" id="html5colorpicker" onchange="clickColor(0, -1, -1, 5)" value="#ff0000" style="width:85%;">
+<input type="color" id="html5colorpicker" onchange="clickColor(0, -1, -1, 5)" value="#ff0000" style="width:85%;" name="{!! isset($settings['field_id'])?get_field_data($settings['field_id'],'name'):''!!}">

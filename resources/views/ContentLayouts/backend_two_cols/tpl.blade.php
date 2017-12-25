5c994150307a693763a3d6c82b92bafd853c9b62
gev2092 <gev2092harutyunyan@gmail.com>
2017-12-25 03:44:47 -0800

    bug fix

--- resources/views/ContentLayouts/backend_two_cols/tpl.blade.php
+++ resources/views/ContentLayouts/backend_two_cols/tpl.blade.php
@@ -1,4 +1,5 @@
 <div class="col-md-3 left-column"  style="background: black;">
+    {{dd($settings)}}
     @if(isset($settings['placeholders']['left_bar']))
         {!! BBRenderUnits($settings['placeholders']['left_bar']) !!}
     @endif

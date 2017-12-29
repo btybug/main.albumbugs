859a13b7dbf5c9b9c0e63e342965acef8bf31e7c
sahakavatar <hakobyan.sahak88@gmail.com>
2017-12-29 09:22:58 +0400

    up

--- vendor/btybug/uploads/src/Resources/Views/Datatables/index.blade.php
+++ vendor/btybug/uploads/src/Resources/Views/Datatables/index.blade.php
@@ -62,39 +62,6 @@
             </div>
 
         </div>
-        <div class="checkbox">
-            <label><input type="checkbox" value="autoFill">AutoFill</label>
-        </div>
-        <div class="checkbox">
-            <label><input type="checkbox" value="">Buttons</label>
-        </div>
-        <div class="checkbox disabled">
-            <label><input type="checkbox" value="ColReorder-1.4.1">ColReorder</label>
-        </div>
-        <div class="checkbox disabled">
-            <label><input type="checkbox" value="FixedColumns-3.2.4">FixedColumns</label>
-        </div>
-        <div class="checkbox disabled">
-            <label><input type="checkbox" value="FixedHeader-3.1.3">FixedHeader</label>
-        </div>
-        <div class="checkbox disabled">
-            <label><input type="checkbox" value="KeyTable-2.3.2">KeyTable</label>
-        </div>
-        <div class="checkbox disabled">
-            <label><input type="checkbox" value="Responsive-2.2.1">Responsive</label>
-        </div>
-        <div class="checkbox disabled">
-            <label><input type="checkbox" value="RowGroup-1.0.2">RowGroup</label>
-        </div>
-        <div class="checkbox disabled">
-            <label><input type="checkbox" value="RowReorder">RowReorder-1.2.3</label>
-        </div>
-        <div class="checkbox disabled">
-            <label><input type="checkbox" value="Scroller-1.4.3">Scroller</label>
-        </div>
-        <div class="checkbox disabled">
-            <label><input type="checkbox" value="Select-1.2.4">Select</label>
-        </div>
     </div>
     <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
         <table id="users-table" class="table table-striped table-bordered" cellspacing="0" width="500px">

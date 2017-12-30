bbee3d042c15f53df29bdb19be607b0379b4da42
midoalone <mido_alone2001@yahoo.com>
2017-12-30 04:00:39 +0200

    BB Button + Form Service check field value

--- vendor/btybug/btybug/src/resources/views/bbbutton.blade.php
+++ vendor/btybug/btybug/src/resources/views/bbbutton.blade.php
@@ -1,17 +1,17 @@
 @php
-        $indentificator=uniqid();
-            if($value){
-                switch ($type){
-                    case 'layouts':
-                    $obj=Btybug\btybug\Models\ContentLayouts\ContentLayouts::findByVariation($value);
-                    $variation=Btybug\btybug\Models\ContentLayouts\ContentLayouts::findVariation($value);
-                    break;
-                    case 'unit':
-                    $obj=\Btybug\btybug\Models\Painter\Painter::findByVariation($value);
-                    $variation = $obj->variations()->find($value);
-                    break;
-                }
+    $indentificator=uniqid();
+        if($value){
+            switch ($type){
+                case 'layouts':
+                $obj=Btybug\btybug\Models\ContentLayouts\ContentLayouts::findByVariation($value);
+                $variation=Btybug\btybug\Models\ContentLayouts\ContentLayouts::findVariation($value);
+                break;
+                case 'unit':
+                $obj=\Btybug\btybug\Models\Painter\Painter::findByVariation($value);
+                $variation = $obj->variations()->find($value);
+                break;
             }
+        }
 
 @endphp
 <div class="input-group">
@@ -33,8 +33,9 @@
                    Uploaded:{!! BBgetDateFormat($obj->created_at) !!}
                    Variation:{!! $variation->title !!}
            @if(isset($variation->updated_at))
-                   Last Modification:{!!BBgetDateFormat($variation->updated_at) !!}"
+                   Last Modification:{!!BBgetDateFormat($variation->updated_at) !!}
            @endif
+                   "
            @else
            value="Nothing Selected!!!"
             @endif
@@ -48,46 +49,5 @@
             type="hidden" {!! $array !!}
             value="{!! $value !!}"
             data-name="{!! $indentificator !!}"
-            name="{!! $hiddenName !!}"
-    >
+            name="{!! $hiddenName !!}"/>
 </div>
-
-{{--<div class="col-md-12">--}}
-    {{--<div class="col-md-4">{!! $text !!}</div>--}}
-    {{--<div class="col-md-5">--}}
-        {{--<input type="text"--}}
-               {{--data-key="title"--}}
-               {{--data-toggle="popover"--}}
-               {{--readonly="readonly"--}}
-               {{--data-id="{!! $indentificator !!}"--}}
-               {{--class="page-layout-title form-control"--}}
-               {{--title="info"--}}
-               {{--style="width: 100%;"--}}
-               {{--@if(isset($obj) && isset($variation)&& is_object($obj) && is_object($variation))--}}
-               {{--value="{!! $obj->title !!}"--}}
-               {{--data-content="--}}
-                   {{--Type:{!! $type !!}--}}
-                       {{--Name:{!! $obj->title !!}--}}
-                       {{--Author:{!! $obj->author !!}--}}
-                       {{--Uploaded:{!! BBgetDateFormat($obj->created_at) !!}--}}
-                       {{--Variation:{!! $variation->title !!}--}}
-               {{--@if(isset($variation->updated_at))--}}
-                       {{--Last Modification:{!!BBgetDateFormat($variation->updated_at) !!}"--}}
-               {{--@endif--}}
-               {{--@else--}}
-               {{--value="Nothing Selected!!!"--}}
-                {{--@endif--}}
-        {{-->--}}
-    {{--</div>--}}
-    {{--<div class="col-md-3">--}}
-    {{--<button type="button" data-action={!! $type !!}  data-key="{!! $indentificator !!}" {!! $atributes !!} >Change--}}
-    {{--</button>--}}
-    {{--</div>--}}
-    {{--<input--}}
-            {{--class="bb-button-realted-hidden-input"--}}
-            {{--type="hidden" {!! $array !!}--}}
-            {{--value="{!! $value !!}"--}}
-            {{--data-name="{!! $indentificator !!}"--}}
-            {{--name="{!! $hiddenName !!}"--}}
-    {{-->--}}
-{{--</div>--}}

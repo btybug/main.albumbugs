13f6e49390184fc7300bfaec4e1beb9934fc344a
midoalone <mido_alone2001@yahoo.com>
2018-01-01 23:28:01 +0200

    unit builder

--- vendor/btybug/uploads/src/Resources/Views/gears/units/_partials/unit_preview.blade.php
+++ vendor/btybug/uploads/src/Resources/Views/gears/units/_partials/unit_preview.blade.php
@@ -4,7 +4,7 @@
     {!! BBCss()  !!}
     {!! HTML::style("public/css/font-awesome/css/font-awesome.min.css") !!}
     {!! HTML::style("public/js/jquery-ui/jquery-ui.min.css") !!}
-    {!! HTML::style("public/css/preview-template.css") !!}
+    {!! HTML::style("public/css/preview-template.css?v=".rand('1111','9999')) !!}
     {!! HTML::style('public/css/cms.css') !!}
     {!! HTML::style('public-x/custom/css/'.str_replace(' ','-',$ui->slug).'.css') !!}
 </head>

dc3e409a10b2f7ed77ddde0bdd4cfa3b444e9f12
sahakavatar <hakobyan.sahak88@gmail.com>
2018-01-08 22:25:04 +0400

    controller pages

--- vendor/btybug/btybug/src/resources/views/app.blade.php
+++ vendor/btybug/btybug/src/resources/views/app.blade.php
@@ -3,8 +3,14 @@
         $page = \Btybug\btybug\Services\RenderService::getFrontPageByURL();
     }
             $settings=json_decode($page->settings,true);
-            if(isset($settings['file_path']) && View::exists($settings['file_path'])){
+            if(isset($settings['file_path'])){
             $path=$settings['file_path'];
           }
 @endphp
-@include($path)
+@if(View::exists($path))
+    @include($path)
+@else
+    @php
+      echo  \App::call($path);
+    @endphp
+@endif

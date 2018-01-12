987438041c9aee3c79a3373e1344468f808efe26
gev2092 <gev2092harutyunyan@gmail.com>
2018-01-12 02:01:21 -0800

    left menu

--- resources/units/left_bar_my_account/tpl.blade.php
+++ resources/units/left_bar_my_account/tpl.blade.php
@@ -1,17 +1,33 @@
+<?php
+
+use Btybug\FrontSite\Models\FrontendPage;
+
+$front_pages = FrontendPage::where('content_type','template')->with('childs')->get();
+
+function renderMenu($data){
+    if(!count($data)){
+        return '<li><a href="#">No menu items</a></li>';
+    }
+    $html = '';
+    foreach ($data as $key => $settings){
+        $children = $settings->childs;
+        if(count($children)){
+            $sub_menu = '';
+            foreach ($children as $ch){
+                $sub_menu .= '<li><a href="'.$ch->url.'">'.$ch->title.'</a></li>';
+            }
+            $html .= '<li><a href="'.$settings->url.'">'.$settings->title.'</a><ul>'.$sub_menu.'</ul></li>';
+        }else{
+            $html .= '<li><a href="'.$settings->url.'">'.$settings->title.'</a></li>';
+        }
+    }
+    return $html;
+}
+
+?>
 <div class="bty-vertical-menu-2">
     <ul>
-        <li><a href="#">Menu 1</a>
-        </li>
-        <li><a href="#">Menu 2</a>
-        </li>
-        <li><a href="#">Menu 3</a>
-            <ul>
-                <li><a href="#">Menu 3.1</a></li>
-                <li><a href="#">Menu 3.2</a></li>
-                <li><a href="#">Menu 3.3</a></li>
-                <li><a href="#">Menu 3.4</a></li>
-            </ul>
-        </li>
+        {!! renderMenu($front_pages) !!}
     </ul>
 </div>
 {!! BBstyle($_this->path.DS.'css'.DS.'styles.css') !!}

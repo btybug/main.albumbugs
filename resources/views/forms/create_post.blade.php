157c8e53283fc95362a6ee919df85b4c7d024600
terakyan <edo.terakyan@gmail.com>
2018-01-16 06:40:29 -0800

    form

--- resources/views/forms/create_post.blade.php
+++ resources/views/forms/create_post.blade.php
@@ -1,22 +1,43 @@
 {{--Form 13 --}}
-<h2 class="form-title">Create Post</h2>
-<div class="field-box">
-<fieldset class="bty-form-textarea" id="bty-input-id-10">
-    {!! Form::textarea(get_field_attr(10,'table_name')."_".get_field_attr(10,'column_name'),null,['class' => 'bty-textarea-1','placeholder' => get_field_attr(10,'placeholder')]) !!}
-</fieldset>
-<fieldset class="bty-form-text" id="bty-input-id-11">
-    <div>
-        {!! Form::text(get_field_attr(11,'table_name')."_".get_field_attr(11,'column_name'),null,['class' => 'bty-input-label-5','placeholder' => get_field_attr(11,'placeholder')]) !!}
-        <label>{!! get_field_attr(11,'label') !!}</label>
-    </div>
-</fieldset>
+<h2 class="form-title">Create Post
+</h2>
 
 
-<fieldset class="bty-form-select" id="bty-input-id-12">
-    <div class="bty-input-select-1">
-        {!! Form::select(get_field_attr(12,'table_name')."_".get_field_attr(12,'column_name'),get_field_data(12),null,['placeholder' => get_field_attr(12,'placeholder'),'class' => 'form-control input-md']) !!}
+<div class="col-md-12">
+    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+        <li class="nav-item active">
+            <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-general" role="tab"
+               aria-controls="pills-general" aria-selected="true">General</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="pills-price-tab" data-toggle="pill" href="#pills-price" role="tab"
+               aria-controls="pills-price" aria-selected="false">Price</a>
+        </li>
+    </ul>
+    <div class="tab-content" id="pills-tabContent">
+        <div class="tab-pane active" id="pills-general" role="tabpanel" aria-labelledby="pills-general-tab">
+            <div class="field-box">
+                <fieldset class="bty-form-textarea" id="bty-input-id-10">
+                    {!! Form::textarea(get_field_attr(10,'table_name')."_".get_field_attr(10,'column_name'),null,['class' => 'bty-textarea-1','placeholder' => get_field_attr(10,'placeholder')]) !!}
+                </fieldset>
+                <fieldset class="bty-form-text" id="bty-input-id-11">
+                    <div>
+                        {!! Form::text(get_field_attr(11,'table_name')."_".get_field_attr(11,'column_name'),null,['class' => 'bty-input-label-5','placeholder' => get_field_attr(11,'placeholder')]) !!}
+                        <label>{!! get_field_attr(11,'label') !!}</label>
+                    </div>
+                </fieldset>
+
+
+                <fieldset class="bty-form-select" id="bty-input-id-12">
+                    <div class="bty-input-select-1">
+                        {!! Form::select(get_field_attr(12,'table_name')."_".get_field_attr(12,'column_name'),get_field_data(12),null,['placeholder' => get_field_attr(12,'placeholder'),'class' => 'form-control input-md']) !!}
+                    </div>
+                </fieldset>
+            </div>
+        </div>
+        <div class="tab-pane fade" id="pills-price" role="tabpanel" aria-labelledby="pills-price-tab">
+            Prices form
+        </div>
     </div>
-</fieldset>
 </div>
 <button type="submit" class="bty-btn bty-btn-save"><span>Save</span></button>
-
--- vendor/btybug/console/src/Resources/Views/structure/developers/forms/form_layout.blade.php
+++ vendor/btybug/console/src/Resources/Views/structure/developers/forms/form_layout.blade.php
@@ -1,4 +1,4 @@
-{!! Form::open(['class' => 'bty-form-5', 'id' => 'form_id_'.$form->id]) !!}
+{!! Form::open(['class' => 'bty-form-5','style' => 'width:auto;', 'id' => 'form_id_'.$form->id]) !!}
 {!! Form::hidden('form_id',$form->id) !!}
     @include('forms.'.$form->slug,['form' => $form])
 {!! Form::close() !!}

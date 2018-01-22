c97bdf72570fcd6a68fcb308a3cb02693a901cc5
sahakavatar <hakobyan.sahak88@gmail.com>
2018-01-22 20:34:29 +0400

    up

--- resources/views/ContentLayouts/front_layout_with_2_col/main.blade.php
+++ resources/views/ContentLayouts/front_layout_with_2_col/main.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-3">
     @if(has_setting($settings,"left_bar",false))
-        {!! BBRenderUnits($settings['left_bar']) !!}
+        {!! BBRenderUnits($settings['left_bar'],isset($settings['_page'])?['_page'=>$settings['_page']]:[]) !!}
     @endif
 </div>
 <div class="col-md-9">

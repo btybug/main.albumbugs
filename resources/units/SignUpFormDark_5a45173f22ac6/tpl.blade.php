03dff07b2418fa4caeff7216dbc58b67b214dfd9
midoalone <mido_alone2001@yahoo.com>
2018-01-24 05:53:14 +0200

    form builder

--- resources/units/SignUpFormDark_5a45173f22ac6/tpl.blade.php
+++ resources/units/SignUpFormDark_5a45173f22ac6/tpl.blade.php
@@ -1,35 +1,37 @@
 {!! BBstyle($_this->path.DS.'css/main.css') !!}
 
-<div class="login-form">
+<div class="login-form" bb-main-wrapper>
 
-    <div class="header">
-        <h1>Create Post</h1>
-        <span>Fill out the form below to create new post</span>
-    </div>
-
-    <div class="content">
-        <div class="row">
-            <div class="col-md-8">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="bb-form-area connectedSortable"></div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="bb-form-area connectedSortable"></div>
-                    </div>
-                    <div class="col-md-12" style="margin-top: 20px;">
-                        <div class="bb-form-area connectedSortable"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="unit-area"></div>
-            </div>
+    <div class="row header">
+        <div class="col-md-12">
+            <h1>Create Post</h1>
+            <span>Fill out the form below to create new post</span>
         </div>
     </div>
 
-    <div class="footer">
-        <input type="submit" name="submit" value="Save" class="button" />
+    <div class="row content">
+        <div class="col-md-8">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="bb-form-area connectedSortable"></div>
+                </div>
+                <div class="col-md-6">
+                    <div class="bb-form-area connectedSortable"></div>
+                </div>
+                <div class="col-md-12" style="margin-top: 20px;">
+                    <div class="bb-form-area connectedSortable"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="unit-area"></div>
+        </div>
+    </div>
+
+    <div class="row footer">
+        <div class="col-md-12">
+            <input type="submit" name="submit" value="Save" class="button" />
+        </div>
     </div>
 
 </div>

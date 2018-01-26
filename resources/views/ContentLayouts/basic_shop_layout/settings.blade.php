e72375200dc15db4b66d72a11ee165fa2ff62ed8
sahakavatar <hakobyan.sahak88@gmail.com>
2018-01-26 18:12:53 +0400

    basic shop ui

---
+++ resources/views/ContentLayouts/basic_shop_layout/settings.blade.php
@@ -0,0 +1,9 @@
+<div class="form-group">
+    <div class="row">
+        <label for="navstyle" class="col-xs-3">Left Areas Unit</label>
+        <div class="col-xs-9">
+            <div class="input-group">
+            </div>
+        </div>
+    </div>
+</div>

a5260d6611b4504dd925ad8ebefcef07972d275f
midoalone <mido_alone2001@yahoo.com>
2018-01-29 03:53:55 +0200

    form builder

--- resources/units/SignUpFormDark_5a45173f22ac6/tpl.blade.php
+++ resources/units/SignUpFormDark_5a45173f22ac6/tpl.blade.php
@@ -1,31 +1,33 @@
 {!! BBstyle($_this->path.DS.'css/main.css') !!}
 
-<div class="login-form" bb-main-wrapper>
+<div class="bb-main-wrapper">
 
-    <div class="row bb-section header">
-        <div class="col-md-12">
-            <h1>Create Post</h1>
-            <span>Fill out the form below to create new post</span>
-        </div>
-    </div>
+    <div class="container"></div>
 
-    <div class="row bb-section content">
-        <div class="col-md-8 parent-column">
-            <div class="row">
-                <div class="col-md-6 item-column"></div>
-                <div class="col-md-6 item-column"></div>
-                <div class="col-md-12 item-column" style="margin-top: 20px;"></div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            Contact area
-        </div>
-    </div>
+    {{--<div class="row bb-section header">--}}
+        {{--<div class="col-md-12">--}}
+            {{--<h1>Create Post</h1>--}}
+            {{--<span>Fill out the form below to create new post</span>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 
-    <div class="row bb-section footer">
-        <div class="col-md-12">
-            <input type="submit" name="submit" value="Save" class="button" />
-        </div>
-    </div>
+    {{--<div class="row bb-section content">--}}
+        {{--<div class="col-md-8 parent-column">--}}
+            {{--<div class="row">--}}
+                {{--<div class="col-md-6 item-column"></div>--}}
+                {{--<div class="col-md-6 item-column"></div>--}}
+                {{--<div class="col-md-12 item-column" style="margin-top: 20px;"></div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="col-md-4">--}}
+            {{--Contact area--}}
+        {{--</div>--}}
+    {{--</div>--}}
+
+    {{--<div class="row bb-section footer">--}}
+        {{--<div class="col-md-12">--}}
+            {{--<input type="submit" name="submit" value="Save" class="button" />--}}
+        {{--</div>--}}
+    {{--</div>--}}
 
 </div>

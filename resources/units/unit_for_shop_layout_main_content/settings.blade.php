1235b500f87b3c09ecde2e5fb4ddd801a123557c
sahakavatar <hakobyan.sahak88@gmail.com>
2018-01-30 15:26:37 +0400

    settings ui

--- resources/units/unit_for_shop_layout_main_content/settings.blade.php
+++ resources/units/unit_for_shop_layout_main_content/settings.blade.php
@@ -1,31 +1,70 @@
-<div class="form-group my_rows1">
-    <label for="newcontainer" class="col-sm-4 labelTitle">Left bar menu</label>
-    <div class="col-sm-8">
-        {!! BBbutton2('menus','left_bar_menu','frontend','Select Menu',['class'=>'form-control input-md','model'=>$settings]) !!}
+<div class="bty-panel-collapse">
+    <div>
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseleftbarmenu" aria-expanded="true">
+            <span class="icon"><i class="fa fa-chevron-down" aria-hidden="true"></i></span>
+            <span class="title">Left bar menu</span>
+        </a>
     </div>
-</div>
-
-<div class="form-group my_rows1">
-    <label for="newcontainer" class="col-sm-4 labelTitle">Nav bar menu</label>
-    <div class="col-sm-8">
-        {!! BBbutton2('menus','nav_bar_menu','frontend','Select Menu',['class'=>'form-control input-md','model'=>$settings]) !!}
-    </div>
-</div>
-
-<button type="button" class="btn btn-info" id="addinput">Add</button>
-<div class="add_custome_page">
-    @if(isset($settings["images"]) && is_array($settings["images"]))
-        @foreach($settings["images"] as $key => $setting)
-            <div style="border: 1px solid silver" class="form-group custom_div_key" data-key="{{$key}}">
-                <input type="text" name="images[{{$key}}][title]" placeholder="Title" class="form-control" value="{{$setting['title']}}">
-                <input type="text" name="images[{{$key}}][description]" placeholder="Description" class="form-control" value="{{$setting['description']}}">
-                <input type="text" name="images[{{$key}}][path]" placeholder="Path" class="form-control" value="{{$setting['path']}}">
-                <input type="button" value="Delete" class="btn btn-danger del" >
+    <div id="collapseleftbarmenu" class="collapse in" aria-expanded="true" style="">
+        <div class="content">
+            <div class="form-group my_rows1">
+                <div class="col-sm-8">
+                    {!! BBbutton2('menus','left_bar_menu','frontend','Select Menu',['class'=>'form-control input-md','model'=>$settings]) !!}
+                </div>
             </div>
-        @endforeach
-    @endif
+
+        </div>
+    </div>
 </div>
 
+<div class="bty-panel-collapse">
+    <div>
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapsenavbarmenu" aria-expanded="true">
+            <span class="icon"><i class="fa fa-chevron-down" aria-hidden="true"></i></span>
+            <span class="title">Nav bar menu</span>
+        </a>
+    </div>
+    <div id="collapsenavbarmenu" class="collapse in" aria-expanded="true" style="">
+        <div class="content">
+            <div class="form-group my_rows1">
+                <div class="col-sm-8">
+                    {!! BBbutton2('menus','nav_bar_menu','frontend','Select Menu',['class'=>'form-control input-md','model'=>$settings]) !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="bty-panel-collapse">
+    <div>
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseunitbanner" aria-expanded="true">
+            <span class="icon"><i class="fa fa-chevron-down" aria-hidden="true"></i></span>
+            <span class="title">Banner</span>
+        </a>
+    </div>
+    <div id="collapseunitbanner" class="collapse in" aria-expanded="true" style="">
+        <div class="content banner-settings">
+            <button type="button" class="btn btn-info btn-block" id="addinput">Add</button>
+            <div class="add_custome_page">
+                @if(isset($settings["images"]) && is_array($settings["images"]))
+                    @foreach($settings["images"] as $key => $setting)
+                        <div class="form-group custom_div_key" data-key="{{$key}}">
+                            <input type="text" name="images[{{$key}}][title]" placeholder="Title" class="form-control" value="{{$setting['title']}}">
+                            <input type="text" name="images[{{$key}}][description]" placeholder="Description" class="form-control" value="{{$setting['description']}}">
+                            <input type="text" name="images[{{$key}}][path]" placeholder="Path" class="form-control" value="{{$setting['path']}}">
+                            <input type="button" value="Delete" class="btn btn-danger btn-block del" >
+                        </div>
+                    @endforeach
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
+
+
+{!! BBstyle($_this->path.DS.'css'.DS.'settings.css') !!}
 
 {!! BBscript($_this->path.DS.'js'.DS.'settings.js') !!}
 

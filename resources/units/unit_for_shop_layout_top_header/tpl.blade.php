a3d153a984e86dafb4d7a986774ad1c12417d429
gev2092 <gev2092harutyunyan@gmail.com>
2018-01-30 03:05:12 -0800

    top header menu with childs

--- resources/units/unit_for_shop_layout_top_header/tpl.blade.php
+++ resources/units/unit_for_shop_layout_top_header/tpl.blade.php
@@ -6,7 +6,6 @@
             @endif
         </div>
         <div class="pull-right">
-
             @if(isset($settings['top_menu']))
                 <ul class="header-top-links">
                     @php
@@ -14,33 +13,22 @@
                     @endphp
                     @if(count($items))
                         @foreach($items as $item)
-                            <li><a href="{!! url($item->url) !!}"><i class="{!! $item->icon !!}"></i> {!! $item->title !!}</a></li>
+                            @if(isset($item->children))
+                                <li class="dropdown default-dropdown">
+                                    <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">{!! $item->title !!}<i class="fa fa-caret-down"></i></a>
+                                    <ul class="custom-menu">
+                                        @foreach($item->children as $child)
+                                            <li><a href="{!! url($child->url) !!}"><i class="{!! $child->icon !!}"></i> {!! $child->title !!}</a></li>
+                                        @endforeach
+                                    </ul>
+                                </li>
+                            @else
+                                <li><a href="{!! url($item->url) !!}"><i class="{!! $item->icon !!}"></i> {!! $item->title !!}</a></li>
+                            @endif
                         @endforeach
                     @endif
                 </ul>
             @endif
-
-            {{--<ul class="header-top-links">
-                <li><a href="#">Store</a></li>
-                <li><a href="#">Newsletter</a></li>
-                <li><a href="#">FAQ</a></li>
-                <li class="dropdown default-dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">ENG <i class="fa fa-caret-down"></i></a>
-                    <ul class="custom-menu">
-                        <li><a href="#">English (ENG)</a></li>
-                        <li><a href="#">Russian (Ru)</a></li>
-                        <li><a href="#">French (FR)</a></li>
-                        <li><a href="#">Spanish (Es)</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown default-dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">USD <i class="fa fa-caret-down"></i></a>
-                    <ul class="custom-menu">
-                        <li><a href="#">USD ($)</a></li>
-                        <li><a href="#">EUR (€)</a></li>
-                    </ul>
-                </li>
-            </ul>--}}
         </div>
     </div>
 </div>

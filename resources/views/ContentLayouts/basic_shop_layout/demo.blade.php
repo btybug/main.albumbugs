00d4cd5ad36287d5aa143e278f44d2a772be81d9
sahakavatar <hakobyan.sahak88@gmail.com>
2018-01-31 12:43:04 +0400

    front layout col ui

---
+++ resources/views/ContentLayouts/basic_shop_layout/demo.blade.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 onecol">
+
+        </div>
+    </div>
+</div>
+<style>
+    .onecol{
+        min-height: 200px;
+        border: 1px solid #000;
+    }
+    </style>

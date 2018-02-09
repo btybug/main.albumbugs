35f120872c2e6a70bfe5210e45825757910b696b
sahakavatar <hakobyan.sahak88@gmail.com>
2018-02-09 10:07:37 +0400

    footer bottom ui

--- resources/units/medicio_footer_5445f2d9858ae/tpl.blade.php
+++ resources/units/medicio_footer_5445f2d9858ae/tpl.blade.php
@@ -1,13 +1,14 @@
+<div class="clearfix"></div>
 <!--footer start from here-->
-<footer>
+<footer class="footer-medicio">
     <div class="container">
         <div class="row">
             <div class="col-md-4 col-sm-6 footerleft ">
                 <div class="logofooter"> Logo</div>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
-                <p><i class="fa fa-map-pin"></i> 210, Aggarwal Tower, Rohini sec 9, New Delhi -        110085, INDIA</p>
-                <p><i class="fa fa-phone"></i> Phone (India) : +91 9999 878 398</p>
-                <p><i class="fa fa-envelope"></i> E-mail : info@webenlance.com</p>
+                <p><i class="fa fa-map-pin"></i> 210, Lorem Ipsum, is simply dummy text of the -        110085, USA</p>
+                <p><i class="fa fa-phone"></i> Phone (USA) : +1 000-000-00</p>
+                <p><i class="fa fa-envelope"></i> E-mail : info@wwwww.com</p>
 
             </div>
             <div class="col-md-2 col-sm-6 paddingtop-bottom">
@@ -25,9 +26,9 @@
             <div class="col-md-3 col-sm-6 paddingtop-bottom">
                 <h6 class="heading7">LATEST POST</h6>
                 <div class="post">
-                    <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2015</span></p>
-                    <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2015</span></p>
-                    <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2015</span></p>
+                    <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2017</span></p>
+                    <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2017</span></p>
+                    <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2017</span></p>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6 paddingtop-bottom">
@@ -45,11 +46,11 @@
 <div class="copyright">
     <div class="container">
         <div class="col-md-6">
-            <p>© 2016 - All Rights with Webenlance</p>
+            <p>© 2018 - All Rights Reserved</p>
         </div>
         <div class="col-md-6">
             <ul class="bottom_ul">
-                <li><a href="#">webenlance.com</a></li>
+                <li><a href="#">wwwwwwww.com</a></li>
                 <li><a href="#">About us</a></li>
                 <li><a href="#">Blog</a></li>
                 <li><a href="#">Faq's</a></li>

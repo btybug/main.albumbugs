c54e06af21cad9318453ceca555fb9ecef769a6a
midoalone <mido_alone2001@yahoo.com>
2018-02-19 23:08:30 +0200

    form builder

--- vendor/btybug/uploads/src/Resources/Views/gears/units/_partials/html_preview.blade.php
+++ vendor/btybug/uploads/src/Resources/Views/gears/units/_partials/html_preview.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/medium-editor@latest/dist/css/medium-editor.min.css" type="text/css" media="screen" charset="utf-8">
     <link rel="stylesheet" href="https://yabwe.github.io/medium-editor/bower_components/medium-editor/dist/css/themes/beagle.min.css" type="text/css" media="screen" charset="utf-8">
 
+    {!! HTML::script("public/libs/medium_editor/medium-button.min.js") !!}
 </head>
 
 <body>
@@ -27,14 +28,36 @@
 @endif
 
 <script>
-    var mediumEditorInstance;
     function initEditor(){
-        mediumEditorInstance= new MediumEditor('body');
+        new MediumEditor('.editable', {
+            toolbar: {
+                buttons: ['bold', 'italic', 'underline', 'link', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'done'],
+                sticky: true,
+                static: true,
+                align: 'center'
+            },
+            extensions: {
+                'done':  new MediumButton({
+                    label:'Done',
+                    action: function (){
+                        destroyEditor();
+                        return '';
+                    }
+                })
+            }
+        });
+
+        $('.medium-editor-toolbar').css({
+            visibility: 'visible',
+            left: 'calc(50% - '+($('.medium-editor-toolbar').width()/2)+'px)'
+        });
     }
 
     function destroyEditor(){
-        mediumEditorInstance= new MediumEditor('body');
-        mediumEditorInstance.destroy();
+        var editor = window._mediumEditors.filter(function(ele, index){return (ele && ele.origElements === '.editable')})[0];
+        editor.destroy();
+        window.parent.disableActiveHoverNode(false);
+        $('.editable').removeClass("editable");
     }
 </script>
 </body>

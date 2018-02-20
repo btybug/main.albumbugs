62798ae8953c25e8b383068c193785fb6d5a83e9
sahakavatar <hakobyan.sahak88@gmail.com>
2018-02-20 14:39:24 +0400

    head

--- vendor/btybug/framework/src/Resources/Views/css/list.blade.php
+++ vendor/btybug/framework/src/Resources/Views/css/list.blade.php
@@ -1,8 +1,17 @@
 @extends('btybug::layouts.admin')
 @section('content')
     <div class="main_lay_cont">
-        <div class="row for_title_row">
-            <h1 class="text-center">Components</h1>
+        {{--<div class="row for_title_row">--}}
+            {{--<h1 class="text-center">Components</h1>--}}
+        {{--</div>--}}
+        <div class="col-md-3">
+
+        </div>
+        <div class="col-md-9 headar-btn">
+            <div></div>
+            <div>
+                <a href="#" class="btn btn-info"><i class="fa fa-plus"></i></a>
+            </div>
         </div>
         <div class="row layouts_row">
            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
@@ -20,6 +29,15 @@
     <style>
         .main_lay_cont{
             min-height: 500px;
+        }
+        .headar-btn {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            background: #78909C;
+            color: #fff;
+            padding: 10px 15px;
+            margin: 0 -15px;
         }
     </style>
 @stop

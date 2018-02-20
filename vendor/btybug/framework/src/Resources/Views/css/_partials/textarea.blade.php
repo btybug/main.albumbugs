834b62e5b25f9ff1ab78e8cc031839d740bbd9df
sahakavatar <hakobyan.sahak88@gmail.com>
2018-02-20 14:24:12 +0400

    textarea

--- vendor/btybug/framework/src/Resources/Views/css/_partials/textarea.blade.php
+++ vendor/btybug/framework/src/Resources/Views/css/_partials/textarea.blade.php
@@ -1,9 +1,41 @@
 <div class="d1">
     <h2>Textarea</h2>
     <div class="col-md-12">
-        <h4>Textarea 1</h4>
-        <h5>bty-textarea-1</h5>
-        <textarea placeholder="This is an placeholder box" class="bty-textarea-1"></textarea>
+        <div class="col-md-6 p-t-34">
+            <textarea placeholder="This is an placeholder box" class="bty-textarea-1"></textarea>
+        </div>
+        <div class="col-md-6">
+            <h5>bty-textarea-1</h5>
+            <textarea name="" id="" cols="35" rows="2" readonly="" style="margin-top: 0px; margin-bottom: 0px; height: 100px; width: 100%">
+              .bty-textarea-1 {
+    width: 100%;
+    height: 100px;
+    -moz-border-bottom-colors: none;
+    -moz-border-left-colors: none;
+    -moz-border-right-colors: none;
+    -moz-border-top-colors: none;
+    background: none repeat scroll 0 0 rgba(0, 0, 0, 0.07);
+    border-color: -moz-use-text-color #FFFFFF #FFFFFF -moz-use-text-color;
+    border-image: none;
+    border-radius: 6px 6px 6px 6px;
+    border-style: none solid solid none;
+    border-width: medium 1px 1px medium;
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.12) inset;
+    color: #555555;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 1em;
+    line-height: 1.4em;
+    padding: 5px 8px;
+    transition: background-color 0.2s ease 0s;
+}
+
+.bty-textarea-1:focus {
+    background: none repeat scroll 0 0 #FFFFFF;
+    outline-width: 0;
+}
+    </textarea>
+        </div>
+
     </div>
     <div class="col-md-12">
         <h4>Textarea 2</h4>

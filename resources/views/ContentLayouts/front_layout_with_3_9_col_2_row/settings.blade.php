76a12733b577cfedb2b9898426b213c6fca0b15c
sahakavatar <hakobyan.sahak88@gmail.com>
2018-02-21 11:59:59 +0400

    layout

---
+++ resources/views/ContentLayouts/front_layout_with_3_9_col_2_row/settings.blade.php
@@ -0,0 +1,16 @@
+<div class="form-group">
+    <div class="row">
+        <label for="navstyle" class="col-xs-3">Left Areas Unit</label>
+        <div class="col-xs-9">
+            <div class="input-group">
+                {!! BBbutton2('unit',"left_bar","frontend_sidebar","Change",['class'=>'btn btn-default change-layout','data-type'=>'frontend_sidebar','model'=>null]) !!}
+            </div>
+        </div>
+        <label for="navstyle" class="col-xs-3">Top content</label>
+        <div class="col-xs-9">
+            <div class="input-group">
+                {!! BBbutton2('unit',"top_content","slider","Change",['class'=>'btn btn-default change-layout','data-type'=>'frontend_sidebar','model'=>$model]) !!}
+            </div>
+        </div>
+    </div>
+</div>

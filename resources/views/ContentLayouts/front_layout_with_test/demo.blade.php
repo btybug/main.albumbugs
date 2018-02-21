ff635afcd3bcbbfeed788fc7d1b334b510f4aca5
gev2092 <gev2092harutyunyan@gmail.com>
2018-02-21 01:42:16 -0800

    test layout

---
+++ resources/views/ContentLayouts/front_layout_with_test/demo.blade.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="left-bar">
+
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div class="top-bar">
+
+            </div>
+            <div class="main-cont">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+{!! BBstyle($_this->path.DS.'css/style.css') !!}
---
+++ resources/views/ContentLayouts/front_layout_with_test/main.blade.php
@@ -0,0 +1,28 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="left-bar">
+                {{--@if(has_setting($settings,"left_bar",false))
+                    {!! BBRenderUnits($settings['left_bar'],isset($settings['_page'])?['_page'=>$settings['_page']]:[]) !!}
+                @endif--}}
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div class="top-bar">
+                @if(has_setting($settings,"top_hook",false))
+                    {!! BBRenderUnits($settings['top_hook'],isset($settings['_page'])?['_page'=>$settings['_page']]:[]) !!}
+                @endif
+            </div>
+            <div class="top-bar">
+                @if(has_setting($settings,"top_content",false))
+                    {!! BBRenderUnits($settings['top_content'],isset($settings['_page'])?['_page'=>$settings['_page']]:[]) !!}
+                @endif
+            </div>
+            <div class="main-cont">
+                {!! main_content() !!}
+            </div>
+        </div>
+    </div>
+</div>
+
+{!! BBstyle($_this->path.DS.'css/style.css') !!}

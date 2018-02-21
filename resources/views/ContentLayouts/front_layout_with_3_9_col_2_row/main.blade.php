8a863d31d01fd11675ec7fbdc1619cb57fb0190f
gev2092 <gev2092harutyunyan@gmail.com>
2018-02-21 04:28:10 -0800

    content layout container styles

--- resources/views/ContentLayouts/front_layout_with_3_9_col_2_row/main.blade.php
+++ resources/views/ContentLayouts/front_layout_with_3_9_col_2_row/main.blade.php
@@ -1,19 +1,19 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-3 col-sm-3 col-xs-12">
-            <div class="left-bar">
+            <div class="left-bar {{isset($settings['left_bar_style']) ? $settings['left_bar_style'] : ''}}">
                 @if(has_setting($settings,"left_bar",false))
                     {!! BBRenderUnits($settings['left_bar'],isset($settings['_page'])?['_page'=>$settings['_page']]:[]) !!}
                 @endif
             </div>
         </div>
         <div class="col-md-9 col-sm-9 col-xs-12">
-            <div class="top-bar">
+            <div class="top-bar {{isset($settings['top_content_style']) ? $settings['top_content_style'] : ''}}">
                 @if(has_setting($settings,"top_content",false))
                     {!! BBRenderUnits($settings['top_content'],isset($settings['_page'])?['_page'=>$settings['_page']]:[]) !!}
                 @endif
             </div>
-            <div class="main-cont">
+            <div class="main-cont {{isset($settings['main_content_style']) ? $settings['main_content_style'] : ''}}">
                 {!! main_content() !!}
             </div>
         </div>
@@ -21,3 +21,4 @@
 </div>
 
 {!! BBstyle($_this->path.DS.'css/style.css') !!}
+{!! useDinamicStyle('containers') !!}

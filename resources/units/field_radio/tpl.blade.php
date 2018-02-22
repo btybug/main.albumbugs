e4694758a0d22da7d3738ca1597aca544591eb6f
Sahak <hakobyan.sahak88@gmail.com>
2018-02-22 13:35:35 +0400

    update

--- resources/units/field_radio/tpl.blade.php
+++ resources/units/field_radio/tpl.blade.php
@@ -1,39 +1,56 @@
 @php
-$arr = [];
-if(isset($settings['json_data']) && count($settings['json_data'])){
-    switch ($settings['data_source']){
-        case "related" :
-            if(isset($settings['json_data']['data_source_table_name']) && isset($settings['json_data']['data_source_columns'])){
-                $table = $settings['json_data']['data_source_table_name'];
-                $column = $settings['json_data']['data_source_columns'];
-                if (\Schema::hasColumn($table, $column)) {
-                    $result = \DB::table($table)->pluck($column,'id');
-                    $arr =  (count($result)) ? $result->toArray() : [];
+    $arr = [];
+    if(isset($settings['json_data']) && count($settings['json_data'])){
+        switch ($settings['data_source']){
+            case "related" :
+                if(isset($settings['json_data']['data_source_table_name']) && isset($settings['json_data']['data_source_columns'])){
+                    $table = $settings['json_data']['data_source_table_name'];
+                    $column = $settings['json_data']['data_source_columns'];
+                    if (\Schema::hasColumn($table, $column)) {
+                        $result = \DB::table($table)->pluck($column,'id');
+                        $arr =  (count($result)) ? $result->toArray() : [];
+                    }
                 }
-            }
-            break;
-        case "manual" :
-            if(isset($settings['json_data']['manual']) && $settings['json_data']['manual']){
-                $arr = (count(explode(',',$settings['json_data']['manual']))) ? explode(',',$settings['json_data']['manual']) : [];
-            }
-            break;
+                break;
+            case "manual" :
+                if(isset($settings['json_data']['manual']) && $settings['json_data']['manual']){
+                    $arr = (count(explode(',',$settings['json_data']['manual']))) ? explode(',',$settings['json_data']['manual']) : [];
+                }
+                break;
+        }
     }
-}
 @endphp
-<fieldset class="bty-form-radio formgeneral" id="bty-input-id-0">
-    <div class="form-group">
-        <label class="col-sm-12 control-label">{!! issetReturn($settings,'label',null) !!}</label>
-        <div class="col-sm-12">
-            @if(count($arr))
-                @foreach($arr as $key => $item)
-                    <div class="radio block">
-                        <label>
-                            <input name="table_name_column_name"  value="{{ $key }}" type="radio" id="bty-gender-form-{{ $key }}">
-                            {{$item}}
-                        </label>
-                    </div>
-                @endforeach
-            @endif
+<div class="col-md-6">
+
+    <fieldset class="bty-form-radio formgeneral" id="bty-input-id-0">
+        <div class="form-group">
+
+            <label class="col-sm-6 control-label">
+            <span class="input-group-addon col-md-2">
+                    <i class="fa {!! issetReturn($settings,'icon',null) !!}"></i>
+                </span>{!! issetReturn($settings,'label',null) !!}</label>
+            <div class="col-sm-12">
+                @if(count($arr))
+                    @foreach($arr as $key => $item)
+                        <div class="radio block">
+                            <label>
+                                <input name="table_name_column_name" value="{{ $key }}" type="radio"
+                                       id="bty-gender-form-{{ $key }}">
+                                {{$item}}
+                            </label>
+                        </div>
+                    @endforeach
+                @endif
+            </div>
+            <div class="col-sm-3">
+            <span class="input-group-addon tooltip1">
+                        <i class="fa {!! issetReturn($settings,'tooltip_icon',null)!!}"></i>
+                    <span class="tooltiptext">{!! issetReturn($settings,'help',null) !!}</span>
+            </span>
+            </div>
         </div>
-    </div>
-</fieldset>
+    </fieldset>
+
+</div>
+
+
---
+++ resources/units/field_text/settings.blade.php
@@ -0,0 +1,102 @@
+<div class="col-md-12">
+    <div class="row  visibility-box  {!! (1) ? "show" : "hide" !!}">
+        <div class="bty-panel-collapse">
+            <div>
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#inputsetting"
+                   aria-expanded="true">
+                    <span class="icon"><i class="fa fa-chevron-down" aria-hidden="true"></i></span>
+                    <span class="title">Input Setting</span>
+                </a>
+            </div>
+            <div id="inputsetting" class="collapse in" aria-expanded="true" style="">
+                <div class="content bty-settings-panel">
+                    <div class="form-group col-md-12">
+                        <div class="col-md-2">
+                            <label>Field style</label>
+                        </div>
+                        <div class="col-md-9">
+                            <select name="" id="" class="form-control">
+                                <option value="">style1</option>
+                                <option value="">style2</option>
+                                <option value="">style3</option>
+                            </select>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-md-6">
+                            <label for="lablename" class="col-sm-3 p-l-0 control-label m-0  text-left">Label
+                                name</label>
+                            <div class="col-sm-8">
+                                {!! Form::text('label',null,['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-md-6">
+                            <label for="fieldicon" class="col-sm-3 p-l-0 control-label m-0 text-left">Field Icon</label>
+                            <div class="col-sm-8">
+                                {!!Form::text('icon',null,['class' => 'form-control icp'])  !!}
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="tooltip-icon" class="col-sm-3 m-0 control-label text-left">Tooltip Icon</label>
+                            <div class="col-sm-8">
+                                {!!Form::text('tooltip_icon',null,['class' => 'form-control icp','id'=>'tooltip-icon'])  !!}
+
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-md-6">
+                            <label for="help" class="col-sm-3 m-0 control-label text-left">help</label>
+                            <div class="col-sm-8">
+                                {!! Form::textarea('help',null,['class'=>'form-control','id'=>'help']) !!}
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <label for='validation_message' class="col-sm-3 m-0 control-label text-left">Error
+                                Message</label>
+                            <div class="col-sm-8">
+                                {!! Form::textarea('validation_message',null,['class' => 'form-control','id'=>'validation_message']) !!}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div  class="row visibility-box {!! (1) ? "show" : "hide" !!}">
+        <div class="bty-panel-collapse">
+            <div>
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#inputdata"
+                   aria-expanded="true">
+                    <span class="icon"><i class="fa fa-chevron-down" aria-hidden="true"></i></span>
+                    <span class="title">Input Data</span>
+                </a>
+            </div>
+            <div id="inputdata" class="collapse in" aria-expanded="true" style="">
+                <div class="content bty-settings-panel">
+                    <div class="col-md-12">
+                        <div class="row m-b-10 mapping-column">
+                            @include($_this->slug."::mapping-column")
+                        </div>
+                    </div>
+                    <div class="form-group col-md-12 m-b-10">
+                        <label class="col-sm-4 p-l-0 control-label m-0  text-left">Extra Validation</label>
+                        <div class="col-sm-8">
+                            {!! Form::text('extravalidation',null,['class' => 'form-control core-val']) !!}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+{!! BBstyle($_this->path.DS.'css'.DS.'settings.css') !!}

0168d36307262684ac08b78b4f13506f67929a0c
Sahak <hakobyan.sahak88@gmail.com>
2018-02-22 10:51:04 +0400

    update

---
+++ resources/units/field_checkbox/settings.blade.php
@@ -0,0 +1,82 @@
+<div class="col-md-12  m-t-20">
+    <div class="row" style="color: black">
+
+        <div class="row  visibility-box  {!! (1) ? "show" : "hide" !!}" style="color: black">
+            <div class="panel panel-default p-0">
+                <div class="panel-heading">Input Setting</div>
+                <div class="panel-body">
+                    <div class="form-group col-md-12 m-b-10">
+                        <div class="col-md-6">
+                            <label for="lablename" class="col-sm-3 p-l-0 control-label m-0  text-left">Label
+                                name</label>
+                            <div class="col-sm-8">
+                                {!! Form::text('label',null,['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="placeholder" class="col-sm-3 control-label m-0 text-left ">Placeholder</label>
+                            <div class="col-sm-8">
+                                {!! Form::text('placeholder',null,['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group col-md-12 m-b-10">
+                        <div class="col-md-6">
+                            <label for="fieldicon" class="col-sm-3 p-l-0 control-label m-0 text-left">Field Icon</label>
+                            <div class="col-sm-8">
+                                {!!Form::text('icon',null,['class' => 'form-control icp'])  !!}
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="tooltip-icon" class="col-sm-3 m-0 control-label text-left">Tooltip Icon</label>
+                            <div class="col-sm-8">
+                                {!!Form::text('tooltip_icon',null,['class' => 'form-control icp','id'=>'tooltip-icon'])  !!}
+
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="form-group col-md-12 m-b-10">
+                        <div class="col-md-6">
+                            <label for="help" class="col-sm-3 m-0 control-label text-left">help</label>
+                            <div class="col-sm-8">
+                                {!! Form::textarea('help',null,['class'=>'form-control','id'=>'help']) !!}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group col-md-12 m-b-10">
+                        <div class="form-group col-md-6 m-b-10">
+                            <label  for='validation_message' class="col-sm-3 m-0 control-label text-left">Error Message</label>
+                            <div class="col-sm-8">
+                                {!! Form::textarea('validation_message',null,['class' => 'form-control','id'=>'validation_message']) !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="row visibility-box {!! (1) ? "show" : "hide" !!}">
+            <div class="panel panel-default p-0">
+                <div class="panel-heading">Input Data</div>
+                <div class="panel-body">
+
+                    <div class="col-md-6">
+                        <div class="row m-b-10 mapping-column">
+                            @include($_this->slug."::mapping-column")
+                        </div>
+                    </div>
+                    <div class="form-group col-md-6 m-b-10">
+                        <label class="col-sm-3 p-l-0 control-label m-0  text-left">Extra Validation</label>
+                        <div class="col-sm-8">
+                            {!! Form::text('extravalidation',null,['class' => 'form-control core-val']) !!}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
--- resources/units/field_checkbox/tpl.blade.php
+++ resources/units/field_checkbox/tpl.blade.php
@@ -1,17 +1,39 @@
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="checkboxes">Multiple Checkboxes</label>
-            <div class="col-md-4">
-                <div class="checkbox">
-                    <label for="checkboxes-0">
-                        <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">
-                        Option one
-                    </label>
-                </div>
-                <div class="checkbox">
-                    <label for="checkboxes-1">
-                        <input type="checkbox" name="checkboxes" id="checkboxes-1" value="2">
-                        Option two
-                    </label>
-                </div>
-            </div>
+@php
+    $arr = [];
+    if(isset($settings['json_data']) && count($settings['json_data'])){
+        switch ($settings['data_source']){
+            case "related" :
+                if(isset($settings['json_data']['data_source_table_name']) && isset($settings['json_data']['data_source_columns'])){
+                    $table = $settings['json_data']['data_source_table_name'];
+                    $column = $settings['json_data']['data_source_columns'];
+                    if (\Schema::hasColumn($table, $column)) {
+                        $result = \DB::table($table)->pluck($column,'id');
+                        $arr =  (count($result)) ? $result->toArray() : [];
+                    }
+                }
+                break;
+            case "manual" :
+                if(isset($settings['json_data']['manual']) && $settings['json_data']['manual']){
+                    $arr = (count(explode(',',$settings['json_data']['manual']))) ? explode(',',$settings['json_data']['manual']) : [];
+                }
+                break;
+        }
+    }
+@endphp
+<fieldset class="bty-form-radio formgeneral" id="bty-input-id-0">
+    <div class="form-group">
+        <label class="col-sm-12 control-label">{!! issetReturn($settings,'label',null) !!}</label>
+        <div class="col-sm-12">
+            @if(count($arr))
+                @foreach($arr as $key => $item)
+                    <div class="radio block">
+                        <label>
+                            <input name="table_name_column_name"  value="{{ $key }}" type="checkbox" id="bty-gender-form-{{ $key }}">
+                            {{$item}}
+                        </label>
+                    </div>
+                @endforeach
+            @endif
         </div>
+    </div>
+</fieldset>

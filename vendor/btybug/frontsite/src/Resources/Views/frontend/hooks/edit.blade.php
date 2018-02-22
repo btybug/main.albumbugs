c5f2da8fe886c6cae55648fe6967c6f0e33c5778
sahakavatar <hakobyan.sahak88@gmail.com>
2018-02-22 10:23:29 +0400

    edit

--- vendor/btybug/frontsite/src/Resources/Views/frontend/hooks/edit.blade.php
+++ vendor/btybug/frontsite/src/Resources/Views/frontend/hooks/edit.blade.php
@@ -1,10 +1,35 @@
 @extends('btybug::layouts.admin')
 @section('content')
     <div role="tabpanel" class="m-t-10" id="main">
-        Units here
+            <div class="head-top">
+                <div class="col-md-3 name">
+                    Hook Name
+                </div>
+                <div class="col-md-3">
+                    <select name="" id="" class="form-control">
+                        <option value="">1</option>
+                        <option value="">2</option>
+                    </select>
+                </div>
+                <div class="col-md-6">
+                    <button class="btn btn-info pull-right">Save</button>
+                </div>
+                <div class="clearfix"></div>
+            </div>
     </div>
 @stop
 @section('CSS')
+    <style>
+        .head-top{
+            padding: 12px 0;
+            background: #78909C;
+            margin: 0 -15px;
+        }
+        .head-top .name{
+            margin-top: 7px;
+            color: white;
+        }
+    </style>
     {!! HTML::style('public/css/menu.css?v=0.16') !!}
     {!! HTML::style('public/css/admin_pages.css') !!}
     {!! HTML::style('public/css/tool-css.css?v=0.23') !!}

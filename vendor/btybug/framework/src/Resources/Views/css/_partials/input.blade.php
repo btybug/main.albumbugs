f8367bc402904ff4f27072dffd16c473639f7d61
sahakavatar <hakobyan.sahak88@gmail.com>
2018-02-23 13:46:29 +0400

    css

--- vendor/btybug/framework/src/Resources/Views/css/_partials/input.blade.php
+++ vendor/btybug/framework/src/Resources/Views/css/_partials/input.blade.php
@@ -1,1 +1,99 @@
-input here
+<div class="d1">
+    <h2>Input Text</h2>
+    <div class="col-md-12">
+        <div class="col-md-4">
+            <h4>Input Text 1</h4>
+            <h5>bty-input-text-1</h5>
+            <input class="bty-input-text-1" type="text" placeholder="name">
+        </div>
+        <div class="col-md-4">
+            <h4>Input Text 2</h4>
+            <h5>bty-input-text-3</h5>
+            <input class="bty-input-text-2" type="text" placeholder="name">
+        </div>
+        <div class="col-md-4">
+            <h4>Input Text 3</h4>
+            <h5>bty-input-text-3</h5>
+            <input class="bty-input-text-3" type="text" placeholder="name">
+        </div>
+
+    </div>
+    <div class="col-md-12">
+        <div class="col-md-4">
+            <h4>Input Text 4</h4>
+            <h5>bty-input-text-4</h5>
+            <input class="bty-input-text-4" type="text" placeholder="name">
+        </div>
+        <div class="col-md-4">
+            <h4>Input Text 5</h4>
+            <h5>bty-input-text-5</h5>
+            <input class="bty-input-text-5" type="text" placeholder="name">
+        </div>
+        <div class="col-md-4">
+            <h4>Input Text 6</h4>
+            <h5>bty-input-text-6</h5>
+            <input class="bty-input-text-6" type="text" placeholder="name" >
+        </div>
+    </div>
+    <h2>Input with label</h2>
+    <div class="col-md-12">
+        <div class="col-md-4">
+            <h4>Input with label 1</h4>
+            <h5>bty-input-label-1</h5>
+            <input class="bty-input-label-1" type="text" >
+            <label><span>What's your name?</span></label>
+        </div>
+        <div class="col-md-4">
+            <h4>Input with label 2</h4>
+            <h5>bty-input-label-2</h5>
+            <input class="bty-input-label-2" type="text">
+            <label>What's your name?</label>
+        </div>
+        <div class="col-md-4">
+            <h4>Input with label 3</h4>
+            <h5>bty-input-label-3</h5>
+            <input class="bty-input-label-3" type="text" placeholder="name">
+            <label>What's your name?</label>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="col-md-4">
+            <h4>Input with label 4</h4>
+            <h5>bty-input-label-4</h5>
+            <input class="bty-input-label-4" type="text" placeholder="Your name?">
+            <label>Name</label>
+        </div>
+        <div class="col-md-4">
+            <h4>Input with label 5</h4>
+            <h5>bty-input-label-5</h5>
+            <input class="bty-input-label-5" type="text" placeholder="Your name?">
+            <label>What's your name?</label>
+        </div>
+        <div class="col-md-4">
+            <h4>Input with label 6</h4>
+            <h5>bty-input-label-6</h5>
+            <input class="bty-input-label-6" type="text" placeholder="Your name?">
+            <label>Name?</label>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="col-md-6">
+            <h4>Input with label hover icon 7</h4>
+            <h5>bty-input-label-7</h5>
+            <input class="bty-input-label-7" type="text" placeholder="Your name?">
+            <label>Name</label>
+            <label><i class="fa fa-question" aria-hidden="true"></i></label>
+            <p>Lorem ipsum Lorem</p>
+        </div>
+        <div class="col-md-6">
+            <h4>Input with label click icon 8</h4>
+            <h5>bty-input-label-8</h5>
+            <input class="bty-input-label-8" type="text" placeholder="Your name?">
+            <label>Name</label>
+            <label><i class="fa fa-question" aria-hidden="true"></i></label>
+            <p>Lorem ipsum Lorem</p>
+        </div>
+
+    </div>
+
+</div>

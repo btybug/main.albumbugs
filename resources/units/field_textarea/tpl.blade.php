9926cfcf9973f2e5be87a61a1599de743f4b7e4e
Eduard <edo.terakyan@gmail.com>
2018-02-23 22:33:30 +0400

    tpl

--- resources/units/field_textarea/tpl.blade.php
+++ resources/units/field_textarea/tpl.blade.php
@@ -1,7 +1,8 @@
-
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="textarea">Text Area</label>
-            <div class="col-md-4">
-                <textarea class="form-control" id="textarea" name="textarea">default text</textarea>
-            </div>
-        </div>
+<div class="form-group">
+    <label class="col-md-4 control-label" for="textarea">{!! issetReturn($settings,'label',null) !!}</label>
+    <div class="col-md-4">
+                <textarea name="{!! (isset($source['field'])) ? print_field_name($source['field']) : "" !!}"
+                          class="form-control"
+                          id="textarea">{!! issetReturn($settings,'default_val',null) !!}</textarea>
+    </div>
+</div>

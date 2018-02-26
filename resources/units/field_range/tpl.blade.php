6235f7e8da7b01fdee12b0ba8a4f4c40c263c667
gev2092 <gev2092harutyunyan@gmail.com>
2018-02-26 04:25:38 -0800

    fix range

--- resources/units/field_range/tpl.blade.php
+++ resources/units/field_range/tpl.blade.php
@@ -2,6 +2,6 @@
 
 @if(isset($settings['range_inp']))
         <input type="range" class="range {{$settings['range_inp']}}" {{isset($settings['range_min'])?'min='.$settings['range_min']:''}}
-        {{isset($settings['range_max'])?'max='.$settings['range_max']:''}} {{isset($settings['range_start'])?'start='.$settings['range_start']:''}} >
+        {{isset($settings['range_max'])?'max='.$settings['range_max']:''}} {{isset($settings['range_start'])?'value='.$settings['range_start']:''}} >
 @endif
 

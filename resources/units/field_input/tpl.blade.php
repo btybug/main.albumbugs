7104f3c49aabd984336a660af77f824757495ec5
Sahak <hakobyan.sahak88@gmail.com>
2018-02-27 10:22:03 +0400

    update

--- resources/units/field_input/tpl.blade.php
+++ resources/units/field_input/tpl.blade.php
@@ -1,20 +1,13 @@
-<fieldset class="formgeneral" id="bty-input-id-{!!  (isset($source['field'])) ? $source['field']['id'] : "" !!}">
-    <div class="form-group">
-        <label class="col-md-12 control-label">{!! issetReturn($settings,'label',null) !!}</label>
-        <div class="col-md-12">
-            <div class="input-group">
-                                    <span class="input-group-addon">
-                    <i class="fa {!! issetReturn($settings,'icon',null) !!}"></i>
-                </span>
-
-                <input class="form-control1 " placeholder="{!! issetReturn($settings,'placeholder',null) !!}"
-                       name="{!! (isset($source['field'])) ? print_field_name($source['field']) : "" !!}" type="text" value="{!! issetReturn($settings,'default_val',null) !!}">
-                <span class="input-group-addon tooltip1">
-                        <i class="fa {!! issetReturn($settings,'tooltip_icon',null) !!}"></i>
-                    <span class="tooltiptext">{!! issetReturn($settings,'help',null) !!}</span>
-            </span>
+<div class="form-horizontal">
+    <fieldset>
+        <!-- Text input-->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="textinput">Text Input</label>
+            <div class="col-md-4">
+                <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+                <span class="help-block">help</span>
             </div>
         </div>
-    </div>
-</fieldset>
 
+    </fieldset>
+</div>

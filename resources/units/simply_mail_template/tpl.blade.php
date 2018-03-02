1368eae6ebda6a87def832c976ce3c68cb0e164e
sahakavatar <hakobyan.sahak88@gmail.com>
2018-03-02 16:57:52 +0400

    mail template

---
+++ resources/units/simply_mail_template/tpl.blade.php
@@ -0,0 +1,31 @@
+{!! BBstyle($_this->path.DS.'css/main.css') !!}
+<table>
+    <tbody>
+    <tr>
+        <td class="head-img">
+            <img src="http://placehold.it/1200x600" alt="alt_text">
+        </td>
+    </tr>
+    <tr>
+        <td class="main-head">
+            <h1>
+                Lorem ipsum h1</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deserunt impedit nam neque nulla tempore voluptates? At dolorem error, in molestiae molestias nemo possimus quibusdam recusandae repellendus ullam velit veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </td>
+    </tr>
+    <tr>
+        <td class="button-td">
+            <a href="javascript:void(0);">
+                <span>&nbsp;&nbsp;&nbsp;&nbsp;A Button&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td class="present-td">
+            <h2>
+                Lorem ipsum 2</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic in tempore veniam! Animi aspernatur debitis dolores eligendi error expedita itaque neque repellendus sint sit. Aspernatur explicabo facere iusto nihil? Delectus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur dolorum ducimus earum eos eveniet ex exercitationem expedita natus nobis obcaecati odit, officia officiis pariatur perferendis quas quisquam rerum tempore voluptates.</p>
+        </td>
+    </tr>
+    </tbody>
+</table>

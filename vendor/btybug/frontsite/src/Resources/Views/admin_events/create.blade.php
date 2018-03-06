79ad65a9c947993ef8488b260f21ddf1a9247de9
Eduard <edo.terakyan@gmail.com>
2018-03-06 23:10:38 +0400

    up

--- vendor/btybug/frontsite/src/Resources/Views/admin_events/create.blade.php
+++ vendor/btybug/frontsite/src/Resources/Views/admin_events/create.blade.php
@@ -17,13 +17,48 @@
            <div class="panel panel-default p-0 boxpanelminheight">
                <div class="panel-heading">1. Select Trigger</div>
                <div class="panel-body">
-                   <div class="col-md-4" style="    min-height: 100%;border: 1px solid;padding: 5px;">
+                   <div class="col-md-6" style="    min-height: 100%;border: 1px solid;padding: 5px;">
                        <div class="col-md-12 item-event">
                            User Login
                        </div>
                        <div class="col-md-12 item-event">
                            Form Submit
                        </div>
+                   </div>
+                   <div class="col-md-6" style="    min-height: 100%;border: 1px solid;padding: 5px;">
+                       <div class="col-md-12" style="height: 30px;border-bottom: 2px solid;background: brown;">
+                           Actions
+                       </div>
+                       <div class="col-md-12">
+                           <div class="col-md-12 item-event">
+                              Action 1
+                               <a class="pull-right">Edit</a>
+                               <a class="pull-right">Delete</a>
+                           </div>
+
+                       </div>
+                   </div>
+               </div>
+           </div>
+
+           <div class="panel panel-default p-0 boxpanelminheight">
+               <div class="panel-heading"></div>
+               <div class="panel-body">
+                   <div class="col-md-3" style="    min-height: 100%;border: 1px solid;padding: 5px;">
+                       <div class="col-md-12" style="height: 30px;border-bottom: 2px solid;background: #34a536;">
+                          Available Actions
+                       </div>
+                       <div class="col-md-12">
+                           <div class="col-md-12 item-event">
+                               Action A
+                           </div>
+                           <div class="col-md-12 item-event">
+                               Action B
+                           </div>
+                       </div>
+                   </div>
+                   <div class="col-md-9" style="    min-height: 100%;border: 1px solid;padding: 5px;">
+
                    </div>
                </div>
            </div>

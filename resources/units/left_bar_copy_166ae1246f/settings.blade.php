ca006ae5b0099d2796444ee4a78d9d225482a6ad
Eduard <edo.terakyan@gmail.com>
2018-03-16 22:41:05 +0400

    settings

--- resources/units/left_bar_copy_166ae1246f/settings.blade.php
+++ resources/units/left_bar_copy_166ae1246f/settings.blade.php
@@ -3,21 +3,21 @@
         <div class="form-group my_rows1">
             <label for="newcontainer" class="col-sm-4 labelTitle">User Cart Class</label>
             <div class="col-sm-8">
-            <input type="checkbox" name="user_cart_class" class="form-control">
+            <input type="text" name="user_cart_class" class="form-control">
             </div>
         </div>
 
         <div class="form-group my_rows1">
             <label class="col-md-4 control-label" for="sitenametextstyle">Background Image</label>
             <div class="col-md-8">
-                <input type="checkbox" name="background_image" class="form-control">
+                <input type="text" name="background_image" class="form-control">
             </div>
         </div>
 
     <div class="form-group my_rows1">
         <label class="col-md-4 control-label" for="socialiconswidget_11">User Image</label>
         <div class="col-md-8">
-            <input type="checkbox" name="user_image" class="form-control">
+            <input type="text" name="user_image" class="form-control">
 
         </div>
     </div>
@@ -25,15 +25,15 @@
     <div class="form-group my_rows1">
         <label class="col-md-4 control-label" for="my-menu">User Name</label>
         <div class="col-md-8">
-            <input type="checkbox" name="user_name" class="form-control">
+            <input type="text" name="user_name" class="form-control">
         </div>
     </div>
-    <div class="form-group my_rows1">
-        <label class="col-md-4 control-label" for="my-menu">Select Menu</label>
-        <div class="col-md-8">
-            {!! BBbutton('menus','my-menu','Select Menu',['class'=>'form-control input-md btn btn-info', 'data-type'=>'backend','model'=>$settings]) !!}
-        </div>
-    </div>
+    {{--<div class="form-group my_rows1">--}}
+        {{--<label class="col-md-4 control-label" for="my-menu">Select Menu</label>--}}
+        {{--<div class="col-md-8">--}}
+            {{--{!! BBbutton('menus','my-menu','Select Menu',['class'=>'form-control input-md btn btn-info', 'data-type'=>'backend','model'=>$settings]) !!}--}}
+        {{--</div>--}}
+    {{--</div>--}}
 
     </div>
 </div>

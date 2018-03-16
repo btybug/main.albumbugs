cb176194e70cdd11cb8247ca1437acc4553e0cb1
sahakavatar <hakobyan.sahak88@gmail.com>
2018-03-16 11:54:44 +0400

    user card

--- vendor/btybug/framework/src/Resources/Views/css/_partials/user_card.blade.php
+++ vendor/btybug/framework/src/Resources/Views/css/_partials/user_card.blade.php
@@ -172,6 +172,504 @@
         </div>
     </div>
     <div class="col-md-12">
+        <div class="col-md-6 p-t-34">
+            <div class="bty-user-card-dark-1">
+                <div>
+                    <img src="http://www.imagefully.com/wp-content/uploads/2015/10/Nice-Best-Photography-Alone-Picture.jpg"
+                         alt="">
+                </div>
+                <div>
+                    <h3>User Name</h3>
+                    <h4>profession</h4>
+                    <p><i class="fa fa-map-marker"></i>location</p>
+                    <h5>Info</h5>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy.</p>
+                </div>
+                <ul>
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h5>bty-user-card-dark-1</h5>
+            <textarea class="form-control" name="" id="" cols="30" rows="5" readonly>
+.bty-user-card-dark-1 {
+    position: relative;
+    -webkit-border-radius: 8px;
+    border-radius: 8px;
+    -webkit-box-shadow: 0 4px 1px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 4px 1px rgba(0, 0, 0, 0.1);
+    width: 100%;
+    background: #000;
+    text-align: center;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    padding-top: 65px;
+    margin-top: 94px;
+}
+
+.bty-user-card-dark-1 div:nth-of-type(2) {
+    padding: 60px 30px;
+}
+
+.bty-user-card-dark-1 div:first-child {
+    -webkit-border-radius: 50%;
+    border-radius: 50%;
+    -webkit-box-shadow: 0 0 0 3px #000000, 0 0 0 8px #fff;
+    box-shadow: 0 0 0 3px #000000, 0 0 0 8px #fff;
+    top: -75px;
+    margin-left: -75px;
+    left: 50%;
+    position: absolute;
+}
+
+.bty-user-card-dark-1 div:first-child img {
+    width: 150px;
+    height: 150px;
+    -webkit-border-radius: 50%;
+    border-radius: 50%;
+    display: block;
+}
+
+.bty-user-card-dark-1 div:nth-of-type(2) h3 {
+    font-size: 38px;
+    font-weight: 100;
+    margin-bottom: 2px;
+    color: #fff;
+}
+
+.bty-user-card-dark-1 div:nth-of-type(2) h4 {
+    font-size: 18px;
+    font-style: italic;
+    color: #707070;
+    margin-bottom: 2px;
+}
+
+.bty-user-card-dark-1 div:nth-of-type(2) h5 {
+    font-size: 34px;
+    font-weight: 100;
+    margin-bottom: 2px;
+    color: #c3c3c3;
+}
+
+.bty-user-card-dark-1 div:nth-of-type(2) h4 + p {
+    color: #b3b3b3;
+    font-size: 14px;
+    margin-top: 5px;
+    margin-bottom: 15px;
+    padding-bottom: 15px;
+    border-bottom: 1px solid #cccccc91;
+}
+
+.bty-user-card-dark-1 div:nth-of-type(2) h4 + p i {
+    color: #c6c6c6;
+    margin-right: 4px;
+}
+
+.bty-user-card-dark-1 div:nth-of-type(2) p:last-child {
+    font-size: 14px;
+    color: #888;
+}
+
+.bty-user-card-dark-1 > ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    padding-top: 1px;
+    display: table;
+    margin: 0 auto;
+}
+
+.bty-user-card-dark-1 > ul > li {
+    float: left;
+    display: block;
+}
+
+.bty-user-card-dark-1 > ul > li > a i {
+    display: block;
+    width: 45px;
+    height: 55px;
+    line-height: 55px;
+    font-size: 28px;
+    text-decoration: none;
+    color: #b3b3b3;
+    border-right: 1px solid #ededed;
+    -webkit-transition: 0.2s;
+    -moz-transition: 0.2s;
+    -o-transition: 0.2s;
+    -ms-transition: 0.2s;
+    transition: 0.2s;
+}
+
+.bty-user-card-dark-1 > ul > li > a:hover {
+    text-decoration: none;
+}
+
+.bty-user-card-dark-1 > ul > li > a:hover i {
+    color: white;
+}
+
+.bty-user-card-dark-1 > ul > li > a:hover .fa-facebook {
+    background: #3b5998;
+}
+
+.bty-user-card-dark-1 > ul > li > a:hover .fa-dribbble {
+    background: #c32361;
+}
+
+.bty-user-card-dark-1 > ul > li > a:hover .fa-twitter {
+    background: #55acee;
+}
+
+.bty-user-card-dark-1 > ul > li > a:hover .fa-pinterest {
+    background: #cd2129;
+}
+
+.bty-user-card-dark-1 > ul > li > a:hover .fa-linkedin {
+    background: #1683bc;
+}
+
+.bty-user-card-dark-1 > ul > li:last-child > a i {
+    border-right: none;
+}
+            </textarea>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="col-md-6 p-t-34">
+            <div class="bty-user-card-dark-img-1">
+                <div>
+                    <img src="http://www.imagefully.com/wp-content/uploads/2015/10/Nice-Best-Photography-Alone-Picture.jpg"
+                         alt="">
+                </div>
+                <div>
+                    <h3>User Name</h3>
+                    <h4>profession</h4>
+                    <p><i class="fa fa-map-marker"></i>location</p>
+                    <h5>Info</h5>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy.</p>
+                </div>
+                <ul>
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h5>bty-user-card-dark-img-1</h5>
+            <textarea class="form-control" name="" id="" cols="30" rows="5" readonly>
+.bty-user-card-dark-img-1 {
+    -webkit-border-radius: 8px;
+    border-radius: 8px;
+    -webkit-box-shadow: 0 4px 1px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 4px 1px rgba(0, 0, 0, 0.1);
+    width: 100%;
+    background: #000;
+    text-align: center;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    padding-top: 65px;
+    margin-top: 94px;
+}
+
+.bty-user-card-dark-img-1 div:nth-of-type(2) {
+    padding: 60px 30px;
+    border-bottom: 1px solid #cccccc91;
+}
+
+.bty-user-card-dark-img-1 div:first-child {
+    -webkit-box-shadow: 0 0 0 3px #000000, 0 0 0 8px #fff;
+    box-shadow: 0 0 0 3px #000000, 0 0 0 8px #fff;
+    width: 50%;
+    margin: 0 auto;
+}
+
+.bty-user-card-dark-img-1 div:first-child img {
+    width: 100%;
+    height: 150px;
+    object-fit: cover;
+}
+
+.bty-user-card-dark-img-1 div:nth-of-type(2) h3 {
+    font-size: 38px;
+    font-weight: 100;
+    margin-bottom: 2px;
+    color: #fff;
+}
+
+.bty-user-card-dark-img-1 div:nth-of-type(2) h4 {
+    font-size: 18px;
+    font-style: italic;
+    color: #707070;
+    margin-bottom: 2px;
+}
+
+.bty-user-card-dark-img-1 div:nth-of-type(2) h5 {
+    font-size: 34px;
+    font-weight: 100;
+    margin-bottom: 2px;
+    color: #c3c3c3;
+}
+
+.bty-user-card-dark-img-1 div:nth-of-type(2) h4 + p {
+    color: #b3b3b3;
+    font-size: 14px;
+    margin-top: 5px;
+    margin-bottom: 15px;
+    padding-bottom: 15px;
+    border-bottom: 1px solid #cccccc91;
+}
+
+.bty-user-card-dark-img-1 div:nth-of-type(2) h4 + p i {
+    color: #c6c6c6;
+    margin-right: 4px;
+}
+
+.bty-user-card-dark-img-1 div:nth-of-type(2) p:last-child {
+    font-size: 14px;
+    color: #888;
+}
+
+.bty-user-card-dark-img-1 > ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    padding-top: 1px;
+    display: table;
+    margin: 0 auto;
+}
+
+.bty-user-card-dark-img-1 > ul > li {
+    float: left;
+    display: block;
+}
+
+.bty-user-card-dark-img-1 > ul > li > a i {
+    display: block;
+    width: 45px;
+    height: 55px;
+    line-height: 55px;
+    font-size: 28px;
+    text-decoration: none;
+    color: #b3b3b3;
+    border-right: 1px solid #cccccc91;
+    -webkit-transition: 0.2s;
+    -moz-transition: 0.2s;
+    -o-transition: 0.2s;
+    -ms-transition: 0.2s;
+    transition: 0.2s;
+}
+
+.bty-user-card-dark-img-1 > ul > li > a:hover {
+    text-decoration: none;
+}
+
+.bty-user-card-dark-img-1 > ul > li > a:hover i {
+    color: white;
+}
+
+.bty-user-card-dark-img-1 > ul > li > a:hover .fa-facebook {
+    background: #3b5998;
+}
+
+.bty-user-card-dark-img-1 > ul > li > a:hover .fa-dribbble {
+    background: #c32361;
+}
+
+.bty-user-card-dark-img-1 > ul > li > a:hover .fa-twitter {
+    background: #55acee;
+}
+
+.bty-user-card-dark-img-1 > ul > li > a:hover .fa-pinterest {
+    background: #cd2129;
+}
+
+.bty-user-card-dark-img-1 > ul > li > a:hover .fa-linkedin {
+    background: #1683bc;
+}
+
+.bty-user-card-dark-img-1 > ul > li:last-child > a i {
+    border-right: none;
+}
+            </textarea>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="col-md-6 p-t-34">
+            <div class="bty-user-card-dark-social-1">
+                <div>
+                    <img src="http://www.imagefully.com/wp-content/uploads/2015/10/Nice-Best-Photography-Alone-Picture.jpg"
+                         alt="">
+                </div>
+                <div>
+                    <h3>User Name</h3>
+                    <h4>profession</h4>
+                    <p><i class="fa fa-map-marker"></i>location</p>
+                    <h5>Info</h5>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy.</p>
+                </div>
+                <ul>
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h5>bty-user-card-dark-social-1</h5>
+            <textarea class="form-control" name="" id="" cols="30" rows="5" readonly>
+
+.bty-user-card-dark-social-1 {
+    width: 100%;
+    background: #0a115d;
+    text-align: center;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    padding-top: 65px;
+    margin-top: 94px;
+}
+
+.bty-user-card-dark-social-1 div:nth-of-type(2) {
+    padding: 25px 30px 35px 30px;
+    border-bottom: 1px solid #cccccc91;
+}
+
+.bty-user-card-dark-social-1 div:first-child {
+    -webkit-box-shadow: 0 0 0 3px #000000, 0 0 0 8px #fff;
+    box-shadow: 0 0 0 3px #000000, 0 0 0 8px #fff;
+    width: 50%;
+    margin: 0 auto;
+}
+
+.bty-user-card-dark-social-1 div:first-child img {
+    width: 100%;
+    height: 150px;
+    object-fit: cover;
+}
+
+.bty-user-card-dark-social-1 div:nth-of-type(2) h3 {
+    font-size: 38px;
+    font-weight: 100;
+    margin-bottom: 2px;
+    color: #fff;
+}
+
+.bty-user-card-dark-social-1 div:nth-of-type(2) h4 {
+    font-size: 18px;
+    font-style: italic;
+    color: #707070;
+    margin-bottom: 2px;
+}
+
+.bty-user-card-dark-social-1 div:nth-of-type(2) h5 {
+    font-size: 34px;
+    font-weight: 100;
+    margin-bottom: 2px;
+    color: #c3c3c3;
+}
+
+.bty-user-card-dark-social-1 div:nth-of-type(2) h4 + p {
+    color: #b3b3b3;
+    font-size: 14px;
+    margin-top: 5px;
+    margin-bottom: 15px;
+    padding-bottom: 15px;
+    border-bottom: 1px solid #cccccc91;
+}
+
+.bty-user-card-dark-social-1 div:nth-of-type(2) h4 + p i {
+    color: #c6c6c6;
+    margin-right: 4px;
+}
+
+.bty-user-card-dark-social-1 div:nth-of-type(2) p:last-child {
+    font-size: 14px;
+    color: #888;
+}
+
+.bty-user-card-dark-social-1 > ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+
+.bty-user-card-dark-social-1 > ul > li {
+    border-right: 1px solid #cccccc91;
+    width: 100%;
+}
+.bty-user-card-dark-social-1 > ul > li a{
+    width: 100%;
+}
+
+.bty-user-card-dark-social-1 > ul > li > a i {
+    display: block;
+    line-height: 55px;
+    font-size: 28px;
+    text-decoration: none;
+    color: #b3b3b3;
+    -webkit-transition: 0.2s;
+    -moz-transition: 0.2s;
+    -o-transition: 0.2s;
+    -ms-transition: 0.2s;
+    transition: 0.2s;
+}
+
+.bty-user-card-dark-social-1 > ul > li > a:hover {
+    text-decoration: none;
+}
+
+.bty-user-card-dark-social-1 > ul > li > a:hover i {
+    color: white;
+}
+
+.bty-user-card-dark-social-1 > ul > li > a:hover .fa-facebook {
+    background: #3b5998;
+}
+
+.bty-user-card-dark-social-1 > ul > li > a:hover .fa-dribbble {
+    background: #c32361;
+}
+
+.bty-user-card-dark-social-1 > ul > li > a:hover .fa-twitter {
+    background: #55acee;
+}
+
+.bty-user-card-dark-social-1 > ul > li > a:hover .fa-pinterest {
+    background: #cd2129;
+}
+
+.bty-user-card-dark-social-1 > ul > li > a:hover .fa-linkedin {
+    background: #1683bc;
+}
+
+.bty-user-card-dark-social-1 > ul > li:last-child > a i {
+    border-right: none;
+}
+            </textarea>
+        </div>
+    </div>
+    <div class="col-md-12">
         <h4>User Card 1</h4>
         <h5>bty-user-card-1</h5>
         <div class="bty-user-card-1">
@@ -359,3 +857,4 @@
             </div>
         </div>
     </div>
+</div>

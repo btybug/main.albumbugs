571504b83b4f35e3ef64837a7c15ba65d0da4de4
sahakavatar <hakobyan.sahak88@gmail.com>
2018-03-16 17:17:54 +0400

    select css

--- vendor/btybug/framework/src/Resources/Views/css/_partials/select.blade.php
+++ vendor/btybug/framework/src/Resources/Views/css/_partials/select.blade.php
@@ -75,6 +75,218 @@
         </div>
     </div>
     <div class="col-md-12">
+        <div class="col-md-6 p-t-34">
+            <div class="bty-input-select-dark-1">
+                <select>
+                    <option>Choose Option</option>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                    <option>Option 4</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h5>bty-input-select-dark-1</h5>
+            <textarea class="form-control" name="" id="" cols="30" rows="5" readonly>
+.bty-input-select-dark-1 select {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    -ms-appearance: none;
+    appearance: none;
+    outline: 0;
+    box-shadow: none;
+    border: 0 !important;
+    background: #2c3e50;
+    background-image: none;
+}
+
+.bty-input-select-dark-1 {
+    position: relative;
+    display: block;
+    width: 100%;
+    line-height: 3
+}
+
+.bty-input-select-dark-1 select {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0 0 0 .5em;
+    color: #fff;
+    cursor: pointer;
+}
+
+.bty-input-select-dark-1 select::-ms-expand {
+    display: none;
+}
+
+.bty-input-select-dark-1::after {
+    content: '\25BC';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    padding: 0 1em;
+    background: #34495e;
+    pointer-events: none;
+    color: white;
+    border-left: 1px solid;
+}
+
+.bty-input-select-dark-1:hover::after {
+    color: #f39c12;
+}
+
+.bty-input-select-dark-1::after {
+    -webkit-transition: .25s all ease;
+    -o-transition: .25s all ease;
+    transition: .25s all ease;
+}
+            </textarea>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="col-md-6 p-t-34">
+            <div class="bty-input-select-option-1">
+                <select>
+                    <option>Choose Option</option>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                    <option>Option 4</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h5>bty-input-select-option-1</h5>
+            <textarea class="form-control" name="" id="" cols="30" rows="5" readonly>
+
+.bty-input-select-option-1 select {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    -ms-appearance: none;
+    appearance: none;
+    outline: 0;
+    box-shadow: none;
+    border: 0 !important;
+    background: transparent;
+    background-image: none;
+}
+
+.bty-input-select-option-1 {
+    position: relative;
+    display: block;
+    width: 100%;
+    line-height: 3;
+    background: #2c3e50;
+}
+
+.bty-input-select-option-1 select {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0 0 0 .5em;
+    color: #fff;
+    cursor: pointer;
+}
+.bty-input-select-option-1 select option{
+    color: black;
+}
+.bty-input-select-option-1 select::-ms-expand {
+    display: none;
+}
+
+.bty-input-select-option-1::after {
+    content: '\25BC';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    padding: 0 1em;
+    background: #34495e;
+    pointer-events: none;
+    color: white;
+    border-left: 1px solid;
+}
+
+.bty-input-select-option-1:hover::after {
+    color: #fff;
+}
+
+.bty-input-select-option-1::after {
+    -webkit-transition: .25s all ease;
+    -o-transition: .25s all ease;
+    transition: .25s all ease;
+}
+            </textarea>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="col-md-6 p-t-34">
+            <div class="bty-input-select-light-1">
+                <select>
+                    <option>Choose Option</option>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                    <option>Option 3</option>
+                    <option>Option 4</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h5>bty-input-select-light-1</h5>
+            <textarea class="form-control" name="" id="" cols="30" rows="5" readonly>
+
+.bty-input-select-light-1 select {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    -ms-appearance: none;
+    appearance: none;
+    outline: 0;
+    box-shadow: none;
+    border: 0 !important;
+    background: transparent;
+    background-image: none;
+}
+
+.bty-input-select-light-1 {
+    position: relative;
+    display: block;
+    width: 100%;
+    line-height: 3;
+}
+
+.bty-input-select-light-1 select {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0 0 0 .5em;
+    color: #555;
+    cursor: pointer;
+    background-color: #fff;
+    border-radius: 4px;
+}
+.bty-input-select-light-1 select option{
+    color: black;
+}
+.bty-input-select-light-1 select::-ms-expand {
+    display: none;
+}
+
+.bty-input-select-light-1::after {
+    content: '\25BC';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    padding: 0 1em;
+    pointer-events: none;
+}
+            </textarea>
+        </div>
+    </div>
+    <div class="col-md-12">
         <div class="col-md-6">
             <h4>Input select 1</h4>
             <h5>bty-input-select-1</h5>

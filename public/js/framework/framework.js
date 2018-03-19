584cd6d135e3e18841884c99d8ceafbb6e7af383
midoalone <mido_alone2001@yahoo.com>
2018-03-19 23:25:16 +0200

    framework

--- public/js/framework/framework.js
+++ public/js/framework/framework.js
@@ -1,10 +1,22 @@
+var nodeCodeEditor;
+
 var framework = {
+
+    globalIndex: 0,
+
+    // Code wallet
+    codeWallet: [],
 
     // Generate node tree
     nodeTreeGenerator: function (node) {
         var nodeEl = node[0];
-        var output = '<li>';
+        var output = '<li data-index="' + this.globalIndex + '">';
         output += '<div class="node-content">' + nodeEl.tagName + this.parseTemplate('controls') + '</div>';
+
+        this.codeWallet.push(node);
+
+        // Increase index
+        this.globalIndex++;
 
         if(node.children().length > 0){
             output += '<ul>';
@@ -32,9 +44,23 @@ var framework = {
         return templateHTML;
     },
 
+    // Set node code editor value
+    setNodeCodeValue: function ($this) {
+        var index = $this.closest("[data-index]").data("index"),
+            nodeCode = this.codeWallet[index];
+
+        nodeCodeEditor.setValue(nodeCode[0].outerHTML);
+    },
+
     // Click events
     clickEvents: {
         editCode: function ($this) {
+
+            if($('#node-code-editor').length !== 0) {
+                framework.setNodeCodeValue($this);
+                return;
+            }
+
             jsPanel.create({
                 id: 'code-editor-panel',
                 container: 'body',
@@ -44,9 +70,11 @@ var framework = {
                 contentSize: '500 250',
                 content: '<div id="node-code-editor"></div>',
                 callback: function () {
-                    var nodeCodeEditor = ace.edit("node-code-editor");
+                    nodeCodeEditor = ace.edit("node-code-editor");
                     nodeCodeEditor.setTheme("ace/theme/monokai");
                     nodeCodeEditor.session.setMode("ace/mode/html");
+
+                    framework.setNodeCodeValue($this);
                 },
                 onclosed: function () {
 
@@ -59,6 +87,10 @@ var framework = {
 $(function () {
     // Listen to code change
     codeEditor.session.on('change', function() {
+        // Reset code wallet & global index
+        framework.codeWallet = [];
+        framework.globalIndex = 0;
+        
         var codeContent = codeEditor.getValue();
         var treeList = framework.nodeTreeGenerator($(codeContent));
         $('.tree-list').html(treeList);

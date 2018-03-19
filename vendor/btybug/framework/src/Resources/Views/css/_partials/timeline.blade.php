f0872fc176987544b097452314c94a930ab62096
sahakavatar <hakobyan.sahak88@gmail.com>
2018-03-19 15:49:47 +0400

    timeline css

--- vendor/btybug/framework/src/Resources/Views/css/_partials/timeline.blade.php
+++ vendor/btybug/framework/src/Resources/Views/css/_partials/timeline.blade.php
@@ -188,63 +188,407 @@
         </div>
     </div>
     <div class="col-md-12">
-        <h4>Timeline 1</h4>
-        <h5>bty-timeline-1</h5>
-        <div class="bty-timeline-1">
-            <article>
-                <div>
+        <div class="col-md-6 p-t-34">
+            <div class="bty-timeline-dark-1">
+                <article>
                     <div>
                         <div>
-                            <span></span>
-                            <span></span>
-                            <span>00:00 am</span>
-                            <h6>12 June | Monday</h6>
-                            <p>Lorem ipsum dolor sit amet consiquest dio</p>
+                            <div>
+                                <span></span>
+                                <span></span>
+                                <span>00:00 am</span>
+                                <h6>12 June | Monday</h6>
+                                <p>Lorem ipsum dolor sit amet consiquest dio</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </article>
-            <article>
-                <div>
+                </article>
+                <article>
                     <div>
                         <div>
-                            <span></span>
-                            <span></span>
-                            <span>00:00 am</span>
-                            <h6>12 June | Monday</h6>
-                            <p>Lorem ipsum dolor sit amet consiquest dio</p>
+                            <div>
+                                <span></span>
+                                <span></span>
+                                <span>00:00 am</span>
+                                <h6>12 June | Monday</h6>
+                                <p>Lorem ipsum dolor sit amet consiquest dio</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </article>
-            <article>
-                <div>
+                </article>
+                <article>
                     <div>
                         <div>
-                            <span></span>
-                            <span></span>
-                            <span>00:00 am</span>
-                            <h6>12 June | Monday</h6>
-                            <p>Lorem ipsum dolor sit amet consiquest dio</p>
+                            <div>
+                                <span></span>
+                                <span></span>
+                                <span>00:00 am</span>
+                                <h6>12 June | Monday</h6>
+                                <p>Lorem ipsum dolor sit amet consiquest dio</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </article>
-            <article>
-                <div>
+                </article>
+                <article>
                     <div>
                         <div>
-                            <span></span>
-                            <span></span>
-                            <span>00:00 am</span>
-                            <h6>12 June | Monday</h6>
-                            <p>Lorem ipsum dolor sit amet consiquest dio</p>
+                            <div>
+                                <span></span>
+                                <span></span>
+                                <span>00:00 am</span>
+                                <h6>12 June | Monday</h6>
+                                <p>Lorem ipsum dolor sit amet consiquest dio</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </article>
+                </article>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h5>bty-timeline-dark-1</h5>
+            <textarea class="form-control" name="" id="" cols="30" rows="5" readonly>
+.bty-timeline-dark-1{
+    border-collapse: collapse;
+    border-spacing: 0;
+    display: table;
+    position: relative;
+    table-layout: fixed;
+    width: 100%;
+    padding: 20px;
+}
+.bty-timeline-dark-1:before{
+    background-color: #C7CBD6;
+    bottom: 0;
+    content: "";
+    left: 50%;
+    position: absolute;
+    top: 0;
+    width: 2px;
+    z-index: 0;
+}
+.bty-timeline-dark-1 article{
+    display: table-row;
+}
+.bty-timeline-dark-1 article:before, .bty-timeline-dark-1 article:after{
+    content: "";
+    display: block;
+    width: 50%;
+}
+.bty-timeline-dark-1 article>div{
+    vertical-align: top;
+    width: 50%;
+}
+.bty-timeline-dark-1 article>div>div{
+    display: block;
+    margin-left: 25px;
+    position: relative;
+    text-align: left;
+    background: #656565;
+    margin-bottom: 5px;
+    border-radius: 4px;
+    box-shadow: 0 0 5px #555;
+}
+.bty-timeline-dark-1 article>div>div>div{
+    padding: 15px;
+}
+.bty-timeline-dark-1 article>div>div>div:before, .bty-timeline-dark-1 article>div>div>div:after{
+    display: table;
+    content: " ";
+    clear: both;
+}
+.bty-timeline-dark-1 article>div>div>div span:nth-of-type(1){
+    border-bottom: 8px solid transparent;
+    border-top: 8px solid transparent;
+    display: block;
+    height: 0;
+    left: -7px;
+    position: absolute;
+    top: 13px;
+    width: 0;
+    border-right: 8px solid #656565  !important;
+}
+.bty-timeline-dark-1 article>div>div>div span:nth-of-type(2){
+    background: #656565;
+    box-shadow: 0 0 0 3px #656565;
+    border: 3px solid #FFFFFF;
+    border-radius: 50%;
+    -webkit-border-radius: 50%;
+    display: block;
+    height: 12px;
+    width: 12px;
+    left: -30px;
+    position: absolute;
+    top: 15px;
+}
+.bty-timeline-dark-1 article>div>div>div span:nth-of-type(3){
+    left: -200px;
+    position: absolute;
+    text-align: right;
+    top: 12px;
+    width: 150px;
+}
+.bty-timeline-dark-1 article>div>div>div h6{
+    font-size: 16px;
+    font-weight: 300;
+    margin: 0 0 5px;
+    color: #fff;
+}
+.bty-timeline-dark-1 article>div>div>div p{
+    font-size: 12px;
+    margin-bottom: 0;
+    color: #eeeeee94;
+}
+.bty-timeline-dark-1 article:nth-of-type(odd)>div{
+    float: right;
+}
+.bty-timeline-dark-1 article:nth-of-type(even)>div>div{
+    margin-left: 0;
+    margin-right: 25px;
+}
+.bty-timeline-dark-1 article:nth-of-type(even)>div>div>div span:nth-of-type(1){
+    border-bottom: 8px solid transparent;
+    border-top: 8px solid transparent;
+    display: block;
+    height: 0;
+    right: -7px;
+    position: absolute;
+    top: 13px;
+    width: 0;
+    left: auto;
+    border-left: 8px solid #656565 !important;
+    border-right: 0 !important;
+}
+.bty-timeline-dark-1 article:nth-of-type(even)>div>div>div span:nth-of-type(2){
+    left: auto;
+    right: -32px;
+}
+.bty-timeline-dark-1 article:nth-of-type(even)>div>div>div span:nth-of-type(3){
+    left: auto;
+    right: -200px;
+    text-align: left;
+}
+            </textarea>
         </div>
     </div>
+    <div class="col-md-12">
+        <div class="col-md-6 p-t-34">
+            <div class="bty-timeline-light-1">
+                <article>
+                    <div>
+                        <div>
+                            <div>
+                                <span></span>
+                                <span></span>
+                                <span>00:00 am</span>
+                                <h6>12 June | Monday</h6>
+                                <p>Lorem ipsum dolor sit amet consiquest dio</p>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <div>
+                        <div>
+                            <div>
+                                <span></span>
+                                <span></span>
+                                <span>00:00 am</span>
+                                <h6>12 June | Monday</h6>
+                                <p>Lorem ipsum dolor sit amet consiquest dio</p>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <div>
+                        <div>
+                            <div>
+                                <span></span>
+                                <span></span>
+                                <span>00:00 am</span>
+                                <h6>12 June | Monday</h6>
+                                <p>Lorem ipsum dolor sit amet consiquest dio</p>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <div>
+                        <div>
+                            <div>
+                                <span></span>
+                                <span></span>
+                                <span>00:00 am</span>
+                                <h6>12 June | Monday</h6>
+                                <p>Lorem ipsum dolor sit amet consiquest dio</p>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h5>bty-timeline-light-1</h5>
+            <textarea class="form-control" name="" id="" cols="30" rows="5" readonly>
+.bty-timeline-light-1{
+    border-collapse: collapse;
+    border-spacing: 0;
+    display: table;
+    position: relative;
+    table-layout: fixed;
+    width: 100%;
+    padding: 20px;
+}
+.bty-timeline-light-1:before{
+    background-color: #C7CBD6;
+    bottom: 0;
+    content: "";
+    left: 50%;
+    position: absolute;
+    top: 0;
+    width: 2px;
+    z-index: 0;
+}
+.bty-timeline-light-1 article{
+    display: table-row;
+}
+.bty-timeline-light-1 article:before, .bty-timeline-light-1 article:after{
+    content: "";
+    display: block;
+    width: 50%;
+}
+.bty-timeline-light-1 article>div{
+    /*display: table-cell;*/
+    vertical-align: top;
+    width: 50%;
+}
+.bty-timeline-light-1 article>div>div{
+    display: block;
+    margin-left: 25px;
+    position: relative;
+    text-align: left;
+    background: #F4F4F4;
+    margin-bottom: 5px;
+    border-radius: 4px;
+    box-shadow: 0 0 4px #999;
+}
+.bty-timeline-light-1 article>div>div>div{
+    padding: 15px;
+}
+.bty-timeline-light-1 article>div>div>div:before, .bty-timeline-light-1 article>div>div>div:after{
+    display: table;
+    content: " ";
+    clear: both;
+}
+.bty-timeline-light-1 article>div>div>div span:nth-of-type(1){
+    border-bottom: 8px solid transparent;
+    border-top: 8px solid transparent;
+    display: block;
+    height: 0;
+    left: -7px;
+    position: absolute;
+    top: 13px;
+    width: 0;
+    border-right: 8px solid #F4F4F4 !important;
+}
+.bty-timeline-light-1 article>div>div>div span:nth-of-type(2){
+    background: #EF6F66;
+    box-shadow: 0 0 0 3px #EF6F66;
+    border: 3px solid #FFFFFF;
+    border-radius: 50%;
+    -webkit-border-radius: 50%;
+    display: block;
+    height: 12px;
+    width: 12px;
+    left: -30px;
+    position: absolute;
+    top: 15px;
+}
+.bty-timeline-light-1 article>div>div>div span:nth-of-type(3){
+    left: -119px;
+    position: absolute;
+    text-align: right;
+    top: 8px;
+    background-color: #ef6f66;
+    padding: 2px 8px;
+    color: white;
+    border-radius: 5px;
+}
+.bty-timeline-light-1 article:nth-of-type(odd)>div>div>div span:nth-of-type(3):after{
+    content: "";
+    border-bottom: 6px solid transparent;
+    border-top: 6px solid transparent;
+    display: block;
+    height: 0;
+    right: -6px;
+    position: absolute;
+    top: 7px;
+    width: 0;
+    left: auto;
+    border-left: 6px solid #ef6f66 !important;
+    border-right: 0 !important;
+}
+.bty-timeline-light-1 article:nth-of-type(even)>div>div>div span:nth-of-type(3):after{
+    content: "";
+    border-bottom: 6px solid transparent;
+    border-top: 6px solid transparent;
+    display: block;
+    height: 0;
+    left: -6px;
+    position: absolute;
+    top: 7px;
+    width: 0;
+    border-right: 6px solid #39B6AE !important;
+}
+.bty-timeline-light-1 article>div>div>div h6{
+    font-size: 16px;
+    font-weight: 300;
+    margin: 0 0 5px;
+    color: #EF6F66;
+}
+.bty-timeline-light-1 article>div>div>div p{
+    font-size: 12px;
+    margin-bottom: 0;
+}
+.bty-timeline-light-1 article:nth-of-type(odd)>div{
+    float: right;
+}
+.bty-timeline-light-1 article:nth-of-type(even)>div>div{
+    margin-left: 0;
+    margin-right: 25px;
+}
+.bty-timeline-light-1 article:nth-of-type(even)>div>div>div span:nth-of-type(1){
+    border-bottom: 8px solid transparent;
+    border-top: 8px solid transparent;
+    display: block;
+    height: 0;
+    right: -7px;
+    position: absolute;
+    top: 13px;
+    width: 0;
+    left: auto;
+    border-left: 8px solid #F4F4F4 !important;
+    border-right: 0 !important;
+}
+.bty-timeline-light-1 article:nth-of-type(even)>div>div>div span:nth-of-type(2){
+    left: auto;
+    right: -32px;
+    background: #39B6AE;
+    box-shadow: 0 0 0 3px #39B6AE;
+}
+.bty-timeline-light-1 article:nth-of-type(even)>div>div>div span:nth-of-type(3){
+    left: auto;
+    right: -121px;
+    text-align: left;
+    background-color: #39b6ae;
+}
+.bty-timeline-light-1 article:nth-of-type(even)>div>div>div h6{
+    color: #39B6AE;
+}
+            </textarea>
+        </div>
+    </div>
+
     <div class="col-md-12">
         <h4>Timeline 2</h4>
         <h5>bty-timeline-2</h5>

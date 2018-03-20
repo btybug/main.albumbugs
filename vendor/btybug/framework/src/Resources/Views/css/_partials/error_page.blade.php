bdff66f8d2f8b45eb424aef649fe0b7845444a69
sahakavatar <hakobyan.sahak88@gmail.com>
2018-03-20 11:24:19 +0400

    404 css

--- vendor/btybug/framework/src/Resources/Views/css/_partials/error_page.blade.php
+++ vendor/btybug/framework/src/Resources/Views/css/_partials/error_page.blade.php
@@ -131,6 +131,634 @@
         </div>
     </div>
     <div class="col-md-12">
+        <div class="col-md-6 p-t-34">
+            <section class="bty-page-error-dark-1">
+                <div>
+                    <p>404<br>
+                        <small>PAGE NOT FOUND</small>
+                    </p>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </section>
+        </div>
+        <div class="col-md-6">
+            <h5>bty-page-error-dark-1</h5>
+            <textarea class="form-control" name="" id="" cols="30" rows="5" readonly>
+
+.bty-page-error-dark-1 > div {
+    background: #00000030;
+    text-align: center;
+    position: relative;
+}
+
+.bty-page-error-dark-1 > div p {
+    font-size: 240px;
+    color: #fff;
+    padding-top: 60px;
+    position: relative;
+    line-height: 100%;
+}
+
+.bty-page-error-dark-1 > div p small {
+    font-size: 40px;
+    line-height: 100%;
+    vertical-align: top;
+}
+
+.bty-page-error-dark-1 > div span:nth-of-type(3) {
+    width: 140px;
+    height: 140px;
+    border-radius: 50%;
+    background: #171717;
+    position: absolute;
+    z-index: 1;
+    top: 80px;
+    left: 50%;
+    animation: 7s small-bty-page-error-dark-1 infinite cubic-bezier(1, .22, .71, .98);
+    -webkit-animation: 7s small-bty-page-error-dark-1 infinite cubic-bezier(1, .22, .71, .98);
+    animation-delay: 1.2s;
+    -webkit-animation-delay: 1.2s;
+}
+
+.bty-page-error-dark-1 > div span:nth-of-type(2) {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background: #171717;
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    left: 10%;
+    animation: 7s med-bty-page-error-dark-1 infinite cubic-bezier(.32, .04, .15, .75);
+    -webkit-animation: 7s med-bty-page-error-dark-1 infinite cubic-bezier(.32, .04, .15, .75);
+    animation-delay: 0.4s;
+    -webkit-animation-delay: 0.4s;
+}
+
+.bty-page-error-dark-1 > div span:nth-of-type(1) {
+    width: 250px;
+    height: 250px;
+    border-radius: 50%;
+    background: #171717;
+    position: absolute;
+    z-index: 1;
+    top: 200px;
+    right: 0;
+    animation: 8s big-bty-page-error-dark-1 infinite;
+    -webkit-animation: 8s big-bty-page-error-dark-1 infinite;
+    animation-delay: 3s;
+    -webkit-animation-delay: 1s;
+}
+
+@-webkit-keyframes small-bty-page-error-dark-1 {
+    0% {
+        top: 10px;
+        left: 45%;
+        opacity: 1;
+    }
+    25% {
+        top: 300px;
+        left: 40%;
+        opacity: 0.7;
+    }
+    50% {
+        top: 240px;
+        left: 55%;
+        opacity: 0.4;
+    }
+    75% {
+        top: 100px;
+        left: 40%;
+        opacity: 0.6;
+    }
+    100% {
+        top: 10px;
+        left: 45%;
+        opacity: 1;
+    }
+}
+
+@keyframes small-bty-page-error-dark-1 {
+    0% {
+        top: 10px;
+        left: 45%;
+        opacity: 1;
+    }
+    25% {
+        top: 300px;
+        left: 40%;
+        opacity: 0.7;
+    }
+    50% {
+        top: 240px;
+        left: 55%;
+        opacity: 0.4;
+    }
+    75% {
+        top: 100px;
+        left: 40%;
+        opacity: 0.6;
+    }
+    100% {
+        top: 10px;
+        left: 45%;
+        opacity: 1;
+    }
+}
+
+@-webkit-keyframes med-bty-page-error-dark-1 {
+    0% {
+        top: 0;
+        left: 20%;
+        opacity: 1;
+    }
+    25% {
+        top: 300px;
+        left: 80%;
+        opacity: 0.7;
+    }
+    50% {
+        top: 240px;
+        left: 55%;
+        opacity: 0.4;
+    }
+    75% {
+        top: 100px;
+        left: 40%;
+        opacity: 0.6;
+    }
+    100% {
+        top: 0;
+        left: 20%;
+        opacity: 1;
+    }
+}
+
+@keyframes med-bty-page-error-dark-1 {
+    0% {
+        top: 0;
+        left: 20%;
+        opacity: 1;
+    }
+    25% {
+        top: 300px;
+        left: 80%;
+        opacity: 0.7;
+    }
+    50% {
+        top: 240px;
+        left: 55%;
+        opacity: 0.4;
+    }
+    75% {
+        top: 100px;
+        left: 40%;
+        opacity: 0.6;
+    }
+    100% {
+        top: 0;
+        left: 20%;
+        opacity: 1;
+    }
+}
+
+@-webkit-keyframes big-bty-page-error-dark-1 {
+    0% {
+        top: 0;
+        right: 4%;
+        opacity: 0.5;
+    }
+    25% {
+        top: 100px;
+        right: 40%;
+        opacity: 0.4;
+    }
+    50% {
+        top: 240px;
+        right: 45%;
+        opacity: 0.8;
+    }
+    75% {
+        top: 100px;
+        right: 35%;
+        opacity: 0.6;
+    }
+    100% {
+        top: 0;
+        right: 4%;
+        opacity: 0.5;
+    }
+}
+
+@keyframes big-bty-page-error-dark-1 {
+    0% {
+        top: 0;
+        right: 4%;
+        opacity: 0.5;
+    }
+    25% {
+        top: 100px;
+        right: 40%;
+        opacity: 0.4;
+    }
+    50% {
+        top: 240px;
+        right: 45%;
+        opacity: 0.8;
+    }
+    75% {
+        top: 100px;
+        right: 35%;
+        opacity: 0.6;
+    }
+    100% {
+        top: 0;
+        right: 4%;
+        opacity: 0.5;
+    }
+}
+            </textarea>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="col-md-6 p-t-34">
+            <section class="bty-page-error-star-1">
+                <div>
+                    <p>404<br>
+                        <small>PAGE NOT FOUND</small>
+                    </p>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </section>
+        </div>
+        <div class="col-md-6">
+            <h5>bty-page-error-star-1</h5>
+            <textarea class="form-control" name="" id="" cols="30" rows="5" readonly>
+.bty-page-error-star-1 > div {
+    background: #cc0b134f;
+    text-align: center;
+    position: relative;
+}
+
+.bty-page-error-star-1 > div p {
+    font-size: 240px;
+    color: #fff;
+    padding-top: 60px;
+    position: relative;
+    z-index: 9;
+    line-height: 100%;
+    text-shadow: 2px 2px 7px #666;
+}
+
+.bty-page-error-star-1 > div p small {
+    font-size: 40px;
+    line-height: 100%;
+    vertical-align: top;
+}
+
+.bty-page-error-star-1 > div span:nth-of-type(3) {
+    top: 80px;
+    left: 50%;
+    position: absolute;
+    z-index: 1;
+    right: 0;
+    display: block;
+    color: red;
+    width: 0;
+    height: 0;
+    border-right: 70px solid transparent;
+    border-bottom: 60px solid red;
+    border-left: 80px solid transparent;
+    -moz-transform: rotate(35deg);
+    -webkit-transform: rotate(35deg);
+    -ms-transform: rotate(35deg);
+    -o-transform: rotate(35deg);
+    animation: 7s small-bty-page-error-star-1 infinite cubic-bezier(1, .22, .71, .98);
+    -webkit-animation: 7s small-bty-page-error-star-1 infinite cubic-bezier(1, .22, .71, .98);
+    animation-delay: 1.2s;
+    -webkit-animation-delay: 1.2s;
+}
+.bty-page-error-star-1 > div span:nth-of-type(3):before{
+    border-bottom: 55px solid red;
+    border-left: 23px solid transparent;
+    border-right: 24px solid transparent;
+    position: absolute;
+    height: 0;
+    width: 0;
+    top: -32px;
+    left: -57px;
+    display: block;
+    content: "";
+    -webkit-transform: rotate(-35deg);
+    -moz-transform: rotate(-35deg);
+    -ms-transform: rotate(-35deg);
+    -o-transform: rotate(-35deg);
+}
+.bty-page-error-star-1 > div span:nth-of-type(3):after{
+    content: "";
+    position: absolute;
+    display: block;
+    color: red;
+    top: 6px;
+    left: -86px;
+    width: 0;
+    height: 0;
+    border-right: 70px solid transparent;
+    border-bottom: 60px solid red;
+    border-left: 80px solid transparent;
+    -webkit-transform: rotate(-70deg);
+    -moz-transform: rotate(-70deg);
+    -ms-transform: rotate(-70deg);
+    -o-transform: rotate(-70deg);
+}
+
+.bty-page-error-star-1 > div span:nth-of-type(2) {
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    left: 10%;
+    right: 0;
+    display: block;
+    color: red;
+    width: 0;
+    height: 0;
+    border-right: 85px solid transparent;
+    border-bottom: 65px solid red;
+    border-left: 85px solid transparent;
+    -moz-transform: rotate(35deg);
+    -webkit-transform: rotate(35deg);
+    -ms-transform: rotate(35deg);
+    -o-transform: rotate(35deg);
+    animation: 7s med-bty-page-error-star-1 infinite cubic-bezier(.32, .04, .15, .75);
+    -webkit-animation: 7s med-bty-page-error-star-1 infinite cubic-bezier(.32, .04, .15, .75);
+    animation-delay: 0.4s;
+    -webkit-animation-delay: 0.4s;
+}
+.bty-page-error-star-1 > div span:nth-of-type(2):after{
+    content: "";
+    position: absolute;
+    display: block;
+    color: red;
+    top: 6px;
+    left: -96px;
+    width: 0;
+    height: 0;
+    border-right: 85px solid transparent;
+    border-bottom: 65px solid red;
+    border-left: 85px solid transparent;
+    -webkit-transform: rotate(-70deg);
+    -moz-transform: rotate(-70deg);
+    -ms-transform: rotate(-70deg);
+    -o-transform: rotate(-70deg);
+}
+.bty-page-error-star-1 > div span:nth-of-type(2):before{
+    border-bottom: 66px solid red;
+    border-left: 26px solid transparent;
+    border-right: 29px solid transparent;
+    position: absolute;
+    height: 0;
+    width: 0;
+    top: -39px;
+    left: -64px;
+    display: block;
+    content: "";
+    -webkit-transform: rotate(-35deg);
+    -moz-transform: rotate(-35deg);
+    -ms-transform: rotate(-35deg);
+    -o-transform: rotate(-35deg);
+}
+.bty-page-error-star-1 > div span:nth-of-type(1) {
+    position: absolute;
+    z-index: 1;
+    top: 86px;
+    right: 0;
+    margin: 50px 0;
+    display: block;
+    color: red;
+    width: 0;
+    height: 0;
+    border-right:  100px solid transparent;
+    border-bottom: 70px  solid red;
+    border-left:   100px solid transparent;
+    -moz-transform:    rotate(35deg);
+    -webkit-transform: rotate(35deg);
+    -ms-transform:     rotate(35deg);
+    -o-transform:      rotate(35deg);
+    animation: 8s big-bty-page-error-star-1 infinite;
+    -webkit-animation: 8s big-bty-page-error-star-1 infinite;
+    animation-delay: 3s;
+    -webkit-animation-delay: 1s;
+}
+.bty-page-error-star-1 > div span:nth-of-type(1):before{
+    border-bottom: 80px solid red;
+    border-left: 30px solid transparent;
+    border-right: 30px solid transparent;
+    position: absolute;
+    height: 0;
+    width: 0;
+    top: -45px;
+    left: -65px;
+    display: block;
+    content: "";
+    -webkit-transform: rotate(-35deg);
+    -moz-transform:    rotate(-35deg);
+    -ms-transform:     rotate(-35deg);
+    -o-transform:      rotate(-35deg);
+}
+.bty-page-error-star-1 > div span:nth-of-type(1):after{
+    position: absolute;
+    display: block;
+    color: red;
+    top: 3px;
+    left: -105px;
+    width: 0;
+    height: 0;
+    border-right: 100px solid transparent;
+    border-bottom: 70px solid red;
+    border-left: 100px solid transparent;
+    -webkit-transform: rotate(-70deg);
+    -moz-transform:    rotate(-70deg);
+    -ms-transform:     rotate(-70deg);
+    -o-transform:      rotate(-70deg);
+    content: "";
+}
+
+@-webkit-keyframes small-bty-page-error-star-1 {
+    0% {
+        top: 10px;
+        left: 45%;
+        opacity: 1;
+    }
+    25% {
+        top: 300px;
+        left: 40%;
+        opacity: 0.7;
+    }
+    50% {
+        top: 240px;
+        left: 55%;
+        opacity: 0.4;
+    }
+    75% {
+        top: 100px;
+        left: 40%;
+        opacity: 0.6;
+    }
+    100% {
+        top: 10px;
+        left: 45%;
+        opacity: 1;
+    }
+}
+
+@keyframes small-bty-page-error-star-1 {
+    0% {
+        top: 10px;
+        left: 45%;
+        opacity: 1;
+    }
+    25% {
+        top: 300px;
+        left: 40%;
+        opacity: 0.7;
+    }
+    50% {
+        top: 240px;
+        left: 55%;
+        opacity: 0.4;
+    }
+    75% {
+        top: 100px;
+        left: 40%;
+        opacity: 0.6;
+    }
+    100% {
+        top: 10px;
+        left: 45%;
+        opacity: 1;
+    }
+}
+
+@-webkit-keyframes med-bty-page-error-star-1 {
+    0% {
+        top: 0;
+        left: 20%;
+        opacity: 1;
+    }
+    25% {
+        top: 300px;
+        left: 80%;
+        opacity: 0.7;
+    }
+    50% {
+        top: 240px;
+        left: 55%;
+        opacity: 0.4;
+    }
+    75% {
+        top: 100px;
+        left: 40%;
+        opacity: 0.6;
+    }
+    100% {
+        top: 0;
+        left: 20%;
+        opacity: 1;
+    }
+}
+
+@keyframes med-bty-page-error-star-1 {
+    0% {
+        top: 0;
+        left: 20%;
+        opacity: 1;
+    }
+    25% {
+        top: 300px;
+        left: 80%;
+        opacity: 0.7;
+    }
+    50% {
+        top: 240px;
+        left: 55%;
+        opacity: 0.4;
+    }
+    75% {
+        top: 100px;
+        left: 40%;
+        opacity: 0.6;
+    }
+    100% {
+        top: 0;
+        left: 20%;
+        opacity: 1;
+    }
+}
+
+@-webkit-keyframes big-bty-page-error-star-1 {
+    0% {
+        top: 0;
+        right: 4%;
+        opacity: 0.5;
+    }
+    25% {
+        top: 100px;
+        right: 40%;
+        opacity: 0.4;
+    }
+    50% {
+        top: 240px;
+        right: 45%;
+        opacity: 0.8;
+    }
+    75% {
+        top: 100px;
+        right: 35%;
+        opacity: 0.6;
+    }
+    100% {
+        top: 0;
+        right: 4%;
+        opacity: 0.5;
+    }
+}
+
+@keyframes big-bty-page-error-star-1 {
+    0% {
+        top: 0;
+        right: 4%;
+        opacity: 0.5;
+    }
+    25% {
+        top: 100px;
+        right: 40%;
+        opacity: 0.4;
+    }
+    50% {
+        top: 240px;
+        right: 45%;
+        opacity: 0.8;
+    }
+    75% {
+        top: 100px;
+        right: 35%;
+        opacity: 0.6;
+    }
+    100% {
+        top: 0;
+        right: 4%;
+        opacity: 0.5;
+    }
+}
+
+            </textarea>
+        </div>
+    </div>
+    <div class="col-md-12">
         <h4>404 page error 1</h4>
         <h5>bty-page-error-1</h5>
         <section class="bty-page-error-1">

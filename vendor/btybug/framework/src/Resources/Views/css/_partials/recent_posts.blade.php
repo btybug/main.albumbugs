23aaa30ae21efc718d6ad3d14c0894ddea39d7d7
sahakavatar <hakobyan.sahak88@gmail.com>
2018-03-20 12:43:11 +0400

    recent post css

--- vendor/btybug/framework/src/Resources/Views/css/_partials/recent_posts.blade.php
+++ vendor/btybug/framework/src/Resources/Views/css/_partials/recent_posts.blade.php
@@ -120,10 +120,8 @@
         </div>
     </div>
     <div class="col-md-12">
-        <h4>Recent Post For sidebar 1</h4>
-        <h5>bty-recent-post-1</h5>
-        <div class="col-md-5">
-            <div class="bty-recent-post-1">
+        <div class="col-md-6 p-t-34">
+            <div class="bty-recent-post-img-1">
                 <h3>Recent Post</h3>
                 <ul>
                     <li>
@@ -168,8 +166,221 @@
                 </ul>
             </div>
         </div>
-        <div class="col-md-3"></div>
-        <div class="col-md-4"></div>
+        <div class="col-md-6">
+            <h5>bty-recent-post-img-1</h5>
+            <textarea class="form-control" name="" id="" cols="30" rows="5" readonly>
+.bty-recent-post-img-1 {
+    background: #ffffff;
+    border: 1px solid #ddd;
+    padding: 10px 20px;
+    box-shadow: 0 0 4px #888;
+}
+
+.bty-recent-post-img-1 h3 {
+    color: #499bc7;
+    margin: 10px 0 15px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    border-left: 5px solid #32aae1;
+}
+
+.bty-recent-post-img-1 ul {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    list-style: none;
+}
+
+.bty-recent-post-img-1 ul li {
+    overflow: hidden;
+    font-size: 14px;
+    margin-bottom: 20px;
+    border-bottom: 1px dashed #ddd;
+    padding-bottom: 20px
+}
+
+.bty-recent-post-img-1 ul li > div:nth-of-type(1) {
+    overflow: hidden;
+}
+
+.bty-recent-post-img-1 ul li > div:nth-of-type(1) img {
+    background: #fff;
+    border: 1px solid #dadada;
+    padding: 2px;
+    height: 145px;
+    width: 100%;
+    object-fit: cover;
+    box-shadow: inset 0 0 4px #000;
+}
+
+.bty-recent-post-img-1 ul li > div:nth-of-type(2) h5 {
+    font-size: 20px;
+    cursor: pointer;
+    line-height: 24px;
+}
+
+.bty-recent-post-img-1 ul li > div:nth-of-type(2) h5 a {
+    color: #202020;
+    outline: 0 none;
+    text-decoration: none;
+    font-weight: bold;
+    transition: 0.1s ease;
+}
+
+.bty-recent-post-img-1 ul li > div:nth-of-type(2) h5 a:hover {
+    color: #499bc7;
+}
+
+.bty-recent-post-img-1 ul li > div:nth-of-type(3) {
+    margin-top: 10px;
+    color: #2e2e2e;
+}
+
+.bty-recent-post-img-1 ul li > div:nth-of-type(3) span i {
+    color: #2996bd;
+}
+
+.bty-recent-post-img-1 ul li > div:nth-of-type(3) span:first-child {
+    margin-right: 10px;
+}
+            </textarea>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="col-md-6 p-t-34">
+            <div class="bty-recent-post-hover-1">
+                <h3>Recent Post</h3>
+                <ul>
+                    <li>
+                        <div>
+                            <img src="http://static6.businessinsider.com/image/5948376b9a7af5d1008b564e/gal-gadots-wonder-woman-salary-was-shockingly-low.jpg"
+                                 alt="">
+                        </div>
+                        <div>
+                            <h5><a href="#">Title lorem 1</a></h5>
+                        </div>
+                        <div>
+                            <span><i class="fa fa-clock-o"></i> Sep 27, 2017</span>
+                            <span><i class="fa fa-comment"></i> 5 comments</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <img src="http://static6.businessinsider.com/image/5948376b9a7af5d1008b564e/gal-gadots-wonder-woman-salary-was-shockingly-low.jpg"
+                                 alt="">
+                        </div>
+                        <div>
+                            <h5><a href="#">Title lorem 1</a></h5>
+                        </div>
+                        <div>
+                            <span><i class="fa fa-clock-o"></i> Sep 27, 2017</span>
+                            <span><i class="fa fa-comment"></i> 5 comments</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <img src="http://static6.businessinsider.com/image/5948376b9a7af5d1008b564e/gal-gadots-wonder-woman-salary-was-shockingly-low.jpg"
+                                 alt="">
+                        </div>
+                        <div>
+                            <h5><a href="#">Title lorem 1</a></h5>
+                        </div>
+                        <div>
+                            <span><i class="fa fa-clock-o"></i> Sep 27, 2017</span>
+                            <span><i class="fa fa-comment"></i> 5 comments</span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h5>bty-recent-post-hover-1</h5>
+            <textarea class="form-control" name="" id="" cols="30" rows="5" readonly>
+.bty-recent-post-hover-1 {
+    background: #ffffff;
+    padding: 10px 20px;
+    box-shadow: inset 2px 2px 4px #888, inset -2px -2px 4px #888;
+}
+
+.bty-recent-post-hover-1 h3 {
+    color: #000;
+    margin: 10px 0 15px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    border-left: 4px solid #000;
+}
+
+.bty-recent-post-hover-1 ul {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    list-style: none;
+}
+
+.bty-recent-post-hover-1 ul li {
+    overflow: hidden;
+    font-size: 14px;
+    margin-bottom: 20px;
+    border-bottom: 1px dashed #ddd;
+    padding-bottom: 20px;
+
+}
+.bty-recent-post-hover-1 ul li:hover img{
+    -webkit-filter: grayscale(100%);
+    filter: grayscale(100%);
+}
+
+.bty-recent-post-hover-1 ul li > div:nth-of-type(1) {
+    overflow: hidden;
+}
+
+.bty-recent-post-hover-1 ul li > div:nth-of-type(1) img {
+    background: #fff;
+    border: 1px solid #dadada;
+    padding: 2px;
+    height: 145px;
+    width: 100%;
+    object-fit: cover;
+    box-shadow: inset 0 0 4px #000;
+    -webkit-transition: all 0.5s ease;
+    -moz-transition:all 0.5s ease;
+    -ms-transition:all 0.5s ease;
+    -o-transition:all 0.5s ease;
+    transition:all 0.5s ease;
+}
+
+.bty-recent-post-hover-1 ul li > div:nth-of-type(2) h5 {
+    font-size: 20px;
+    cursor: pointer;
+    line-height: 24px;
+}
+
+.bty-recent-post-hover-1 ul li > div:nth-of-type(2) h5 a {
+    color: #8a8a8a;
+    outline: 0 none;
+    text-decoration: none;
+    font-weight: bold;
+    transition: 0.1s ease;
+}
+
+.bty-recent-post-hover-1 ul li > div:nth-of-type(2) h5 a:hover {
+    color: #444;
+}
+
+.bty-recent-post-hover-1 ul li > div:nth-of-type(3) {
+    margin-top: 10px;
+    color: #2e2e2e;
+}
+
+.bty-recent-post-hover-1 ul li > div:nth-of-type(3) span i {
+    color: #8a8a8a;
+}
+
+.bty-recent-post-hover-1 ul li > div:nth-of-type(3) span:first-child {
+    margin-right: 10px;
+}
+            </textarea>
+        </div>
     </div>
     <div class="col-md-12">
         <h4>Recent Post For sidebar 2</h4>

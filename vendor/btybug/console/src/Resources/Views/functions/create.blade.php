6cf8a329b620609ff26582f13c3b8ec8271d67ca
terakyan <edo.terakyan@gmail.com>
2018-03-21 11:07:57 +0400

    up

--- vendor/btybug/console/src/Resources/Views/functions/create.blade.php
+++ vendor/btybug/console/src/Resources/Views/functions/create.blade.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-md-4">
                 <label>Function name</label>
-                {!! Form::text('name',null,['class' => 'form-name', 'placeholder' => 'Form Name']) !!}
+                {!! Form::text('name',null,['class' => 'form-name', 'placeholder' => 'Fn Name']) !!}
             </div>
             <div class="col-md-8">
                 <button type="submit" class="form-save pull-right" bb-click="saveHTML">Save</button>

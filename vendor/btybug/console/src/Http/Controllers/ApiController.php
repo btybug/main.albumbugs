4c5574f4c82a2dac063501574a6bf01c0ff346a6
Sahak <hakobyan.sahak88@gmail.com>
2018-03-21 10:53:48 +0400

    up

---
+++ vendor/btybug/console/src/Http/Controllers/ApiController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Btybug\Console\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Btybug\btybug\Services\SettingsService;
+use Illuminate\Http\Request;
+use Btybug\btybug\Models\ContentLayouts\ContentLayouts;
+use Btybug\Console\Services\BackendService;
+use Btybug\Framework\Repository\VersionsRepository;
+use Btybug\btybug\Repositories\AdminsettingRepository;
+
+/**
+ * Class BackendController
+ * @package Btybug\Console\Http\Controllers
+ */
+class ApiController extends Controller
+{
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function getIndex()
+    {
+        return view('console::api.index');
+    }
+
+
+
+}

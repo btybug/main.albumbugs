6bc67969b0d0157dd287f3393addbef62349a79b
sahakavatar <hakobyan.sahak88@gmail.com>
2018-03-21 12:30:46 +0400

    css

--- vendor/btybug/console/src/Resources/Views/functions/create.blade.php
+++ vendor/btybug/console/src/Resources/Views/functions/create.blade.php
@@ -32,6 +32,44 @@
             <div class="form-group">
 
             </div>
+
+            <div class="cust-group">
+                <div class="form-group form-horizonta">
+                    <div class="col-md-5">
+                        <label class="col-md-4 control-label">Select column</label>
+                        <div class="col-md-8">
+                            <select name="" id="" class="form-control">
+                                <option value="">1</option>
+                                <option value="">2</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <label class="col-md-2 control-label">Value</label>
+                        <div class="col-md-5">
+                            <select name="" id="" class="form-control">
+                                <option value="">1</option>
+                                <option value="">2</option>
+                            </select>
+                        </div>
+                        <div class="col-md-5">
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <select name="" id="" class="form-control">
+                            <option value="">and</option>
+                            <option value="">or</option>
+                        </select>
+                    </div>
+                    <div class="col-md-1">
+                        <button class="btn btn-sm btn-danger pull-right"><i class=" fa fa-minus"></i></button>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <button class="btn btn-md btn-info pull-right"><i class=" fa fa-plus"></i></button>
+
+            </div>
         </div>
     </div>
 @stop
@@ -39,7 +77,10 @@
     {!! Html::style("public/css/form-builder/form-builder.css?m=m") !!}
     {!! HTML::style("public/css/bty.css") !!}
     <style>
-
+        .cust-group > .form-group {
+            box-shadow: 0 0 4px #ccc;
+            padding: 20px 0;
+        }
     </style>
 @stop
 @section('JS')

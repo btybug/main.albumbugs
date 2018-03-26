58e9f565031fb7792418381a834d74317d5af191
Sahak <hakobyan.sahak88@gmail.com>
2018-03-26 21:12:21 +0400

    up

--- resources/units/api_connection_settings/tpl.blade.php
+++ resources/units/api_connection_settings/tpl.blade.php
@@ -2,7 +2,7 @@
 <div id="exTab1" class="container">
     <ul  class="nav nav-pills">
         <li class="active">
-            <a  href="#general" data-toggle="tab">General</a>
+            <a  href="#general" data-toggle="tab" aria-expanded="true">General</a>
         </li>
         <li><a href="#panel" data-toggle="tab">Panel</a>
         </li>
@@ -12,7 +12,7 @@
         <div class="tab-pane active" id="general">
             <h3>General</h3>
         </div>
-        <div class="tab-pane active" id="panel">
+        <div class="tab-pane " id="panel">
             <h3>Panel</h3>
         </div>
     </div>

36ee4e7bd16cefb0af15a5bc52e22f35dbb5fa7d
Sahak <hakobyan.sahak88@gmail.com>
2018-03-26 21:07:57 +0400

    up

--- resources/units/api_connection_settings/tpl.blade.php
+++ resources/units/api_connection_settings/tpl.blade.php
@@ -1,1 +1,20 @@
-wefgsfsdfgsdfg
+
+<div id="exTab1" class="container">
+    <ul  class="nav nav-pills">
+        <li class="active">
+            <a  href="#general" data-toggle="tab">General</a>
+        </li>
+        <li><a href="#2a" data-toggle="tab">Using nav-pills</a>
+        </li>
+        <li><a href="#3a" data-toggle="tab">Applying clearfix</a>
+        </li>
+        <li><a href="#4a" data-toggle="tab">Background color</a>
+        </li>
+    </ul>
+
+    <div class="tab-content clearfix">
+        <div class="tab-pane active" id="general">
+            <h3>General</h3>
+        </div>
+    </div>
+</div>

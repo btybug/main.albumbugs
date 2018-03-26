c4d19a0984d698d2f26295535ef2b60b0276b776
Sahak <hakobyan.sahak88@gmail.com>
2018-03-26 21:10:09 +0400

    up

--- resources/units/api_connection_settings/tpl.blade.php
+++ resources/units/api_connection_settings/tpl.blade.php
@@ -4,17 +4,16 @@
         <li class="active">
             <a  href="#general" data-toggle="tab">General</a>
         </li>
-        <li><a href="#2a" data-toggle="tab">Using nav-pills</a>
-        </li>
-        <li><a href="#3a" data-toggle="tab">Applying clearfix</a>
-        </li>
-        <li><a href="#4a" data-toggle="tab">Background color</a>
+        <li><a href="#panel" data-toggle="tab">Panel</a>
         </li>
     </ul>
 
     <div class="tab-content clearfix">
         <div class="tab-pane active" id="general">
             <h3>General</h3>
+        </div>
+        <div class="tab-pane active" id="panel">
+            <h3>Panel</h3>
         </div>
     </div>
 </div>

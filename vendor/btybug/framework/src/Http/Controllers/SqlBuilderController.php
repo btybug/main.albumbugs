fdd994828d560c9cec6a031b0c193325690d4df6
terakyan <edo.terakyan@gmail.com>
2018-03-29 13:24:09 +0400

    at

--- vendor/btybug/framework/src/Http/Controllers/SqlBuilderController.php
+++ vendor/btybug/framework/src/Http/Controllers/SqlBuilderController.php
@@ -22,7 +22,7 @@ class SqlBuilderController extends Controller
     public function renderSql(Request $request)
     {
         try{
-            $result = \Db::query($request->query);
+            $result = \DB::select($request->query);
         }catch (\Exception $exception){
             return response()->json([
                 'error' => true,

c2db3c0fce204b77788ea127c1ff569eedcd3e43
sahakavatar <hakobyan.sahak88@gmail.com>
2018-03-31 22:53:25 +0400

    update

---
+++ resources/units/api_product_settings/tpl.blade.php
@@ -0,0 +1,47 @@
+
+@php
+    $params = \Request::route()->parameters();
+    $client=null;
+    $url='javascript::void(0)';
+    if(isset($params['param'])){
+        $clientId=$params['param'];
+        $productId=$params['id'];
+        $url=url('/oauth/clients/',$clientId);
+        $client=\Laravel\Passport\Client::find($clientId);
+
+          $appProductsRepository=new \Btybug\Uploads\Repository\AppProductRepository();
+
+    $product=$appProductsRepository->find($productId);
+    }
+
+@endphp
+
+
+<div class="tab">
+    <button class="tablinks" onclick="openCity(event, 'default')" id="defaultOpen">Default Options</button>
+    <button class="tablinks" onclick="openCity(event, 'data')">Data Manage</button>
+    <button class="tablinks" onclick="openCity(event, 'Tokyo')">Usage</button>
+</div>
+
+<div id="default" class="tabcontent">
+    <h3>Default Options</h3>
+    <p>Default options form</p>
+</div>
+
+<div id="data" class="tabcontent">
+    <h3>Data Manage</h3>
+    <div>
+        @if(View::exists($product->app->form_path))
+            @include($product->app->form_path)
+        @endif
+    </div>
+</div>
+
+<div id="Tokyo" class="tabcontent">
+    <h3>Documentation</h3>
+    <p>SDK/USAGE</p>
+</div>
+
+{!! BBstyle($_this->path.DS.'css/style.css') !!}
+{!! BBscript($_this->path.DS.'js'.DS.'custom.js') !!}
+
--- vendor/btybug/user/src/Resources/Views/apps/perm_tab.blade.php
+++ vendor/btybug/user/src/Resources/Views/apps/perm_tab.blade.php
@@ -1,1 +1,103 @@
-permissions
+<form class="form-horizontal">
+    <fieldset>
+
+        <!-- Form Name -->
+        <legend>Example Form Comming from app</legend>
+
+        <!-- Multiple Checkboxes -->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="checkboxes">Multiple Checkboxes</label>
+            <div class="col-md-4">
+                <div class="checkbox">
+                    <label for="checkboxes-0">
+                        <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">
+                        Option one
+                    </label>
+                </div>
+                <div class="checkbox">
+                    <label for="checkboxes-1">
+                        <input type="checkbox" name="checkboxes" id="checkboxes-1" value="2">
+                        Option two
+                    </label>
+                </div>
+            </div>
+        </div>
+
+        <!-- Select Basic -->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="selectbasic">Select Basic</label>
+            <div class="col-md-4">
+                <select id="selectbasic" name="selectbasic" class="form-control">
+                    <option value="1">Option one</option>
+                    <option value="2">Option two</option>
+                </select>
+            </div>
+        </div>
+
+        <!-- Multiple Radios (inline) -->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="radios">Inline Radios</label>
+            <div class="col-md-4">
+                <label class="radio-inline" for="radios-0">
+                    <input type="radio" name="radios" id="radios-0" value="1" checked="checked">
+                    1
+                </label>
+                <label class="radio-inline" for="radios-1">
+                    <input type="radio" name="radios" id="radios-1" value="2">
+                    2
+                </label>
+                <label class="radio-inline" for="radios-2">
+                    <input type="radio" name="radios" id="radios-2" value="3">
+                    3
+                </label>
+                <label class="radio-inline" for="radios-3">
+                    <input type="radio" name="radios" id="radios-3" value="4">
+                    4
+                </label>
+            </div>
+        </div>
+
+        <!-- Multiple Checkboxes (inline) -->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="checkboxes">Inline Checkboxes</label>
+            <div class="col-md-4">
+                <label class="checkbox-inline" for="checkboxes-0">
+                    <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1">
+                    1
+                </label>
+                <label class="checkbox-inline" for="checkboxes-1">
+                    <input type="checkbox" name="checkboxes" id="checkboxes-1" value="2">
+                    2
+                </label>
+                <label class="checkbox-inline" for="checkboxes-2">
+                    <input type="checkbox" name="checkboxes" id="checkboxes-2" value="3">
+                    3
+                </label>
+                <label class="checkbox-inline" for="checkboxes-3">
+                    <input type="checkbox" name="checkboxes" id="checkboxes-3" value="4">
+                    4
+                </label>
+            </div>
+        </div>
+
+        <!-- Multiple Radios -->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="radios">Multiple Radios</label>
+            <div class="col-md-4">
+                <div class="radio">
+                    <label for="radios-0">
+                        <input type="radio" name="radios" id="radios-0" value="1" checked="checked">
+                        Option one
+                    </label>
+                </div>
+                <div class="radio">
+                    <label for="radios-1">
+                        <input type="radio" name="radios" id="radios-1" value="2">
+                        Option two
+                    </label>
+                </div>
+            </div>
+        </div>
+
+    </fieldset>
+</form>

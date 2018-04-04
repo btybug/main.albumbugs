1a2d90264aa865ecb089f7c326223bf8cd9ecff6
Sahak <hakobyan.sahak88@gmail.com>
2018-04-04 17:14:20 +0400

    up

--- vendor/btybug/frontsite/src/Resources/Views/_partials/fbapi.blade.php
+++ vendor/btybug/frontsite/src/Resources/Views/_partials/fbapi.blade.php
@@ -26,7 +26,7 @@
         <div class="form-group">
             <label class="col-md-4 control-label" for="callbackurl">Call Back Url</label>
             <div class="col-md-4">
-                <input id="callbackurl" name="callbackurl" type="text" placeholder="placeholder" class="form-control input-md">
+                <input id="callbackurl" type="text" readonly class="form-control input-md">
                 <span class="help-block">Use this url to create App</span>
             </div>
         </div>

b91e6b68c0f8493a7e6e22261dc8379bbfacecf6
Sahak <hakobyan.sahak88@gmail.com>
2018-04-04 16:41:29 +0400

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,51 @@
-@extends('layouts.app')
-
+@extends('layouts.login')
 @section('content')
-<div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="text" class="form-control" name="usernameOremail" value="{{ old('usernameOremail') }}"  autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" >
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                {{--<a class="btn btn-link" href="{{ route('password.request') }}">--}}
-                                    {{--Forgot Your Password?--}}
-                                {{--</a>--}}
-                            </div>
-                        </div>
-                    </form>
+        <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3 logadmin">
+            @if (session('flash.message') != null)
+                <div class="flash alert {{ Session::has('flash.class') ? session('flash.class') : 'alert-success' }}">
+                    {!! session('flash.message') !!}
                 </div>
-            </div>
+            @endif
+            {!! Form::open() !!}
+            <fieldset>
+                <h2>Please Login</h2>
+                <hr class="colorgraph">
+                <div class="form-group">
+                    <input type="text" name="usernameOremail" id="email" class="form-control input-lg"
+                           placeholder="Username or Email">
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password" id="password" class="form-control input-lg"
+                           placeholder="Password" value="">
+                </div>
+
+                <div class="checkbox">
+                    <label>
+                        <input name="remember" type="checkbox" value="Remember Me"> Remember Me
+                    </label>
+                    <a href="{!! url('forgot')!!}" class="btn btn-link pull-right">Forgot Password?</a>
+                </div>
+
+
+                <hr class="colorgraph">
+                <div class="row">
+                    <div class="col-xs-6 col-sm-6 col-md-6">
+                        <input type="submit" class="btn btn-lg btn-success btn-block" value="Login">
+                    </div>
+                    @if(isset($enable_reg) && $enable_reg)
+                        <div class="col-xs-6 col-sm-6 col-md-6">
+                            <a href="{!! url('register')!!}" class="btn btn-lg btn-primary btn-block">Register</a>
+                        </div>
+                    @endif
+                </div>
+            </fieldset>
+            {!! Form::close() !!}
         </div>
     </div>
-</div>
-@endsection
+@stop
+@section('footer')
+@stop
+
+
+

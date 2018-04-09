e7cc110f9ade50129380d6a798cb7d8fd741915e
Sahak <hakobyan.sahak88@gmail.com>
2018-04-09 12:42:29 +0400

    update

---
+++ resources/units/my_items_content/tpl.blade.php
@@ -0,0 +1,184 @@
+<section class="myaccount-about-all">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-3 text-center myaccount-about-left">
+                <div class="head">
+                    <figure>
+                        <img src="https://images.askmen.com/1080x540/grooming/project/top-10-simple-ways-to-impress-a-woman-1030509-TwoByOne.jpg"
+                             alt="">
+                    </figure>
+                    <h2>About Me</h2>
+                    <p>My profession</p>
+                </div>
+                <article>
+                    <i class="fa fa-desktop"></i>
+                    <h4>proffesion 1</h4>
+                </article>
+                <article>
+                    <i class="fa fa-mobile"></i>
+                    <h4>proffesion 2</h4>
+                </article>
+            </div>
+            <div class="col-md-9 col-sm-9 myaccount-about-right">
+                <div class="head">
+                    <h2>Name lorem</h2>
+                    <h3>less ipsum</h3>
+                </div>
+                <p>Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                    scrambled it to make a type specimen book. It has survived not only five centuries, but also. Lorem
+                    Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+                    a galley of type and scrambled it to make a type specimen book. It has survived not only five
+                    centuries, but also.</p>
+                <div class="devider">
+                    <div class="icon-green">
+                        <div class="white-icon"></div>
+                    </div>
+                </div>
+
+                <div class="vertical_space"></div>
+                <p><span>Date Of Birth :</span> 11 Jun 2018 </p>
+                <p><span>Phone : </span> 00- 000-0000</p>
+                <p><span>Email : </span> lorem@gmail.com </p>
+                <p><span>Address :  </span> 00000 arddress lorem ipsum,lorem </p>
+                <p><span>Website :  </span> www.wwwww.com </p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="myaccount-posts">
+    <div class="container">
+        <div class="row">
+            <h2>My posts</h2>
+            <article>
+                <div class="col-md-4 col-sm-4">
+                    <div class="img">
+                        <div class="day">
+                            <span>17.10.2018</span>
+                        </div>
+                        <img src="http://finedininglovers.cdn.crosscast-system.com/BlogPost/xl_13272_Nice-Tasting-Tour-FDL-TP.jpg"
+                             alt="">
+                    </div>
+                </div>
+                <div class="col-md-8 col-sm-8">
+                    <div class="content">
+                        <p>
+                            Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                            of type and scrambled it to make a type specimen book. It has survived not only five
+                            centuries, but also. Lorem Ipsum has been the industry's standard dummy text ever since the
+                            1500s, when an unknown printer took a galley of type and scrambled it to make a type
+                            specimen book. It has survived not only five centuries, but also.
+                        </p>
+                        <button class="more">See more</button>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </article>
+            <article>
+                <div class="col-md-4 col-sm-4">
+                    <div class="img">
+                        <div class="day">
+                            <span>17.10.2018</span>
+                        </div>
+                        <img src="http://kb4images.com/images/nice-wallpaper-hd/38102801-nice-wallpaper-hd.jpg"
+                             alt="">
+                    </div>
+                </div>
+                <div class="col-md-8 col-sm-8">
+                    <div class="content">
+                        <p>
+                            Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                            of type and scrambled it to make a type specimen book. It has survived not only five
+                            centuries, but also. Lorem Ipsum has been the industry's standard dummy text ever since the
+                            1500s, when an unknown printer took a galley of type and scrambled it to make a type
+                            specimen book. It has survived not only five centuries, but also.
+                        </p>
+                        <button class="more">See more</button>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </article>
+            <article>
+                <div class="col-md-4 col-sm-4">
+                    <div class="img">
+                        <div class="day">
+                            <span>17.10.2018</span>
+                        </div>
+                        <img src="https://78.media.tumblr.com/avatar_0d3fe4da5e02_128.png"
+                             alt="">
+                    </div>
+                </div>
+                <div class="col-md-8 col-sm-8">
+                    <div class="content">
+                        <p>
+                            Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                            of type and scrambled it to make a type specimen book. It has survived not only five
+                            centuries, but also. Lorem Ipsum has been the industry's standard dummy text ever since the
+                            1500s, when an unknown printer took a galley of type and scrambled it to make a type
+                            specimen book. It has survived not only five centuries, but also.
+                        </p>
+                        <button class="more">See more</button>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </article>
+        </div>
+    </div>
+</section>
+<section class="myphotos">
+    <div class="container">
+        <div class="row">
+            <h2>My photos</h2>
+            <ul>
+                <li class="col-md-4 col-xs-12 col-sm-4">
+                    <div class="photo">
+                        <img src="http://finedininglovers.cdn.crosscast-system.com/BlogPost/xl_13272_Nice-Tasting-Tour-FDL-TP.jpg" alt="">
+                    </div>
+                </li>
+                <li class="col-md-4 col-xs-12 col-sm-4">
+                    <div class="photo">
+                        <img src="http://finedininglovers.cdn.crosscast-system.com/BlogPost/xl_13272_Nice-Tasting-Tour-FDL-TP.jpg" alt="">
+                    </div>
+                </li>
+                <li class="col-md-4 col-xs-12 col-sm-4">
+                    <div class="photo">
+                        <img src="http://finedininglovers.cdn.crosscast-system.com/BlogPost/xl_13272_Nice-Tasting-Tour-FDL-TP.jpg" alt="">
+                    </div>
+                </li>
+                <li class="col-md-4 col-xs-12 col-sm-4">
+                    <div class="photo">
+                        <img src="http://finedininglovers.cdn.crosscast-system.com/BlogPost/xl_13272_Nice-Tasting-Tour-FDL-TP.jpg" alt="">
+                    </div>
+                </li>
+                <li class="col-md-4 col-xs-12 col-sm-4">
+                    <div class="photo">
+                        <img src="http://finedininglovers.cdn.crosscast-system.com/BlogPost/xl_13272_Nice-Tasting-Tour-FDL-TP.jpg" alt="">
+                    </div>
+                </li>
+                <li class="col-md-4 col-xs-12 col-sm-4">
+                    <div class="photo">
+                        <img src="http://finedininglovers.cdn.crosscast-system.com/BlogPost/xl_13272_Nice-Tasting-Tour-FDL-TP.jpg" alt="">
+                    </div>
+                </li>
+                <li class="col-md-4 col-xs-12 col-sm-4">
+                    <div class="photo">
+                        <img src="http://finedininglovers.cdn.crosscast-system.com/BlogPost/xl_13272_Nice-Tasting-Tour-FDL-TP.jpg" alt="">
+                    </div>
+                </li>
+                <li class="col-md-4 col-xs-12 col-sm-4">
+                    <div class="photo">
+                        <img src="http://finedininglovers.cdn.crosscast-system.com/BlogPost/xl_13272_Nice-Tasting-Tour-FDL-TP.jpg" alt="">
+                    </div>
+                </li>
+                <div class="clearfix"></div>
+            </ul>
+        </div>
+    </div>
+</section>
+
+
+{!! BBstyle($_this->path.DS.'css'.DS.'styles.css') !!}
+{!! BBscript($_this->path.DS.'js'.DS.'popper'.DS.'popper.min.js') !!}

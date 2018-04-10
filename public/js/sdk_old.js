4c3a18f2c8ff4fd9cd04e4dbd5ecbbdd90c1e6fc
Sahak <hakobyan.sahak88@gmail.com>
2018-04-10 17:40:41 +0400

    version 0.1

---
+++ public/js/sdk_old.js
@@ -0,0 +1,83 @@
+var BtyBug = {
+    my_window: null,
+    result: null,
+    client_id:null,
+    tmp: [],
+    init: function (client_id, client_secret) {
+        this.client_secret = client_secret;
+        this.client_id = client_id;
+        if (this.findGetParameter('code')) {
+            my_window.clone();
+        }
+        this.postSendAjax(this.access_token.url, this.access_token, function (success) {
+            console.log(success);
+            request = success;
+        });
+    },
+    findGetParameter: function (parameterName) {
+        location.search
+            .substr(1)
+            .split("&")
+            .forEach(function (item) {
+                this.tmp = item.split("=");
+                if (this.tmp[0] === parameterName) this.result = decodeURIComponent(this.tmp[1]);
+            });
+        return this.result;
+    },
+    access_token: {
+        url: 'http://forms.albumbugs.com/oauth/token',
+        grant_type: 'client_credentials',
+        client_id:  this.client_id,
+        client_secret: this.client_secret,
+        scope: '',
+    },
+    login: {
+        url: 'http://forms.albumbugs.com/oauth/authorize',
+        data: {
+            'client_id':this.client_id,
+            'redirect_uri': 'http://www.albumbugs.com/callback',
+            // 'access_token':null,
+            'response_type': 'code',
+            'scope': '*'
+        }
+    },
+    refreshToken: {
+        'grant_type': 'refresh_token',
+        'refresh_token': 'the-refresh-token',
+        client_id: this.client_id,
+        client_secret: this.client_secret,
+        'scope': '',
+    },
+    get_access_token: {
+        'grant_type': 'authorization_code',
+        'client_id': 'client-id',
+        'client_secret': 'client-secret',
+        'redirect_uri': 'http://example.com/callback',
+        'code': null,
+    },
+    callwindow: function () {
+        console.log(this.login.data);
+        this.my_window = window.open(this.login.url + '?' + $.param(this.login.data), "popupWindow", "width=600,height=600,scrollbars=yes");
+    },
+    postSendAjax: function (url, data, success, error) {
+        $.ajax({
+            type: 'post',
+            url: url,
+            cache: false,
+            datatype: "json",
+            data: data,
+            success: function (data) {
+                if (success) {
+                    success(data);
+                }
+                return data;
+            },
+            error: function (errorThrown) {
+                if (error) {
+                    error(errorThrown);
+                }
+                return errorThrown;
+            }
+        });
+    }
+}
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
-<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
-<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if IE 8]>
+<html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]>
+<html lang="en" class="ie9 no-js"> <![endif]-->
 <!--[if !IE]><!-->
 <html lang="en" class="no-js">
 <!--<![endif]-->
@@ -12,55 +14,82 @@
     <meta content="" name="description"/>
     <meta content="" name="author"/>
 
-    <link rel="stylesheet" href="{{ url("public/css/bootstrap/css/bootstrap.min.css") }}" />
-    <link rel="stylesheet" href="{{ url("public/css/font-awesome/css/font-awesome.min.css") }}" />
-    <link rel="stylesheet" href="{{ url("public/css/cms.css") }}" />
-    <link rel="stylesheet" href="{{ url("public/js/jquery-ui/jquery-ui.min.css") }}" />
+    <link rel="stylesheet" href="{{ url("public/css/bootstrap/css/bootstrap.min.css") }}"/>
+    <link rel="stylesheet" href="{{ url("public/css/font-awesome/css/font-awesome.min.css") }}"/>
+    <link rel="stylesheet" href="{{ url("public/css/cms.css") }}"/>
+    <link rel="stylesheet" href="{{ url("public/js/jquery-ui/jquery-ui.min.css") }}"/>
 
 
 </head>
-<body id="page-top">
-    <div class="container">
-        @if (count($errors) > 0)
-            <div class="alert alert-danger" role="alert">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+<div class="container">
+    @if (count($errors) > 0)
+        <div class="alert alert-danger" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
 
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        @if (session('flash.message') != null)
-            <div class="flash alert {{ Session::has('flash.class') ? session('flash.class') : 'alert-success' }}" role="alert">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                {!! session('flash.message') !!}
-            </div>
-        @endif
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    @if (session('flash.message') != null)
+        <div class="flash alert {{ Session::has('flash.class') ? session('flash.class') : 'alert-success' }}"
+             role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+            {!! session('flash.message') !!}
+        </div>
+    @endif
 
-        @if(Session::has('message'))
-            <div class="m-t-10 alert {{ Session::get('alert-class', 'alert-info') }} alert-dismissible" role="alert">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                {{ Session::get('message') }} </div>
-        @endif
-        @yield('content')
-        @include('modal')
-    </div>
+    @if(Session::has('message'))
+        <div class="m-t-10 alert {{ Session::get('alert-class', 'alert-info') }} alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+            {{ Session::get('message') }} </div>
+    @endif
+    @yield('content')
+    @include('modal')
+</div>
+<button class="customBtn_loginBtn_facebook oauth-login">
+    Login with icon
+</button>
+<script src="{{ url("public/js/jquery-3.2.1.min.js") }}" type="text/javascript"></script>
+<script src="{{ url("public/js/jquery-ui/jquery-ui.min.js") }}" type="text/javascript"></script>
+<script src="{{ url("public/css/bootstrap/js/bootstrap.min.js") }}" type="text/javascript"></script>
+@yield('footer')
+@stack('javascript')
+{{--@if(Session::has('message_code') && Session::get('message_code') == 200)--}}
+{{--<script>--}}
+{{--$(function() {--}}
+{{--$('#message-modal .modal-body').html("{!! Session::pull('success_mes') !!}");--}}
+{{--$('#message-modal').modal();--}}
+{{--});--}}
+{{--</script>--}}
+{{--@endif--}}
+<input name="token" type="hidden" value="{{ csrf_token() }}" id="token"/>
+<script>
+    $(document).ready(function () {
 
-    <script src="{{ url("public/js/jquery-3.2.1.min.js") }}" type="text/javascript"></script>
-    <script src="{{ url("public/js/jquery-ui/jquery-ui.min.js") }}" type="text/javascript"></script>
-    <script src="{{ url("public/css/bootstrap/js/bootstrap.min.js") }}" type="text/javascript"></script>
-    @yield('footer')
-    @stack('javascript')
-    {{--@if(Session::has('message_code') && Session::get('message_code') == 200)--}}
-        {{--<script>--}}
-            {{--$(function() {--}}
-                {{--$('#message-modal .modal-body').html("{!! Session::pull('success_mes') !!}");--}}
-                {{--$('#message-modal').modal();--}}
-            {{--});--}}
-        {{--</script>--}}
-    {{--@endif--}}
-    <input name="token" type="hidden" value="{{ csrf_token() }}" id="token"/>
+        window.fbAsyncInit = function () {
+            BtyBug.init(8, '3veI8Gaa3xDKvH3Lb2GPqJ0IkaZdKqOuSsHXtUDd');
+        }
+
+        $('body').on('click', '.oauth-login', function () {
+            BtyBug.callwindow();
+        });
+    });
+
+    (function (d, s, id) {
+        var js, bjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'http://forms.albumbugs.com/public/js/sdk.js?v=1';
+        bjs.parentNode.insertBefore(js, bjs);
+    })(document, 'script', 'BtyBug-jssdk');
+
+</script>
 </body>
 </html>

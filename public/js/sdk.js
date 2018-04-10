4cbd5d2666d66db81882feaf1fa7b346b832490c
terakyan <edo.terakyan@gmail.com>
2018-04-10 18:04:01 +0400

    sdk

--- public/js/sdk.js
+++ public/js/sdk.js
@@ -1,28 +1,22 @@
 var BtyBug = {
     my_window: null,
     result: null,
-    tmp: [],
     init: function (client_id, client_secret) {
         console.log(client_id);
         this.client_secret = client_secret;
         this.client_id = client_id;
-        // if (this.findGetParameter('code')) {
-        //     my_window.clone();
-        // }
-
     },
     login: {
         url: 'http://forms.albumbugs.com/oauth/authorize',
         data: {
-            'client_id':null,
+            'client_id': null,
             'redirect_uri': 'http://www.albumbugs.com/callback',
-            // 'access_token':null,
             'response_type': 'code',
             'scope': '*'
         }
     },
     callwindow: function () {
-        this.login.data.client_id=this.client_id;
+        this.login.data.client_id = this.client_id;
         this.my_window = window.open(this.login.url + '?' + $.param(this.login.data), "popupWindow", "width=600,height=600,scrollbars=yes");
     },
     postSendAjax: function (url, data, success, error) {
@@ -46,4 +40,4 @@ var BtyBug = {
             }
         });
     }
-}
+};

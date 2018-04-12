62404d4caefbf83ad396ccdf55312444e5b1e9a9
Sahak <hakobyan.sahak88@gmail.com>
2018-04-12 13:11:50 +0400

    up

--- public/js/sdk.js
+++ public/js/sdk.js
@@ -2,7 +2,6 @@ var BtyBug = {
     my_window: null,
     result: null,
     init: function (client_id, client_secret) {
-        console.log(client_id);
         this.client_secret = client_secret;
         this.client_id = client_id;
     },
@@ -18,7 +17,6 @@ var BtyBug = {
     callwindow: function () {
         this.login.data.client_id = this.client_id;
         this.my_window = window.open(this.login.url + '?' + $.param(this.login.data), "popupWindow", "width=600,height=600,scrollbars=yes");
-        this.my_window.close();
     },
     postSendAjax: function (url, data, success, error) {
         $.ajax({

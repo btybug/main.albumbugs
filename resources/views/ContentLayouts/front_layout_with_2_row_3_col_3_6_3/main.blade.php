278274e16a4861faab2bc7a01feb977e8a5ff12f
sahakavatar <hakobyan.sahak88@gmail.com>
2018-04-17 12:58:29 +0400

    create some layout

---
+++ resources/views/ContentLayouts/front_layout_with_2_row_3_col_3_6_3/main.blade.php
@@ -0,0 +1,30 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="top">
+
+            </div>
+        </div>
+    </div>
+    <div class="row div-flex">
+        <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="left-bar">
+
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="main-cont">
+
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="right-bar">
+
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+{!! BBstyle($_this->path.DS.'css/style.css') !!}
---
+++ resources/views/ContentLayouts/front_layout_with_2_row_3_col_9_col_2row/demo.blade.php
@@ -0,0 +1,28 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="top">
+
+            </div>
+        </div>
+    </div>
+    <div class="row div-flex">
+        <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="left-bar">
+
+            </div>
+        </div>
+        <div class="col-md-9 col-sm-9 col-xs-12">
+            <div class="top-bar">
+
+            </div>
+            <div class="main-cont">
+
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+{!! BBstyle($_this->path.DS.'css/style.css') !!}

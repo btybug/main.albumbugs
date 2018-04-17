8888cd2b95e7b650f6b5d893c15d72e877ddd9ef
sahakavatar <hakobyan.sahak88@gmail.com>
2018-04-17 15:35:52 +0400

    unit message

---
+++ resources/units/unit_for_message_with_time/demo.blade.php
@@ -0,0 +1,94 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="bty-message-3">
+                <ul>
+                    <li>
+                        <div>
+                            <img src="https://www.seducewithpersonality.com/wp-content/uploads/2013/08/How-to-attract-a-girl.jpg"
+                                 alt="">
+                        </div>
+                        <div>
+                            <h5>Lorem name</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id nunc non eros fermentum
+                                vestibulum ut id felis. Nunc molestie libero eget urna aliquet, vitae laoreet felis
+                                ultricies. Fusce sit amet massa malesuada</p>
+                        </div>
+                        <div>
+                            14:50 am
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <img src="https://www.seducewithpersonality.com/wp-content/uploads/2013/08/How-to-attract-a-girl.jpg"
+                                 alt="">
+                        </div>
+                        <div>
+                            <h5>Lorem name</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id nunc non eros fermentum
+                                vestibulum ut id felis. Nunc molestie l</p>
+                        </div>
+                        <div>
+                            14:50 am
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <img src="https://www.seducewithpersonality.com/wp-content/uploads/2013/08/How-to-attract-a-girl.jpg"
+                                 alt="">
+                        </div>
+                        <div>
+                            <h5>Lorem name</h5>
+                            <p>Lorem ipsum dolor sit a malesuada</p>
+                        </div>
+                        <div>
+                            14:50 am
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <img src="https://www.seducewithpersonality.com/wp-content/uploads/2013/08/How-to-attract-a-girl.jpg"
+                                 alt="">
+                        </div>
+                        <div>
+                            <h5>Lorem name</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id nunc non eros fermentum
+                                vestibulum ut id felis. Nunc molestie </p>
+                        </div>
+                        <div>
+                            14:50 am
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <img src="https://www.seducewithpersonality.com/wp-content/uploads/2013/08/How-to-attract-a-girl.jpg"
+                                 alt="">
+                        </div>
+                        <div>
+                            <h5>Lorem name</h5>
+                            <p>Lorem ipsum dolor sit amet, </p>
+                        </div>
+                        <div>
+                            14:50 am
+                        </div>
+                    </li>
+
+
+                </ul>
+                <div>
+                    <div>
+                        <textarea name="" placeholder="Type your message here..."></textarea>
+                    </div>
+                    <div>
+                        <button type="button"><i class="fa fa-paper-plane-o"></i> </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}
+{!! BBscript($_this->path.DS.'js'.DS.'custom.js') !!}

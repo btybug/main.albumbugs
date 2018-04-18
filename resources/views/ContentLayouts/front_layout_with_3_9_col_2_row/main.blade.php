4c28b100eb4a3b0a111ed0e89bbbac629bafb37f
sahakavatar <hakobyan.sahak88@gmail.com>
2018-04-18 17:18:40 +0400

    layout

--- resources/views/ContentLayouts/front_layout_with_3_9_col_2_row/main.blade.php
+++ resources/views/ContentLayouts/front_layout_with_3_9_col_2_row/main.blade.php
@@ -2,59 +2,63 @@
     <div class="row row-no-gutter {{(isset($settings['main_page_style'])&& $settings['main_page_style']) ? $settings['main_page_style'] : ''}}">
         <div class="col-md-3 col-sm-3 col-xs-12 ">
             <div class="default-column {{(isset($settings['ls_style'])&& $settings['ls_style'] ) ? $settings['ls_style'] : ''}}">
-                @switch(issetReturn($settings,'ls_content_type'))
-                    @case('unit')
-                    @if(has_setting($settings,'ls_unit'))
-                        {!! BBRenderUnits($settings['ls_unit'],isset($settings['_page'])?['_page'=>$settings['_page']]:[]) !!}
-                    @endif
-                    @break
+                <div>
+                    @switch(issetReturn($settings,'ls_content_type'))
+                        @case('unit')
+                        @if(has_setting($settings,'ls_unit'))
+                            {!! BBRenderUnits($settings['ls_unit'],isset($settings['_page'])?['_page'=>$settings['_page']]:[]) !!}
+                        @endif
+                        @break
 
-                    @case('hook')
-                    @if(has_setting($settings,'hooks') && is_array($settings['hooks']['ls_hook']))
-                        {!! BBrenderHook($settings['hooks']['ls_hook']) !!}
-                    @endif
-                    @break
+                        @case('hook')
+                        @if(has_setting($settings,'hooks') && is_array($settings['hooks']['ls_hook']))
+                            {!! BBrenderHook($settings['hooks']['ls_hook']) !!}
+                        @endif
+                        @break
 
-                    @default
-                    <span>Something went wrong, please try again</span>
-                @endswitch
+                        @default
+                        <span>Something went wrong, please try again</span>
+                    @endswitch
+                </div>
             </div>
         </div>
         <div class="col-md-9 col-sm-9 col-xs-12 default-column {{isset($settings['right_area_style']) ? $settings['right_area_style'] : ''}}">
-            <div class=" {{(isset($settings['right_area_style'])&& $settings['right_area_style']) ? '' : ''}} ">
-                @switch(issetReturn($settings,'tr_content_type'))
-                    @case('unit')
-                    @if(has_setting($settings,'tr_unit'))
-                        {!! BBRenderUnits($settings['tr_unit'],isset($settings['_page'])?['_page'=>$settings['_page']]:[]) !!}
-                    @endif
-                    @break
+            <div>
+                <div class=" {{(isset($settings['right_area_style'])&& $settings['right_area_style']) ? '' : ''}} ">
+                    @switch(issetReturn($settings,'tr_content_type'))
+                        @case('unit')
+                        @if(has_setting($settings,'tr_unit'))
+                            {!! BBRenderUnits($settings['tr_unit'],isset($settings['_page'])?['_page'=>$settings['_page']]:[]) !!}
+                        @endif
+                        @break
 
-                    @case('hook')
-                    @if(has_setting($settings,'hooks') && isset($settings['hooks']['tr_hook']))
-                        {!! BBrenderHook($settings['hooks']['tr_hook']) !!}
-                    @endif
-                    @break
-                    @default
-                    <span>Something went wrong, please try again</span>
-                @endswitch
-            </div>
-            <div class="{{(isset($settings['right_area_style'])&& $settings['right_area_style']) ? '' : ''}} ">
-                @switch(issetReturn($settings,'main_content_type'))
-                    @case('unit')
-                    @if(has_setting($settings,'main_unit'))
-                        {!! BBRenderUnits($settings['main_unit'],isset($settings['_page'])?['_page'=>$settings['_page']]:[]) !!}
-                    @endif
-                    @break
+                        @case('hook')
+                        @if(has_setting($settings,'hooks') && isset($settings['hooks']['tr_hook']))
+                            {!! BBrenderHook($settings['hooks']['tr_hook']) !!}
+                        @endif
+                        @break
+                        @default
+                        <span>Something went wrong, please try again</span>
+                    @endswitch
+                </div>
+                <div class="{{(isset($settings['right_area_style'])&& $settings['right_area_style']) ? '' : ''}} ">
+                    @switch(issetReturn($settings,'main_content_type'))
+                        @case('unit')
+                        @if(has_setting($settings,'main_unit'))
+                            {!! BBRenderUnits($settings['main_unit'],isset($settings['_page'])?['_page'=>$settings['_page']]:[]) !!}
+                        @endif
+                        @break
 
-                    @case('hook')
-                    @if(has_setting($settings,'hooks') && is_array($settings['hooks']['main_hook']))
-                        {!! BBrenderHook($settings['hooks']['main_hook']) !!}
-                    @endif
-                    @break
+                        @case('hook')
+                        @if(has_setting($settings,'hooks') && is_array($settings['hooks']['main_hook']))
+                            {!! BBrenderHook($settings['hooks']['main_hook']) !!}
+                        @endif
+                        @break
 
-                    @default
-                    <span>Something went wrong, please try again</span>
-                @endswitch
+                        @default
+                        <span>Something went wrong, please try again</span>
+                    @endswitch
+                </div>
             </div>
         </div>
     </div>

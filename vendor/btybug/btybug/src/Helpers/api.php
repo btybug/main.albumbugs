9792d72beaf8dd9af3ebced5d610b8e40f63f775
Sahak <hakobyan.sahak88@gmail.com>
2018-04-18 16:24:49 +0400

    up

--- vendor/btybug/btybug/src/Helpers/api.php
+++ vendor/btybug/btybug/src/Helpers/api.php
@@ -1989,7 +1989,7 @@ function getDinamicStyleForCssFileDemo($filename, $table_name)
 
 function useDinamicStyle($filename)
 {
-    return '<link href="' . asset('public/dinamiccss/' . $filename . '.css') . '" rel="stylesheet">';
+    return '<link href="' . asset('public/dinamiccss/' . $filename . '.css?v=1') . '" rel="stylesheet">';
 }
 
 function useDinamicStyleByPath($path, $main)

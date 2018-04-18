8a7cb0c15af938bf017c2da7a7a9d0af4c10d041
terakyan <edo.terakyan@gmail.com>
2018-04-18 13:44:45 +0400

    at

--- vendor/btybug/frontsite/src/Resources/Views/frontend/pages/_partials/page-data.blade.php
+++ vendor/btybug/frontsite/src/Resources/Views/frontend/pages/_partials/page-data.blade.php
@@ -14,12 +14,22 @@
         {{--{!! Btybug\btybug\Models\ContentLayouts\ContentLayouts::getChildrenPageLayout($page) !!}--}}
 
         <div class="panel panel-default custompanel m-t-20">
-            <div class="panel-heading">All Hooks</div>
-            <div class="panel-body all-hooks-content">
-                @if($page->type != 'classify' && $page->type != 'tags')
-                    {{--{!! Btybug\btybug\Models\ContentLayouts\ContentLayouts::getPageLayoutHooks($page) !!}--}}
-                    {!! Btybug\btybug\Models\ContentLayouts\ContentLayouts::getPageLayoutHook($page) !!}
-                @endif
+            <div class="panel-heading">Header & footer</div>
+            <div class="panel-body">
+                <div class="form-group">
+                    <div class="col-md-12">
+                        {{Form::hidden('header',0)}}
+                        {{Form::hidden('footer',0)}}
+                        <label class="bd_layout pull-left m-r-15">{!! Form::checkbox('header',1,null,['style' => 'position:initial;z-index:1;']) !!}
+                            <span class="labls">Header</span>
+
+                        </label>
+                        <label class="bd_layout"> {!! Form::checkbox('footer',1,null,['style' => 'position:initial;z-index:1;']) !!}
+                            <span class="labls">Footer</span>
+
+                        </label>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="panel panel-default custompanel m-t-20">

4bdcba0544eea5fefe59f5c95a31d0ed536b150e
sahakavatar <hakobyan.sahak88@gmail.com>
2018-04-19 13:09:53 +0400

    unit services

---
+++ resources/units/unit_for_frontend_my_services/tpl.blade.php
@@ -0,0 +1,70 @@
+<section class="my-service">
+    <div>
+        <div class="section-content">
+            <div class="title">
+                <div class="head">
+                    <h2>My Services / </h2>
+                    <p>what services I offer remotley</p>
+                </div>
+            </div>
+            <div class="services-content-all">
+                <div class="row">
+                    <div class="col-sm-4 col-xs-12">
+                        <div class="services-content">
+                            <div class="service-icon">
+                                <i class="fa fa-code"></i>
+                            </div>
+                            <div class="sub-tittle">
+                                <div class="head">
+                                    <h2>Development</h2>
+                                    <span class="service-no pull-right">01</span>
+                                </div>
+                            </div>
+                            <div class="service-text">
+                                <p>Exercitationem illo labore magni nesciunt, nihil non obcaecati odio?
+                                    Accusamus, at deleniti dolore enim laborum libero sequi voluptatem?</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-xs-12">
+                        <div class="services-content">
+                            <div class="service-icon">
+                                <i class="fa fa-briefcase"></i>
+                            </div>
+                            <div class="sub-tittle">
+                                <div class="head">
+                                    <h2>WooCommerce</h2>
+                                    <span class="service-no pull-right">02</span>
+                                </div>
+                            </div>
+                            <div class="service-text">
+                                <p>xercitationem illo labore magni nesciunt, nihil non obcaecati odio?
+                                    Accusamus, at deleniti dolore enim laborum libero sequi voluptatem?</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-xs-12">
+                        <div class="services-content">
+                            <div class="service-icon">
+                                <i class="fa fa-cogs"></i>
+                            </div>
+                            <div class="sub-tittle">
+                                <div class="head">
+                                    <h2>Software</h2>
+                                    <span class="service-no pull-right">03</span>
+                                </div>
+                            </div>
+                            <div class="service-text">
+                                <p>xercitationem illo labore magni nesciunt, nihil non obcaecati odio?
+                                    Accusamus, at deleniti dolore enim laborum libero sequi voluptatem?</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}
+{!! BBscript($_this->path.DS.'js'.DS.'custom.js') !!}

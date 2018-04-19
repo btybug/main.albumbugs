2124683e3a1cba2c8659595a9285e48f99f79fbe
sahakavatar <hakobyan.sahak88@gmail.com>
2018-04-19 14:52:13 +0400

    unit recent post

---
+++ resources/units/unit_for_frontend_recent_blog/demo.blade.php
@@ -0,0 +1,80 @@
+<section class="recent-blog">
+    <div class="container">
+        <div class="row section-content">
+            <div class="title">
+                <div class="head">
+                    <h2>Recent Blog / </h2>
+                    <p>don’t miss every single post</p>
+                </div>
+            </div>
+            <div class="blog-content">
+                <div class="row">
+                    <div class="col-sm-4 col-xs-12">
+                        <div class="blog-text-pic">
+                            <div class="blog-pic">
+                                <img src="https://cdn.pixabay.com/photo/2018/02/12/12/02/nice-3148140__340.jpg" alt="image">
+                            </div>
+                            <div class="blog-meta">
+                                <ul class="meta">
+                                    <li>26 April 2016 <span>/</span></li>
+                                    <li class="blog-by">by<a class="blog-admin" href="#">Admin</a></li>
+                                </ul>
+                            </div>
+                            <div class="blog-title">
+                                <h3><a href="#">My First Blog Post of The Begining of This Year</a></h3>
+                            </div>
+                            <div class="read-more text-uppercase">
+                                <a class="more" href="#">Read More</a>
+                                <span class="right-arrow"><a href="#"><i class="fa fa-arrow-right"></i></a></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-xs-12">
+                        <div class="blog-text-pic">
+                            <div class="blog-pic">
+                                <img src="http://hdwpro.com/wp-content/uploads/2017/02/Girl-Nice-Wallpaper.jpg" alt="image">
+                            </div>
+                            <div class="blog-meta">
+                                <ul class="meta">
+                                    <li>26 April 2016 <span>/</span></li>
+                                    <li class="blog-by">by<a class="blog-admin" href="#">Admin</a></li>
+                                </ul>
+                            </div>
+                            <div class="blog-title">
+                                <h3><a href="#">My First Blog Post of The Begining of This Year</a></h3>
+                            </div>
+                            <div class="read-more text-uppercase">
+                                <a class="more" href="#">Read More</a>
+                                <span class="right-arrow"><a href="#"><i class="fa fa-arrow-right"></i></a></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-xs-12">
+                        <div class="blog-text-pic">
+                            <div class="blog-pic">
+                                <img src="http://www.kinyu-z.net/data/wallpapers/95/1033230.jpg" alt="image">
+                            </div>
+                            <div class="blog-meta">
+                                <ul class="meta">
+                                    <li>26 April 2016 <span>/</span></li>
+                                    <li class="blog-by">by<a class="blog-admin" href="#">Admin</a></li>
+                                </ul>
+                            </div>
+                            <div class="blog-title">
+                                <h3><a href="#">My First Blog Post of The Begining of This Year</a></h3>
+                            </div>
+                            <div class="read-more text-uppercase">
+                                <a class="more" href="#">Read More</a>
+                                <span class="right-arrow"><a href="#"><i class="fa fa-arrow-right"></i></a></span>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}
+{!! BBscript($_this->path.DS.'js'.DS.'custom.js') !!}

f74941175c2e5f47d46780c2d431b8ce1df3de34
sahakavatar <hakobyan.sahak88@gmail.com>
2018-04-20 12:51:33 +0400

    unit search

---
+++ resources/units/unit_for_frontend_search/demo.blade.php
@@ -0,0 +1,17 @@
+<div class="col-md-3">
+    <div class="sidebar-search">
+        <div class="head">
+            <h3>Search</h3>
+        </div>
+        <div class="serach-block">
+            <form action="#" method="get">
+                <input type="text" class="" placeholder="Search...">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+        </div>
+    </div>
+
+</div>
+
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

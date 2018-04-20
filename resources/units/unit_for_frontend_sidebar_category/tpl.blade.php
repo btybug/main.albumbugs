dcf4a68ed21cab9600e98998ad7f2415e40debe2
sahakavatar <hakobyan.sahak88@gmail.com>
2018-04-20 15:43:38 +0400

    unit category

---
+++ resources/units/unit_for_frontend_sidebar_category/tpl.blade.php
@@ -0,0 +1,19 @@
+    <div class="sidebar-category">
+        <div class="head">
+            <h3>Category</h3>
+        </div>
+        <div class="category">
+            <ul>
+                <li><a href="#">Video</a></li>
+                <li><a href="#">Audio</a></li>
+                <li><a href="#">Photo</a></li>
+                <li><a href="#">Media</a></li>
+                <li><a href="#">Galery</a></li>
+                <li><a href="#">Music</a></li>
+                <li><a href="#">Images</a></li>
+                <li><a href="#">Design</a></li>
+            </ul>
+        </div>
+    </div>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

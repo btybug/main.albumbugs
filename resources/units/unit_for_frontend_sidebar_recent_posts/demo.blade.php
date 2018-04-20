8aa2c75654de0d6df7af8a53fb4044ed39d45b6a
sahakavatar <hakobyan.sahak88@gmail.com>
2018-04-20 16:52:13 +0400

    unit recent post sidebar

---
+++ resources/units/unit_for_frontend_sidebar_recent_posts/demo.blade.php
@@ -0,0 +1,76 @@
+<div class="col-md-3">
+    <div class="sidebar-recent-post">
+        <div class="head">
+            <h3>Category</h3>
+        </div>
+        <div class="recent-post">
+            <ul>
+                <li>
+                    <div class="wrapper row">
+                        <div class="col-sm-3 col-xs-12">
+                            <div class="img">
+                                <img src="https://www.pixelstalk.net/wp-content/uploads/images1/Animal-New-Nice-Backgrounds.jpg"
+                                     alt="">
+                            </div>
+
+                        </div>
+                        <div class="col-sm-9 col-xs-12">
+                            <div class="content">
+                                <h6><a href="#">apr 20,2018</a></h6>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid autem culpa</p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="wrapper row">
+                        <div class="col-sm-3 col-xs-12">
+                            <div class="img">
+                                <img src="http://hdwpro.com/wp-content/uploads/2017/02/Girl-Nice-Wallpaper.jpg" alt="">
+                            </div>
+
+                        </div>
+                        <div class="col-sm-9 col-xs-12">
+                            <div class="content">
+                                <h6><a href="#">apr 20,2018</a></h6>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid autem culpa</p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="wrapper row">
+                        <div class="col-sm-3 col-xs-12">
+                            <div class="img">
+                                <img src="http://sf.co.ua/16/04/wallpaper-21c28.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-sm-9 col-xs-12">
+                            <div class="content">
+                                <h6><a href="#">apr 20,2018</a></h6>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid autem culpa</p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="wrapper row">
+                        <div class="col-sm-3 col-xs-12">
+                            <div class="img">
+                                <img src="https://c1.staticflickr.com/1/397/19986449882_c2f42592c5_b.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-sm-9 col-xs-12">
+                            <div class="content">
+                                <h6><a href="#">apr 20,2018</a></h6>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid autem culpa</p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

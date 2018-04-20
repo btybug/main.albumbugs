df22cfe0c159d80bddd9f46f87d22841d4526ef5
sahakavatar <hakobyan.sahak88@gmail.com>
2018-04-20 15:12:12 +0400

    unit tags and project

---
+++ resources/units/unit_for_frontend_recent_project/tpl.blade.php
@@ -0,0 +1,17 @@
+    <div class="sidebar-project">
+        <div class="head">
+            <h3>Recent project</h3>
+        </div>
+        <div class="recent-project">
+            <ul class="row">
+                <li class="col-sm-4 col-xs-12"><a href="#"><img src="http://gateway-tour.com/wp-content/uploads/2016/11/visiter-nice.jpg" alt=""></a></li>
+                <li class="col-sm-4 col-xs-12"><a href="#"><img src="https://mein-traumurlaub.at/wp-content/uploads/2017/06/nizza-800x445.jpg" alt=""></a></li>
+                <li class="col-sm-4 col-xs-12"><a href="#"><img src="https://podroze.smcloud.net/t/photos/t/139613/5-albi_1031620.jpg" alt=""></a></li>
+                <li class="col-sm-4 col-xs-12"><a href="#"><img src="http://perfekt.boleslawiec.pl/wp-content/uploads/2018/02/4231_95739780-5cb8-4d7a-a581-9ebdbf2fdfd7.jpg" alt=""></a></li>
+                <li class="col-sm-4 col-xs-12"><a href="#"><img src="http://www.xlj-comimmo.fr/images/biens/1/6b82e410e4eb1660db8545f124d7edb3/ac4f724c53c871159d932f3cfe4b625a.jpg" alt=""></a></li>
+                <li class="col-sm-4 col-xs-12"><a href="#"><img src="https://www.agencelongchamp.com/picture-1024-1533/1532633/81464012058b9408ac91df6.74482715_960.jpg" alt=""></a></li>
+            </ul>
+        </div>
+    </div>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}
---
+++ resources/units/unit_for_frontend_tags/tpl.blade.php
@@ -0,0 +1,19 @@
+<div class="sidebar-tags">
+    <div class="head">
+        <h3>Search</h3>
+    </div>
+    <div class="tags-block">
+        <ul>
+            <li><a href="#">psd</a></li>
+            <li><a href="#">html</a></li>
+            <li><a href="#">js</a></li>
+            <li><a href="#">css</a></li>
+            <li><a href="#">wordpress</a></li>
+            <li><a href="#">jquery</a></li>
+            <li><a href="#">images</a></li>
+            <li><a href="#">templates</a></li>
+        </ul>
+    </div>
+</div>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

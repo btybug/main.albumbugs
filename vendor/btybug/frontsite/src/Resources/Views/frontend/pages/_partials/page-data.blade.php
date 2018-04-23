fc4b4c4a68ec1357f98f13a414c167ae6335103f
terakyan <edo.terakyan@gmail.com>
2018-04-23 10:59:28 +0400

    back

--- vendor/btybug/frontsite/src/Resources/Views/frontend/pages/_partials/page-data.blade.php
+++ vendor/btybug/frontsite/src/Resources/Views/frontend/pages/_partials/page-data.blade.php
@@ -32,26 +32,26 @@
                 </div>
             </div>
         </div>
-        {{--<div class="panel panel-default custompanel m-t-20">--}}
-            {{--<div class="panel-heading">Main Content--}}
-                {{--<div class="pull-right">--}}
-                    {{--Editor{!! Form::radio('content_type','editor',null,['data-role'=>'editor']) !!}--}}
-                    {{--Template{!! Form::radio('content_type','template',null,['data-role'=>'template']) !!}</div>--}}
-            {{--</div>--}}
-            {{--<div class="panel-body editor_body @if($page->content_type!='editor') hide @endif">--}}
-                {{--{!! Form::textarea('main_content',null,['id' => 'main_content']) !!}--}}
-            {{--</div>--}}
+        <div class="panel panel-default custompanel m-t-20">
+            <div class="panel-heading">Main Content
+                <div class="pull-right">
+                    Editor{!! Form::radio('content_type','editor',null,['data-role'=>'editor']) !!}
+                    Template{!! Form::radio('content_type','template',null,['data-role'=>'template']) !!}</div>
+            </div>
+            <div class="panel-body editor_body @if($page->content_type!='editor') hide @endif">
+                {!! Form::textarea('main_content',null,['id' => 'main_content']) !!}
+            </div>
 
-            {{--<div class="panel-body template_body @if($page->content_type!='template') hide @endif">--}}
-                {{--<div class="col-sm-5 p-l-0 p-r-10">--}}
-                {{--<input name="selcteunit" data-key="title" readonly="readonly" data-id="template"--}}
-                {{--class="page-layout-title form-control"--}}
-                {{--value="{!! BBgetUnitAttr(($page->template)??null,'title') !!}"--}}
-                {{-->--}}
-                {{--</div>--}}
-                {{--{!! BBbutton2('unit','template','front_page_content',"Change",['class'=>'btn btn-default change-layout','data-action'=>'main_content','model'=>($page->content_type=='editor')?null:$page]) !!}--}}
-            {{--</div>--}}
-        {{--</div>--}}
+            <div class="panel-body template_body @if($page->content_type!='template') hide @endif">
+                <div class="col-sm-5 p-l-0 p-r-10">
+                <input name="selcteunit" data-key="title" readonly="readonly" data-id="template"
+                class="page-layout-title form-control"
+                value="{!! BBgetUnitAttr(($page->template)??null,'title') !!}"
+                >
+                </div>
+                {!! BBbutton2('unit','template','front_page_content',"Change",['class'=>'btn btn-default change-layout','data-action'=>'main_content','model'=>($page->content_type=='editor')?null:$page]) !!}
+            </div>
+        </div>
     </div>
     <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 p-20">
         <div class="panel panel-default custompanel m-t-20">

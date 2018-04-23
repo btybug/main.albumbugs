dcc4b299083aebe040b9a648576ea6d53b1952e2
sahakavatar <hakobyan.sahak88@gmail.com>
2018-04-23 11:45:19 +0400

    unit top post

---
+++ resources/units/unit_for_frontend_sidebar_top_post/tpl.blade.php
@@ -0,0 +1,27 @@
+    <div class="sidebar-top-post">
+        <div class="head">
+            <h3>Top Post</h3>
+        </div>
+        <div class="top-post">
+            <div class="img">
+                <img src="https://www.pixelstalk.net/wp-content/uploads/2016/10/Crazy-cap-wear-cute-doll-nice-photos.jpg" alt="">
+            </div>
+            <div class="post-info">
+                <p class="title"><a href="#">Lorem ipsum ipsum</a></p>
+                <span class="cat">Category</span>
+                <ul class="meta">
+                    <li><span><i class="fa fa-cog"></i></span><span class="tag">Lorem Ipsum</span></li>
+                    <li><span><i class="fa fa-cog"></i></span><span class="tag">Lorem</span></li>
+                </ul>
+                <hr>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur cupiditate debitis delectus ex explicabo facilis ipsam.</p>
+                </div>
+                <div class="button">
+                    <a href="#">Get in touch</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

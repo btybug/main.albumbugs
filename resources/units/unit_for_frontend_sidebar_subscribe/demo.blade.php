d2ef7b371974f13341153710165b8e0bc61ada73
sahakavatar <hakobyan.sahak88@gmail.com>
2018-04-23 14:18:02 +0400

    unit subscribe

---
+++ resources/units/unit_for_frontend_sidebar_subscribe/demo.blade.php
@@ -0,0 +1,22 @@
+<div class="col-md-3">
+    <div class="sidebar-subscribe">
+        <div class="head">
+            <h3>NEWSLETTER</h3>
+        </div>
+        <div class="subscribe">
+            <p class="sub-title">Enter your e-mail to get the latest news.</p>
+            <div>
+                <form method="" action="">
+                    <div class="form-input">
+                        <input type="email" name="email" placeholder="Your e-mail address...">
+                    </div>
+                    <div class="form-button">
+                        <button type="submit">Subscribe</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

80221cfb34aaf43c36cd747c0d7e23cd58ec1145
sahakavatar <hakobyan.sahak88@gmail.com>
2018-04-26 11:11:37 +0400

    unit testimonials

---
+++ resources/units/unit_for_frontend_popular_categories/tpl.blade.php
@@ -0,0 +1,86 @@
+<div class="popular_cat">
+    <div class="head">
+        <h3>Category</h3>
+    </div>
+    <div class="category">
+        <a href="#">
+            <article>
+                <div class="left"><i class="fa fa-calculator"></i></div>
+                <div class="right">
+                    <p class="title">Accounting &amp; Finance</p>
+                    <p class="second">2590 open positions</p>
+                </div>
+            </article>
+        </a>
+        <a href="#">
+            <article>
+                <div class="left"><i class="fa fa-balance-scale"></i></div>
+                <div class="right">
+                    <p class="title">Sales &amp; Marketing</p>
+                    <p class="second">1214 open positions</p>
+                </div>
+            </article>
+        </a>
+        <a href="#">
+            <article>
+                <div class="left"><i class="fa fa-globe"></i></div>
+                <div class="right">
+                    <p class="title">Information Technology</p>
+                    <p class="second">122 open positions</p>
+                </div>
+            </article>
+        </a>
+        <a href="#">
+            <article>
+                <div class="left"><i class="fa fa-coffee"></i></div>
+                <div class="right">
+                    <p class="title">Food Service</p>
+                    <p class="second">2 open positions</p>
+                </div>
+            </article>
+        </a>
+        <a href="#">
+            <article>
+                <div class="left">
+                    <i class="fa fa-picture-o"></i>
+                </div>
+                <div class="right">
+                    <p class="title">Design &amp; Art</p>
+                    <p class="second">39 open positions</p>
+                </div>
+            </article>
+        </a>
+        <a href="#">
+            <article>
+                <div class="left"><i class="fa fa-automobile"></i></div>
+                <div class="right">
+                    <p class="title">Automotive</p>
+                    <p class="second">215 open positions</p>
+                </div>
+            </article>
+        </a>
+        <a href="#">
+            <article>
+                <div class="left"><i class="fa fa-heart"></i></div>
+                <div class="right">
+                    <p class="title">Healthcare</p>
+                    <p class="second">1653 open positions</p>
+                </div>
+            </article>
+        </a>
+        <a href="#">
+            <article>
+                <div class="left"><i class="fa fa-building"></i></div>
+                <div class="right">
+                    <p class="title">Construction</p>
+                    <p class="second">392 open positions</p>
+                </div>
+            </article>
+        </a>
+    </div>
+    <div class="text-center">
+        <a class="button" href="#">Browse all categories</a>
+    </div>
+</div>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

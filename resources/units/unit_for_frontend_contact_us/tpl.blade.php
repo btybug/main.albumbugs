ecddb2236a1360893b010ab5b7ed56e64a88f2f1
sahakavatar <hakobyan.sahak88@gmail.com>
2018-04-26 12:49:50 +0400

    unit contact us

---
+++ resources/units/unit_for_frontend_contact_us/tpl.blade.php
@@ -0,0 +1,99 @@
+<div class="contact_us">
+    <h3>Testimonials</h3>
+    <h4>Details</h4>
+    <p class="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci autem commodi
+        dicta enim
+        eum exercitationem.</p>
+    <div class="all-item">
+        <div class="item">
+            <div class="item-inner">
+                <div class="unit">
+                    <div class="icon">
+                        <i class="fa fa-phone"></i>
+                    </div>
+                    <div class="unit-body">
+                        <p>Phones</p>
+                        <ul>
+                            <li>
+                                <dl>
+                                    <dt>Phone 01</dt>
+                                    <dd><a href="tel:#">+0 (000) 000-0000</a></dd>
+                                </dl>
+                            </li>
+                            <li>
+                                <dl>
+                                    <dt>Phone 02</dt>
+                                    <dd><a href="tel:#">+0 (000) 000-0000</a></dd>
+                                </dl>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item-inner">
+                <div class="unit">
+                    <div class="icon">
+                        <i class="fa fa-location-arrow"></i>
+                    </div>
+                    <div class="unit-body">
+                        <p>ADDRESS</p>
+                        <ul>
+                            <li>
+                                <a href="#">Lorem address, <br> ipsum adress 2000</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item-inner">
+                <div class="unit">
+                    <div class="icon">
+                        <i class="fa fa-envelope-o"></i>
+                    </div>
+                    <div class="unit-body">
+                        <p>E-MAILS</p>
+                        <ul>
+                            <li><a href="mailto:#">lorem1@lorem.org</a></li>
+                            <li><a href="mailto:#">lorem2@lorem.org</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h4>Get in touch with us</h4>
+    <p class="subtitle">Lorem ipsum dolor sit amet, consectetur.</p>
+    <form action="">
+        <div class="row">
+            <div class="col-sm-4 col-xs-12">
+                <div class="form-group">
+                    <input class="form-control" type="text" name="" placeholder="Enter Your Name...">
+                </div>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <div class="form-group">
+                    <input class="form-control" type="text" name="" placeholder="E-mail">
+                </div>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <div class="form-group">
+                    <input class="form-control" type="text" name="" placeholder="Phone">
+                </div>
+            </div>
+            <div class="col-xs-12">
+                <div class="form-group">
+                    <textarea class="form-control area" name="" id="" cols="30" rows="10"
+                              placeholder="Your Message"></textarea>
+                </div>
+            </div>
+            <div class="col-xs-12">
+                <button type="submit" class="button">Send Message</button>
+            </div>
+        </div>
+    </form>
+</div>
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}
---
+++ resources/units/unit_for_frontend_testimonials/tpl.blade.php
@@ -0,0 +1,123 @@
+<div class="testimonials">
+    <h3>Testimonials</h3>
+    <div class="block">
+        <div class="row">
+            <div class="col-sm-6 col-lg-4">
+                <blockquote>
+                    <span class="icon"><i class="fa fa-thumb-tack"></i></span>
+                    <div class="text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo incidunt nesciunt provident
+                            tempore velit? A architecto aspernatur, aut corporis cumque, cupiditate doloribus eaque
+                            eveniet modi possimus quibusdam ratione repudiandae tenetur!</p>
+                    </div>
+                    <div class="meta">
+                        <img class="avatar"
+                             src="http://skygroup.in.ua/wp-content/uploads/2017/01/Faces-400x400px-1_1_30-thegem-person-256x256.jpg"
+                             alt="">
+                        <div class="info">
+                            <cite class="title">Jhon Jhony</cite>
+                            <p class="subtitle">Developer</p>
+                        </div>
+                    </div>
+                </blockquote>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+                <blockquote>
+                    <span class="icon"><i class="fa fa-thumb-tack"></i></span>
+                    <div class="text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo incidunt nesciunt provident
+                            tempore velit? A architecto aspernatur, aut corporis cumque, cupiditate doloribus eaque
+                            eveniet modi possimus quibusdam ratione repudiandae tenetur!</p>
+                    </div>
+                    <div class="meta">
+                        <img class="avatar"
+                             src="http://skygroup.in.ua/wp-content/uploads/2017/01/Faces-400x400px-1_1_30-thegem-person-256x256.jpg"
+                             alt="">
+                        <div class="info">
+                            <cite class="title">Jhon Jhony</cite>
+                            <p class="subtitle">Developer</p>
+                        </div>
+                    </div>
+                </blockquote>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+                <blockquote>
+                    <span class="icon"><i class="fa fa-thumb-tack"></i></span>
+                    <div class="text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo incidunt nesciunt provident
+                            tempore velit? A architecto aspernatur, aut corporis cumque, cupiditate doloribus eaque
+                            eveniet modi possimus quibusdam ratione repudiandae tenetur!</p>
+                    </div>
+                    <div class="meta">
+                        <img class="avatar"
+                             src="http://skygroup.in.ua/wp-content/uploads/2017/01/Faces-400x400px-1_1_30-thegem-person-256x256.jpg"
+                             alt="">
+                        <div class="info">
+                            <cite class="title">Jhon Jhony</cite>
+                            <p class="subtitle">Developer</p>
+                        </div>
+                    </div>
+                </blockquote>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+                <blockquote>
+                    <span class="icon"><i class="fa fa-thumb-tack"></i></span>
+                    <div class="text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo incidunt nesciunt provident
+                            tempore velit? A architecto aspernatur, aut corporis cumque, cupiditate doloribus eaque
+                            eveniet modi possimus quibusdam ratione repudiandae tenetur!</p>
+                    </div>
+                    <div class="meta">
+                        <img class="avatar"
+                             src="http://skygroup.in.ua/wp-content/uploads/2017/01/Faces-400x400px-1_1_30-thegem-person-256x256.jpg"
+                             alt="">
+                        <div class="info">
+                            <cite class="title">Jhon Jhony</cite>
+                            <p class="subtitle">Developer</p>
+                        </div>
+                    </div>
+                </blockquote>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+                <blockquote>
+                    <span class="icon"><i class="fa fa-thumb-tack"></i></span>
+                    <div class="text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo incidunt nesciunt provident
+                            tempore velit? A architecto aspernatur, aut corporis cumque, cupiditate doloribus eaque
+                            eveniet modi possimus quibusdam ratione repudiandae tenetur!</p>
+                    </div>
+                    <div class="meta">
+                        <img class="avatar"
+                             src="http://skygroup.in.ua/wp-content/uploads/2017/01/Faces-400x400px-1_1_30-thegem-person-256x256.jpg"
+                             alt="">
+                        <div class="info">
+                            <cite class="title">Jhon Jhony</cite>
+                            <p class="subtitle">Developer</p>
+                        </div>
+                    </div>
+                </blockquote>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+                <blockquote>
+                    <span class="icon"><i class="fa fa-thumb-tack"></i></span>
+                    <div class="text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo incidunt nesciunt provident
+                            tempore velit? A architecto aspernatur, aut corporis cumque, cupiditate doloribus eaque
+                            eveniet modi possimus quibusdam ratione repudiandae tenetur!</p>
+                    </div>
+                    <div class="meta">
+                        <img class="avatar"
+                             src="http://skygroup.in.ua/wp-content/uploads/2017/01/Faces-400x400px-1_1_30-thegem-person-256x256.jpg"
+                             alt="">
+                        <div class="info">
+                            <cite class="title">Jhon Jhony</cite>
+                            <p class="subtitle">Developer</p>
+                        </div>
+                    </div>
+                </blockquote>
+            </div>
+        </div>
+    </div>
+
+</div>
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

90545170fbcfe01207b2639014512b28ffd785ab
Sahak <hakobyan.sahak88@gmail.com>
2018-04-30 16:49:54 +0400

    up

--- vendor/btybug/frontsite/src/Resources/Views/panels/css.blade.php
+++ vendor/btybug/frontsite/src/Resources/Views/panels/css.blade.php
@@ -1,4 +1,5 @@
 <div>
     @if(!File::exists(public_path('css'.DS.'pages'.DS.str_replace(' ','-',$page->title).'.css')))
     {!! str_replace(' ','-',$page->title).'.css' !!}
+        @endif
 </div>

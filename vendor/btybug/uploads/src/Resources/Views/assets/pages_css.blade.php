a469ba56806179287f33db827e2c6a0284b059ff
midoalone <mido_alone2001@yahoo.com>
2018-04-30 14:40:45 +0200

    pages js

--- vendor/btybug/uploads/src/Resources/Views/assets/pages_css.blade.php
+++ vendor/btybug/uploads/src/Resources/Views/assets/pages_css.blade.php
@@ -1,6 +1,23 @@
 @extends('btybug::layouts.mTabs',['index'=>'upload_assets'])
 @section('tab')
 
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Page CSS</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>home.css</td>
+            <td>
+                <button class="btn btn-danger">View</button>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+
 @stop
 @section('CSS')
 @stop

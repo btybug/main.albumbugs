af210d3ade308bfa21c5d8782c968c654e37368e
Sahak <hakobyan.sahak88@gmail.com>
2018-04-30 16:51:33 +0400

    up

--- vendor/btybug/frontsite/src/Resources/Views/panels/css.blade.php
+++ vendor/btybug/frontsite/src/Resources/Views/panels/css.blade.php
@@ -1,5 +1,5 @@
 <div>
-    @if(!File::exists(public_path('css'.DS.'pages'.DS.str_replace(' ','-',$page->title).'.css')))
+    @if(File::exists(public_path('css'.DS.'pages'.DS.str_replace(' ','-',$page->title).'.css')))
     {!! str_replace(' ','-',$page->title).'.css' !!}
         @endif
 </div>

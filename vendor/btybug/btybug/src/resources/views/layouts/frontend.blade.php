d9d59af7d2e1215efbbabb809ebafe55be2d7f70
terakyan <edo.terakyan@gmail.com>
2018-04-30 15:01:53 +0400

    at

--- vendor/btybug/btybug/src/resources/views/layouts/frontend.blade.php
+++ vendor/btybug/btybug/src/resources/views/layouts/frontend.blade.php
@@ -8,41 +8,11 @@
 @endphp
 <html lang="@yield('locale')">
 <head>
-    @include("btybug::layouts._partials.head")
+    @include("btybug::layouts._partials.frontend.head")
 </head>
 <body>
 <div class="full_page">
-    @if (isset($errors) && count($errors) > 0)
-        <div class="alert alert-danger" role="alert">
-            <button type="button" class="close" data-dismiss="alert"
-                    aria-label="Close"><span aria-hidden="true">&times;</span>
-            </button>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{!! $error !!}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    @if (session('flash.message') != null)
-        <div class="flash alert {{ Session::has('flash.class') ? session('flash.class') : 'alert-success' }}"
-             role="alert">
-            <button type="button" class="close" data-dismiss="alert"
-                    aria-label="Close"><span aria-hidden="true">&times;</span>
-            </button>
-            {!! session('flash.message') !!}
-        </div>
-    @endif
 
-    @if(Session::has('message'))
-        <div class="m-t-10 alert {{ Session::get('alert-class', 'alert-info') }} alert-dismissible"
-             role="alert">
-            <button type="button" class="close" data-dismiss="alert"
-                    aria-label="Close"><span aria-hidden="true">&times;</span>
-            </button>
-            {!! Session::get('message') !!}
-        </div>
-    @endif
     <div>
         @yield('content')
     </div>

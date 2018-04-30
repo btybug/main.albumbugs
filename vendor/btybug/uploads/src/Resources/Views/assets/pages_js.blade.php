6f9876aaab20a25523ac629100f2d0b5e5d432d2
midoalone <mido_alone2001@yahoo.com>
2018-04-30 13:57:28 +0200

    pages js

--- vendor/btybug/uploads/src/Resources/Views/assets/pages_js.blade.php
+++ vendor/btybug/uploads/src/Resources/Views/assets/pages_js.blade.php
@@ -1,6 +1,87 @@
 @extends('btybug::layouts.mTabs',['index'=>'upload_assets'])
 @section('tab')
 
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>File name</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr><th colspan="2">Page 1 Title</th></tr>
+        <tr>
+            <td>bootstrap.css</td>
+            <td>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> Keep Inside
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> Update
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"> Ignore
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio4" value="option4"> Keep Outside
+                </label>
+            </td>
+        </tr>
+        <tr>
+            <td>fontawsome.css</td>
+            <td>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> Keep Inside
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> Update
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"> Ignore
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio4" value="option4"> Keep Outside
+                </label>
+            </td>
+        </tr>
+        <tr><th colspan="2">Page 2 Title</th></tr>
+        <tr>
+            <td>bootstrap.css</td>
+            <td>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> Keep Inside
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> Update
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"> Ignore
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio4" value="option4"> Keep Outside
+                </label>
+            </td>
+        </tr>
+        <tr>
+            <td>fontawsome.css</td>
+            <td>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> Keep Inside
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> Update
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"> Ignore
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="inlineRadioOptions" id="inlineRadio4" value="option4"> Keep Outside
+                </label>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+
 @stop
 @section('CSS')
 @stop

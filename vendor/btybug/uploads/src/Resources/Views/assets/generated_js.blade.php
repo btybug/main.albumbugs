c3091630ab6e9dfcb11bf9febdc64227f50279ab
Eduard <edo.terakyan@gmail.com>
2018-05-05 20:19:17 +0400

    panels

--- vendor/btybug/uploads/src/Resources/Views/assets/generated_js.blade.php
+++ vendor/btybug/uploads/src/Resources/Views/assets/generated_js.blade.php
@@ -1,5 +1,30 @@
 @extends('btybug::layouts.mTabs',['index'=>'upload_assets'])
 @section('tab')
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    Header JS
+                </h4>
+            </div>
+            <div class="panel-body">
+                
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-12 m-t-15">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    Footer JS
+                </h4>
+            </div>
+            <div class="panel-body">
+
+            </div>
+        </div>
+    </div>
 @stop
 
 @section('CSS')

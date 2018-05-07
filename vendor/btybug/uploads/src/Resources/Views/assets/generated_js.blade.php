77474e896d087b90757c5f0638b9c52ac1f64db6
Eduard <edo.terakyan@gmail.com>
2018-05-07 23:43:45 +0400

    header front panel

--- vendor/btybug/uploads/src/Resources/Views/assets/generated_js.blade.php
+++ vendor/btybug/uploads/src/Resources/Views/assets/generated_js.blade.php
@@ -11,28 +11,40 @@
                 </h4>
             </div>
             <div class="panel-body">
-                <ul data-nav-drag="" id="menus-list" class="sortable ui-sortable ui-droppable dvmin-height">
-                    @if(count($assets))
-                        @foreach($assets as $item)
-                            <li class="list-group-item" data-id="{{$item->path}}"
-                                data-name="{{ get_filename_from_path($item->path,DS) }}" data-link="{{ $item->path }}"
-                                data-type="{{$item->type}}">
 
-                                {{ get_filename_from_path($item->path,DS) }} (unit: {{ $item->page->slug }})
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            front-header.js
+                        </h4>
+                    </div>
+                    <div class="panel-body">
+                        <ul data-nav-drag="" id="menus-list" class="sortable ui-sortable ui-droppable dvmin-height">
+                            @if(count($assets))
+                                @foreach($assets as $item)
+                                    <li class="list-group-item" data-id="{{$item->path}}"
+                                        data-name="{{ get_filename_from_path($item->path,DS) }}" data-link="{{ $item->path }}"
+                                        data-type="{{$item->type}}">
 
-                                <label class="radio-inline">
-                                    <input type="radio" name="inlineRadioOptions[{{$item->id}}]" id="inlineRadio1" checked value="option1"> Keep Inside
-                                </label>
-                                <label class="radio-inline">
-                                    <input type="radio" name="inlineRadioOptions[{{$item->id}}]" id="inlineRadio2" value="option2"> Update
-                                </label>
-                                <label class="radio-inline">
-                                    <input type="radio" name="inlineRadioOptions[{{$item->id}}]" id="inlineRadio3" value="option3"> Ignore
-                                </label>
-                            </li>
-                        @endforeach
-                    @endif
-                </ul>
+                                        {{ get_filename_from_path($item->path,DS) }} (unit: {{ $item->page->slug }})
+
+                                        <label class="radio-inline">
+                                            <input type="radio" name="inlineRadioOptions[{{$item->id}}]" id="inlineRadio1" checked value="option1"> Keep Inside
+                                        </label>
+                                        <label class="radio-inline">
+                                            <input type="radio" name="inlineRadioOptions[{{$item->id}}]" id="inlineRadio2" value="option2"> Update
+                                        </label>
+                                        <label class="radio-inline">
+                                            <input type="radio" name="inlineRadioOptions[{{$item->id}}]" id="inlineRadio3" value="option3"> Ignore
+                                        </label>
+                                    </li>
+                                @endforeach
+                            @endif
+                        </ul>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     </div>

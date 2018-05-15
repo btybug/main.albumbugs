143a8571b39904c6eb5dacb41ea72c9b797a346c
sahakavatar <hakobyan.sahak88@gmail.com>
2018-05-15 14:18:37 +0400

    units

---
+++ resources/units/unit_for_about_us_our_specialization/demo.blade.php
@@ -0,0 +1,80 @@
+<div class="container">
+    <div class="about-us-others-say">
+
+        <div class="row">
+            <div class="col-sm-6 col-xs-12">
+                <div class="left">
+                    <img src="https://kriesi.at/themes/enfold/wp-content/uploads/sites/37/2013/04/photodune-202599-smart-business-man-with-a-laptop-and-colleagues-at-the-back-s-495x400.jpg"
+                         alt="img">
+                </div>
+            </div>
+            <div class="col-sm-6 col-xs-12">
+                <div class="right">
+                    <div class="heading">
+                        <h3>Welcome at our Agency</h3>
+                        <div class="heading-border">
+                            <div class="inner"></div>
+                        </div>
+                    </div>
+                    <div class="text">
+                        <p>
+                            Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
+                            parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
+                            pretium quis, sem.Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+                        </p>
+                    </div>
+                    <div class="heading second">
+                        <h3>Our specialization</h3>
+                        <div class="heading-border">
+                            <div class="inner"></div>
+                        </div>
+                    </div>
+                    <div class="progress-bar-container">
+                        <div class="prog-bar">
+                            <div class="progressbar-title-wrap">
+                                <div class="progressbar-title">HTML</div>
+                            </div>
+                            <div class="progress">
+                                <div class="bar-outer">
+                                    <div class="bar bar-html"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="prog-bar">
+                            <div class="progressbar-title-wrap">
+                                <div class="progressbar-title">CSS</div>
+                            </div>
+                            <div class="progress">
+                                <div class="bar-outer">
+                                    <div class="bar bar-css"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="prog-bar blue-bar">
+                            <div class="progressbar-title-wrap">
+                                <div class="progressbar-title">Javascript</div>
+                            </div>
+                            <div class="progress">
+                                <div class="bar-outer">
+                                    <div class="bar bar-js"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="prog-bar blue-bar">
+                            <div class="progressbar-title-wrap">
+                                <div class="progressbar-title">PHP</div>
+                            </div>
+                            <div class="progress">
+                                <div class="bar-outer">
+                                    <div class="bar bar-php"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css',$_this) !!}
---
+++ resources/units/unit_for_about_us_what_others_say/tpl.blade.php
@@ -0,0 +1,122 @@
+<div class="about-us-others-say">
+    <h2>WHAT OTHERS SAY ABOUT US</h2>
+    <div class="hr">
+            <span class="inner ">
+                <span class="inner-style"></span>
+            </span>
+    </div>
+    <div class="row">
+        <div class="col-sm-6 col-xs-12 bord-left">
+            <div class="member">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <div class="image">
+                            <img src="https://kriesi.at/themes/enfold/wp-content/uploads/sites/37/2013/04/team-1-180x180.jpg"
+                                 alt="img">
+                        </div>
+                    </div>
+                    <div class="col-xs-9">
+                        <div class="content">
+                            <p>Nulla consequat massa quis enim. Donec pede
+                                justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+                                imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer
+                                tincidunt. Cras dapibus.</p>
+                            <div class="meta">
+                                <strong class="name">Martha M. Masters</strong>
+                                <div class="subtitle">Marketing - <span><a href="#">WikiTravel</a></span>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-sm-6 col-xs-12 bord-right">
+            <div class="member">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <div class="image">
+                            <img src="https://kriesi.at/themes/enfold/wp-content/uploads/sites/37/2013/04/team-1-180x180.jpg"
+                                 alt="img">
+                        </div>
+                    </div>
+                    <div class="col-xs-9">
+                        <div class="content">
+                            <p>Nulla consequat massa quis enim. Donec pede
+                                justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,</p>
+                            <div class="meta">
+                                <strong class="name">Martha M. Masters</strong>
+                                <div class="subtitle">Marketing - <span><a href="#">WikiTravel</a></span>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-sm-6 col-xs-12 bord-left no-bord">
+            <div class="member">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <div class="image">
+                            <img src="https://kriesi.at/themes/enfold/wp-content/uploads/sites/37/2013/04/team-1-180x180.jpg"
+                                 alt="img">
+                        </div>
+                    </div>
+                    <div class="col-xs-9">
+                        <div class="content">
+                            <p>Nulla consequat massa quis enim. Donec pede
+                                justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+                                imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer
+                                tincidunt. Cras dapibus.</p>
+                            <div class="meta">
+                                <strong class="name">Martha M. Masters</strong>
+                                <div class="subtitle">Marketing - <span><a href="#">WikiTravel</a></span>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-sm-6 col-xs-12 bord-right no-bord">
+            <div class="member">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <div class="image">
+                            <img src="https://kriesi.at/themes/enfold/wp-content/uploads/sites/37/2013/04/team-1-180x180.jpg"
+                                 alt="img">
+                        </div>
+                    </div>
+                    <div class="col-xs-9">
+                        <div class="content">
+                            <p>Nulla consequat massa quis enim. Donec pede
+                                justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+                                imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer
+                                tincidunt. Cras dapibus.</p>
+                            <div class="meta">
+                                <strong class="name">Martha M. Masters</strong>
+                                <div class="subtitle">Marketing - <span><a href="#">WikiTravel</a></span>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css',$_this) !!}

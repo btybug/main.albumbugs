471305e4a8e0f7ec162c6b66b793d2330febbf69
sahakavatar <hakobyan.sahak88@gmail.com>
2018-05-15 11:29:04 +0400

    team

---
+++ resources/units/unit_for_about_us_team/demo.blade.php
@@ -0,0 +1,143 @@
+<div class="container">
+    <div class="about-us-team">
+        <h2>MEET THE TEAM</h2>
+        <p>We are a team of dedicated professionals, ready to do what ever it takes to make your business grow.</p>
+        <div class="row">
+            <div class="col-sm-4 col-xs-12">
+                <div class="member">
+                    <div class="team-img-container">
+                        <img class="image-team" src="https://kriesi.at/themes/enfold/files/2013/04/team-2-300x300.jpg"
+                             alt="Frank Furious">
+                        <div class="team-social">
+                            <div class="team-social-inner">
+                                <a href="http://www.linkedin.com/"><i class="fa fa-linkedin"></i></a>
+                                <a href="http://www.facebook.com"><i class="fa fa-facebook-f"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <h3 class="name" itemprop="name">Frank Furious</h3>
+                    <div class="job-title">Art Director</div>
+                    <div class="description">
+                        <p>Cum sociis natoque penatibus et
+                            magnis dis parturient montes, nascetur ridiculus mus. <strong>Lorem ipsum dolor</strong>
+                            sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <div class="member">
+                    <div class="team-img-container">
+                        <img class="image-team" src="https://kriesi.at/themes/enfold/files/2013/04/team-3-300x300.jpg"
+                             alt="Frank Furious">
+                        <div class="team-social">
+                            <div class="team-social-inner">
+                                <a href="http://www.linkedin.com/"><i class="fa fa-linkedin"></i></a>
+                                <a href="http://www.facebook.com"><i class="fa fa-facebook-f"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <h3 class="name" itemprop="name">Frank Furious</h3>
+                    <div class="job-title">Art Director</div>
+                    <div class="description">
+                        <p>Cum sociis natoque penatibus et
+                            magnis dis parturient montes, nascetur ridiculus mus. <strong>Lorem ipsum dolor</strong>
+                            sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <div class="member">
+                    <div class="team-img-container">
+                        <img class="image-team" src="https://kriesi.at/themes/enfold/files/2013/04/team-2-300x300.jpg"
+                             alt="Frank Furious">
+                        <div class="team-social">
+                            <div class="team-social-inner">
+                                <a href="http://www.linkedin.com/"><i class="fa fa-linkedin"></i></a>
+                                <a href="http://www.facebook.com"><i class="fa fa-facebook-f"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <h3 class="name" itemprop="name">Frank Furious</h3>
+                    <div class="job-title">Art Director</div>
+                    <div class="description">
+                        <p>Cum sociis natoque penatibus et
+                            magnis dis parturient montes, nascetur ridiculus mus. <strong>Lorem ipsum dolor</strong>
+                            sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <div class="member">
+                    <div class="team-img-container">
+                        <img class="image-team" src="https://kriesi.at/themes/enfold/files/2013/04/team-2-300x300.jpg"
+                             alt="Frank Furious">
+                        <div class="team-social">
+                            <div class="team-social-inner">
+                                <a href="http://www.linkedin.com/"><i class="fa fa-linkedin"></i></a>
+                                <a href="http://www.facebook.com"><i class="fa fa-facebook-f"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <h3 class="name" itemprop="name">Frank Furious</h3>
+                    <div class="job-title">Art Director</div>
+                    <div class="description">
+                        <p>Cum sociis natoque penatibus et
+                            magnis dis parturient montes, nascetur ridiculus mus. <strong>Lorem ipsum dolor</strong>
+                            sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <div class="member">
+                    <div class="team-img-container">
+                        <img class="image-team" src="https://kriesi.at/themes/enfold/files/2013/04/team-3-300x300.jpg"
+                             alt="Frank Furious">
+                        <div class="team-social">
+                            <div class="team-social-inner">
+                                <a href="http://www.linkedin.com/"><i class="fa fa-linkedin"></i></a>
+                                <a href="http://www.facebook.com"><i class="fa fa-facebook-f"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <h3 class="name" itemprop="name">Frank Furious</h3>
+                    <div class="job-title">Art Director</div>
+                    <div class="description">
+                        <p>Cum sociis natoque penatibus et
+                            magnis dis parturient montes, nascetur ridiculus mus. <strong>Lorem ipsum dolor</strong>
+                            sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <div class="member">
+                    <div class="team-img-container">
+                        <img class="image-team" src="https://kriesi.at/themes/enfold/files/2013/04/team-2-300x300.jpg"
+                             alt="Frank Furious">
+                        <div class="team-social">
+                            <div class="team-social-inner">
+                                <a href="http://www.linkedin.com/"><i class="fa fa-linkedin"></i></a>
+                                <a href="http://www.facebook.com"><i class="fa fa-facebook-f"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <h3 class="name" itemprop="name">Frank Furious</h3>
+                    <div class="job-title">Art Director</div>
+                    <div class="description">
+                        <p>Cum sociis natoque penatibus et
+                            magnis dis parturient montes, nascetur ridiculus mus. <strong>Lorem ipsum dolor</strong>
+                            sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css',$_this) !!}

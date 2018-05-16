0e6131aff1df33d3a20f3f176fbdd45fe96075b4
sahakavatar <hakobyan.sahak88@gmail.com>
2018-05-16 15:41:53 +0400

    unit blog

---
+++ resources/units/unit_felicia_blog/tpl.blade.php
@@ -0,0 +1,90 @@
+<section class="section-felic-blog">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="section-title text-center">
+                <h2 class="title separator">BLOG</h2>
+                <div class="sub-title">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+            </div>
+        </div>
+    </div>
+    <div class="row m-bot-50">
+        <div class="col-md-4">
+            <div class="blog-wrap">
+                <a href="#"><img class="img-responsive radius6"
+                                 src="https://i2-prod.mirror.co.uk/incoming/article11736965.ece/ALTERNATES/s615/Beautiful-woman-with-make-up.jpg"
+                                 alt=""></a>
+                <div class="post-header">
+                    <div class="cat">
+                        <ul class="post-categories">
+                            <li><a href="#" rel="category tag">Technology</a></li>
+                            <li><a href="#" rel="category tag">World</a></li>
+                        </ul>
+                    </div>
+                    <h4><a href="#">The impossibility of intelligence explosion</a></h4>
+                    <div class="post-auth-date">
+                        <a href="#">John Doe</a> | <a href="#">April 10, 2018</a>
+                    </div>
+                </div>
+                <div class="post-blog">
+                    <p>Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers
+                        sadips amets. Lid est quidem rerum facilis dolores nemis omnis fugats vitaes nemo</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="blog-wrap">
+                <a href="#" class=""><img class="img-responsive radius6"
+                                          src="https://i.pinimg.com/originals/0b/f2/1e/0bf21e3949063783453089b6f83f96d5.jpg"
+                                          alt=""></a>
+                <div class="post-header">
+                    <div class="cat">
+                        <ul class="post-categories">
+                            <li><a href="#" rel="category tag">Business</a></li>
+                            <li><a href="#" rel="category tag">Economy</a></li>
+                        </ul>
+                    </div>
+                    <h4><a href="#">The Future of Technology in the Automotive Industry</a></h4>
+                    <div class="post-auth-date">
+                        <a href="#">Martin Smith</a> | <a href="#">February 14, 2018</a>
+                    </div>
+                </div>
+                <div class="post-blog">
+                    <p>Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers
+                        sadips amets. Lid est quidem rerum facilis dolores nemis omnis fugats vitaes nemo</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="blog-wrap">
+                <a href="#" class=""><img class="img-responsive radius6"
+                                          src="http://www.eraeveriut.com/wp-content/uploads/2017/03/beauty-tips-for-monsoon.jpg"
+                                          alt=""></a>
+                <div class="post-header">
+                    <div class="cat">
+                        <ul class="post-categories">
+                            <li><a href="#" rel="category tag">World</a></li>
+                            <li><a href="#" rel="category tag">Sports</a></li>
+                        </ul>
+                    </div>
+                    <h4><a href="#"> The Multibillion-Dollar Online Crime Industry</a></h4>
+                    <div class="post-auth-date">
+                        <a href="#">Martin Smith</a> | <a href="#">August 14, 2018</a>
+                    </div>
+                </div>
+                <div class="post-blog">
+                    <p>Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers
+                        sadips amets. Lid est quidem rerum facilis dolores nemis omnis fugats vitaes nemo</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <a href="#" class="btn-link">VIEW ALL BLOGS</a>
+        </div>
+    </div>
+</section>
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css',$_this) !!}

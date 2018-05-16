293936dc520e9653b7b715c78844be989b984eba
sahakavatar <hakobyan.sahak88@gmail.com>
2018-05-16 17:40:30 +0400

    unit factors

---
+++ resources/units/unit_felicia_factors/demo.blade.php
@@ -0,0 +1,40 @@
+<section class="section-felic-factors">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="section-title text-center">
+                    <h2 class="title separator"> FUN FACTORS</h2>
+                    <div class="sub-title">Contrary to popular belief, Lorem Ipsum is not simply random text</div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="fun-box text-center fun-separator fun-separator-blue">
+                    <div class="value">10</div>
+                    <div class="title">ONE PAGE DEMO</div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="fun-box text-center fun-separator fun-separator-red">
+                    <div class="value">25</div>
+                    <div class="title">PROJECTS DELIVERED</div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="fun-box text-center fun-separator fun-separator-green ">
+                    <div class="value">2312</div>
+                    <div class="title">SATISFIED CLIENTS</div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="fun-box text-center fun-separator fun-separator-yellow">
+                    <div class="value">5</div>
+                    <div class="title">WINING AWARDS</div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css',$_this) !!}

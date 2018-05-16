01d0a247d0ce6d8c26e0827d08ff0ad34cb175ce
sahakavatar <hakobyan.sahak88@gmail.com>
2018-05-16 12:42:57 +0400

    unit about

---
+++ resources/units/unit_felicia_about/demo.blade.php
@@ -0,0 +1,98 @@
+<section class="section-felic-about">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title">
+                    <h2 class="title text-uppercase separator"> About Onepage</h2>
+                    <div class="sub-title">
+                        We’re a creative agency located in New York.<br>
+                        we're very passionate about our work and love to create awesome products.
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+                <div class="services-list text-left">
+                    <div class="services-icon text-primary">
+                        <i class="fa fa-newspaper-o" aria-hidden="true"></i>
+                    </div>
+                    <div class="services-desk">
+                        <h4 class="services-title">
+                            Web Solution
+                        </h4>
+                        <p class="services-info">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="services-list text-left">
+                    <div class="services-icon text-danger">
+                        <i class="fa fa-credit-card" aria-hidden="true"></i>
+                    </div>
+                    <div class="services-desk">
+                        <h4 class="services-title">
+                            Corporate Branding
+                        </h4>
+                        <p class="services-info">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="services-list text-left">
+                    <div class="services-icon text-success">
+                        <i class="fa fa-globe" aria-hidden="true"></i>
+                    </div>
+                    <div class="services-desk">
+                        <h4 class="services-title">
+                            Web Security
+                        </h4>
+                        <p class="services-info">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="services-list text-left">
+                    <div class="services-icon text-warning">
+                        <i class="fa fa-tablet" aria-hidden="true"></i>
+                    </div>
+                    <div class="services-desk">
+                        <h4 class="services-title">
+                            App Development
+                        </h4>
+                        <p class="services-info">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="services-list text-left">
+                    <div class="services-icon text-info">
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                    </div>
+                    <div class="services-desk">
+                        <h4 class="services-title">
+                            Search Engine Optimaization
+                        </h4>
+                        <p class="services-info">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="services-list text-left">
+                    <div class="services-icon">
+                        <i class="fa fa-server" aria-hidden="true"></i>
+                    </div>
+                    <div class="services-desk">
+                        <h4 class="services-title">
+                            Data Analysis
+                        </h4>
+                        <p class="services-info">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css',$_this) !!}

ed02cda63980b312960c62c051766d519e682215
sahakavatar <hakobyan.sahak88@gmail.com>
2018-05-16 17:05:29 +0400

    unit subscribe

---
+++ resources/units/unit_felicia_subscribe/tpl.blade.php
@@ -0,0 +1,25 @@
+<section class="section-felic-subscribe">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="section-title text-center">
+                <h2 class="title separator text-uppercase">SUBSCRIBE FOR NEWSLETTER</h2>
+                <div class="sub-title">subscribe to get all newsletter and publication</div>
+            </div>
+        </div>
+
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel-subscribe">
+                <form class="subscribe-from form-inline row">
+                    <div class="form-group col-sm-8">
+                        <input type="email" class="form-control" placeholder="Enter your email address">
+                    </div>
+                    <div class="form-group col-sm-4">
+                        <button type="submit" class="btn btn-block">subscription</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css',$_this) !!}

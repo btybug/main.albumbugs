18a7a76df0763020a8dd18f3e59d999bc0dac327
sahakavatar <hakobyan.sahak88@gmail.com>
2018-05-16 13:59:12 +0400

    unit team

---
+++ resources/units/unit_felicia_team/tpl.blade.php
@@ -0,0 +1,116 @@
+<section class="section-felic-team">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="section-title text-center">
+                    <h2 class="title separator">OUR TEAM</h2>
+                    <div class="sub-title">Lorem Ipsum is simply dummy text of the printing and typesetting industry</div>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-6 team-member">
+                <div class="thumb">
+                    <img src="https://www.alvinailey.org/sites/default/files/styles/slideshow_image/public/melanie-person.jpg" alt="">
+                    <div class="team-info">
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin. Lorem ipsum dolor sit amet consiquest lirba erose
+                    </div>
+                </div>
+                <h3 class="team-title">John Doe</h3>
+                <div class="team-designation">
+                    Chief Operating Officer
+                </div>
+                <div class="team-socail-links">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-youtube"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                    <a href="#"><i class="fa fa-dribbble"></i></a>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-6 team-member">
+                <div class="thumb">
+                    <img src="http://img.timeinc.net/time/daily/2012/1211/poy_benbernanke.jpg" alt="">
+                    <div class="team-info">
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin. Lorem ipsum dolor sit amet consiquest lirba erose
+                    </div>
+                </div>
+                <h3 class="team-title">Mr Dumble</h3>
+                <div class="team-designation">
+                    Business Analyst
+                </div>
+                <div class="team-socail-links">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-youtube"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                    <a href="#"><i class="fa fa-dribbble"></i></a>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-6 team-member">
+                <div class="thumb">
+                    <img src="https://s3.amazonaws.com/abn-prod/wp-content/uploads/sites/3/2017/04/gretchen_carlson.jpg" alt="">
+                    <div class="team-info">
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin. Lorem ipsum dolor sit amet consiquest lirba erose
+                    </div>
+                </div>
+                <h3 class="team-title">Mark Henry</h3>
+                <div class="team-designation">
+                    Marketing Officer
+                </div>
+                <div class="team-socail-links">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-youtube"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                    <a href="#"><i class="fa fa-dribbble"></i></a>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-6 team-member">
+                <div class="thumb">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/5/52/Sheila_Nevins_Headshot_2016-Photograph_by_Brigitte_Lacombe.jpg" alt="">
+                    <div class="team-info">
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin. Lorem ipsum dolor sit amet consiquest lirba erose
+                    </div>
+                </div>
+                <h3 class="team-title">Ritchi Rian</h3>
+                <div class="team-designation">
+                    HR Manger
+                </div>
+                <div class="team-socail-links">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-youtube"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                    <a href="#"><i class="fa fa-dribbble"></i></a>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-6 team-member">
+                <div class="thumb">
+                    <img src="https://amp.businessinsider.com/images/57cdb88d8a4565e850e88c2c-750-750.jpg" alt="">
+                    <div class="team-info">
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin. Lorem ipsum dolor sit amet consiquest lirba erose
+                    </div>
+                </div>
+                <h3 class="team-title">Kali Kala</h3>
+                <div class="team-designation">
+                    Development Manager
+                </div>
+                <div class="team-socail-links">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-youtube"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                    <a href="#"><i class="fa fa-dribbble"></i></a>
+                </div>
+            </div>
+        </div>
+</section>
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css',$_this) !!}

6947ea590480de92e14cc202a2e1e3f071e378b3
sahakavatar <hakobyan.sahak88@gmail.com>
2018-05-16 15:11:39 +0400

    unit prices

---
+++ resources/units/unit_felicia_pricing/tpl.blade.php
@@ -0,0 +1,64 @@
+<section class="section-felic-price">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="section-title text-center">
+                <h2 class="title separator text-uppercase">Pricing Table</h2>
+                <div class="sub-title">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+            </div>
+        </div>
+
+        <div class="col-md-4 col-sm-6">
+            <div class="price-list text-center">
+                <h2 class="price-title">Basic</h2>
+                <div class="price-value">
+                    <sup>$</sup> 49
+                    <div class="price-duration">Per Year</div>
+                </div>
+                <ul class="list-unstyled price-info-list">
+                    <li>- 24/7 Tech Support</li>
+                    <li>- Advanced Options</li>
+                    <li>- 5GB Storage</li>
+                    <li>- 1 GB Bandwidth</li>
+                </ul>
+                <a href="#" class="btn btn-gray-border">Sign Up</a>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <div class="price-list text-center featured-price">
+                <!--only for featured price-->
+                <div class="recommended">Featured</div>
+                <!--only for featured price-->
+
+                <h2 class="price-title">Standard</h2>
+                <div class="price-value">
+                    <sup>$</sup> 99
+                    <div class="price-duration">Per Year</div>
+                </div>
+                <ul class="list-unstyled price-info-list">
+                    <li>- 24/7 Tech Support</li>
+                    <li>- Advanced Options</li>
+                    <li>- 10GB Storage</li>
+                    <li>- 10 GB Bandwidth</li>
+                </ul>
+                <a href="#" class="btn btn-info-solid">Sign Up</a>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-12">
+            <div class="price-list text-center">
+                <h2 class="price-title">Premium</h2>
+                <div class="price-value">
+                    <sup>$</sup> 199
+                    <div class="price-duration">Per Year</div>
+                </div>
+                <ul class="list-unstyled price-info-list">
+                    <li>- 24/7 Tech Support</li>
+                    <li>- Advanced Options</li>
+                    <li>- 100GB Storage</li>
+                    <li>- 30 GB Bandwidth</li>
+                </ul>
+                <a href="#" class="btn btn-gray-border">Sign Up</a>
+            </div>
+        </div>
+    </div>
+</section>
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css',$_this) !!}

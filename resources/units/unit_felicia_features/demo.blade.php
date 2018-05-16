dbd6318ccb6ad7de216cc166f8fb4b3f1a517644
sahakavatar <hakobyan.sahak88@gmail.com>
2018-05-16 14:23:37 +0400

    unit feature

---
+++ resources/units/unit_felicia_features/demo.blade.php
@@ -0,0 +1,134 @@
+<section class="section-felic-feature">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="section-title text-center">
+                    <h2 class="title separator"> FEATURES</h2>
+                    <div class="sub-title">Lorem Ipsum is simply dummy text of the printing and typesetting industry</div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="feature-list text-center">
+                    <!-- for icon start -->
+                    <!--<div class="feature-list-icon red-icon">-->
+                    <!--<i class="bi bi-article"></i>-->
+                    <!--</div>-->
+                    <!-- for icon end -->
+
+                    <!-- for img start -->
+                    <div class="feature-list-img">
+                        <img src="http://icons.iconarchive.com/icons/paomedia/small-n-flat/128/layers-icon.png" alt="">
+                    </div>
+                    <!-- for img end -->
+
+                    <h3 class="feature-title">
+                        Development Solutions
+                    </h3>
+                    <div class="feature-info">
+                        Phasellus fringilla suscipit risus nec eleifend. Pellentesque eu quam sem malesuada quam pellente.
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="feature-list text-center">
+                    <!--<div class="feature-list-icon info-icon">-->
+                    <!--<i class="bi bi-money-bag"></i>-->
+                    <!--</div>-->
+
+                    <!-- for img start -->
+                    <div class="feature-list-img">
+                        <img src="https://cdn2.iconfinder.com/data/icons/flat-icons-19/128/Light_bulb.png" alt="">
+                    </div>
+                    <!-- for img end -->
+
+                    <h3 class="feature-title">
+                        Business Policy
+                    </h3>
+                    <div class="feature-info">
+                        Phasellus fringilla suscipit risus nec eleifend. Pellentesque eu quam sem malesuada quam pellente.
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="feature-list text-center">
+                    <!--<div class="feature-list-icon yellow-icon">-->
+                    <!--<i class="bi bi-globe3"></i>-->
+                    <!--</div>-->
+                    <!-- for img start -->
+                    <div class="feature-list-img">
+                        <img src="http://wfarm1.dataknet.com/static/resources/icons/set81/623aae3f.png" alt="">
+                    </div>
+                    <!-- for img end -->
+                    <h3 class="feature-title">
+                        Web Security
+                    </h3>
+                    <div class="feature-info">
+                        Phasellus fringilla suscipit risus nec eleifend. Pellentesque eu quam sem malesuada quam pellente.
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="feature-list text-center">
+                    <!--<div class="feature-list-icon green-icon">-->
+                    <!--<i class="bi bi-server"></i>-->
+                    <!--</div>-->
+                    <!-- for img start -->
+                    <div class="feature-list-img">
+                        <img src="https://image.flaticon.com/icons/png/128/123/123377.png" alt="">
+                    </div>
+                    <!-- for img end -->
+                    <h3 class="feature-title">
+                        Data Analysis
+                    </h3>
+                    <div class="feature-info">
+                        Phasellus fringilla suscipit risus nec eleifend. Pellentesque eu quam sem malesuada quam pellente.
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="feature-list text-center">
+                    <!--<div class="feature-list-icon">-->
+                    <!--<i class="bi bi-search"></i>-->
+                    <!--</div>-->
+                    <!-- for img start -->
+                    <div class="feature-list-img">
+                        <img src="https://cdn4.iconfinder.com/data/icons/simplicio/128x128/file_add.png" alt="">
+                    </div>
+                    <!-- for img end -->
+                    <h3 class="feature-title">
+                        Search Engine Optimaization
+                    </h3>
+                    <div class="feature-info">
+                        Phasellus fringilla suscipit risus nec eleifend. Pellentesque eu quam sem malesuada quam pellente.
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="feature-list text-center">
+                    <!--<div class="feature-list-icon blue-icon">-->
+                    <!--<i class="bi bi-tablet"></i>-->
+                    <!--</div>-->
+                    <!-- for img start -->
+                    <div class="feature-list-img">
+                        <img src="http://icons.iconarchive.com/icons/elegantthemes/beautiful-flat-one-color/128/smartphone-icon.png" alt="">
+                    </div>
+                    <!-- for img end -->
+                    <h3 class="feature-title">
+                        App Development
+                    </h3>
+                    <div class="feature-info">
+                        Phasellus fringilla suscipit risus nec eleifend. Pellentesque eu quam sem malesuada quam pellente.
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css',$_this) !!}

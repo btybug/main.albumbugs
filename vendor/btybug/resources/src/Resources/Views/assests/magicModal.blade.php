bb921eead9bd297380cc85496e57676aea9a65f3
sahakavatar <hakobyan.sahak88@gmail.com>
2018-05-22 13:51:31 +0400

    modal

--- vendor/btybug/resources/src/Resources/Views/assests/magicModal.blade.php
+++ vendor/btybug/resources/src/Resources/Views/assests/magicModal.blade.php
@@ -3,10 +3,35 @@
     <div class="modal-dialog modal-lg " role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close btn-red-close " data-dismiss="modal" aria-label="Close"><span
-                            aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title p-t-10" id="myModalLabel">
-                </h4>
+                <div class="row">
+                    <div class="col-md-3">
+                        {{--<h4 class="modal-title p-t-10 text-center" id="myModalLabel">--}}
+                        {{--</h4>--}}
+                        <div class="mod-head-left">
+                            <input type="text" class="form-control" placeholder="Search area">
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <div class="mod-head-right">
+                            <div>
+                                <select name="" id="" class="form-control">
+                                    <option value="">Default Variation</option>
+                                    <option value="">1</option>
+                                    <option value="">2</option>
+                                </select>
+                                <button class="btn btn-default">Select</button>
+                            </div>
+
+                            <button type="button" class="close btn-red-close " data-dismiss="modal" aria-label="Close"><span
+                                        aria-hidden="true">&times;</span></button>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
             </div>
             <div class="modal-body" style="min-height: 500px;">
 

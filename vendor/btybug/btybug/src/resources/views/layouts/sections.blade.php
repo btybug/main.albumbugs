3041c8ffb6352d7ee7548e297e6ee706a77b1b8c
Sahak <hakobyan.sahak88@gmail.com>
2018-05-23 10:16:40 +0400

    fix bug

--- vendor/btybug/btybug/src/resources/views/layouts/sections.blade.php
+++ vendor/btybug/btybug/src/resources/views/layouts/sections.blade.php
@@ -69,29 +69,8 @@
     @include('btybug::header')
 </header>
 
-<div class="modal fade bigfullModal" id="magic-settings" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    {{--{!! Form::open(['url'=>'/admin/backend/theme-edit/live-save', 'id'=>'magic-form']) !!}--}}
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
-                </button>
-                {{--{!! Form::submit('Save',['class' => 'btn btn-success pull-right m-r-10']) !!}--}}
-                <h4 class="modal-title" id="myModalLabel"></h4>
-            </div>
-            <div class="modal-body" style="min-height: 500px;">
-
-                <div id="magic-body">
-
-                </div>
-            </div>
-        </div>
-    </div>
-    {{--{!! Form::close() !!}--}}
-</div>
-
-{{--@include('resources::assests.stylesModal')--}}
-{{--@include('resources::assests.magicModal')--}}
+@include('resources::assests.stylesModal')
+@include('resources::assests.magicModal')
 <div class="modal fade" id="save-as-variation" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
--- vendor/btybug/uploads/src/Resources/Views/gears/page_sections/live_preview/settings.blade.php
+++ vendor/btybug/uploads/src/Resources/Views/gears/page_sections/live_preview/settings.blade.php
@@ -59,31 +59,6 @@
         </div>
     </div>
 
-    {{--<div class="withoutifreamsetting animated bounceInRight hide" data-settinglive="settings">--}}
-    {{--{!! Form::model($model,['id'=>'add_custome_page']) !!}--}}
-    {{--@include($settingsHtml)--}}
-    {{--{!! Form::close() !!}--}}
-    {{--</div>--}}
-    {{--<div class="modal fade" id="magic-settings" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">--}}
-    {{--{!! Form::open(['url'=>'/admin/backend/theme-edit/live-save', 'id'=>'magic-form']) !!}--}}
-    {{--<div class="modal-dialog modal-lg" role="document">--}}
-    {{--<div class="modal-content">--}}
-    {{--<div class="modal-header">--}}
-    {{--<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span--}}
-    {{--aria-hidden="true">&times;</span></button>--}}
-    {{--{!! Form::submit('Save',['class' => 'btn btn-success pull-right m-r-10']) !!}--}}
-    {{--<h4 class="modal-title" id="myModalLabel"></h4>--}}
-    {{--</div>--}}
-    {{--<div class="modal-body" style="min-height: 500px;">--}}
-
-    {{--<div id="magic-body">--}}
-
-    {{--</div>--}}
-    {{--</div>--}}
-    {{--</div>--}}
-    {{--</div>--}}
-    {{--{!! Form::close() !!}--}}
-    {{--</div>--}}
 @stop
 @section('CSS')
     {!! HTML::style("https://jqueryvalidation.org/files/demo/site-demos.css") !!}

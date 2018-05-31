3c513ec82f5e09dbd1e3eef5b45342d0b399f964
Sahak <hakobyan.sahak88@gmail.com>
2018-05-31 15:05:34 +0400

    update

--- resources/units/unit_for_user_card/settings.blade.php
+++ resources/units/unit_for_user_card/settings.blade.php
@@ -6,12 +6,32 @@
                 <label for="">Functions</label>
             </div>
             <div class="col-md-8">
-
+                {!! Form::select('main_function',[
+                null=>'Main Function',
+                'authUser'=>'Auth User',
+                'specificUser'=>'Specific User',
+                'specificUsers'=>'Specific Users'],null,['class'=>'form-control double-select','data-value'=>'main']) !!}
             </div>
         </div>
     </div>
 </div>
+<div id="main_function_main_specificUser"
+     class="main_function_main collapse in @if(issetReturn($settings,'main_function') !=='specificUser') hide   @endif"
+     data-type="unit" aria-expanded="true" style="">
+    <div class="content bty-settings-panel">
+        <div class="col-md-12">
+            <div class="form-group">
+                <div class="col-md-4">
+                    <label for="">Static Name</label>
+                </div>
+                <div class="col-md-8">
+                    {!! Form::select('specificUser',\Btybug\User\User::all()->pluck('username','id'),null,['class'=>'form-control']) !!}
+                </div>
+            </div>
+        </div>
 
+    </div>
+</div>
 @endoption
 
 @option('name','f',$data)
@@ -22,12 +42,12 @@
                 <label for="">Name Area Type</label>
             </div>
             <div class="col-md-8">
-                {!! Form::select('name_type',[null=>'Select Content Type','static'=>'Static','dynamic'=>'Dynamic'],null,['class'=>'form-control name_type','data-value'=>'name']) !!}
+                {!! Form::select('name_type',[null=>'Select Content Type','static'=>'Static','dynamic'=>'Dynamic'],null,['class'=>'form-control double-select','data-value'=>'name']) !!}
             </div>
         </div>
     </div>
 </div>
-<div id="name_type_static_name_static"
+<div id="name_type_name_static"
      class="name_type_name collapse in @if(issetReturn($settings,'tr_content_type') !=='unit') hide   @endif"
      data-type="unit" aria-expanded="true" style="">
     <div class="content bty-settings-panel">
@@ -48,10 +68,13 @@
 
 <script>
     $(function () {
-        $('.name_type').on('change', function () {
+        $('body').on('change','.double-select', function () {
+            var prefix=$(this).attr('name')+'_';
             var panel = $(this).data('value');
-            $('.name_type_' + panel).addClass('hide');
-            var id = '#name_type_static_' + panel + '_' + $(this).val();
+
+            $('.'+prefix + panel).addClass('hide');
+            var id = '#'+prefix+panel + '_' + $(this).val();
+            console.log(prefix+panel)
             $(id).removeClass('hide');
         });
     });

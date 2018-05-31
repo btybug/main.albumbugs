9848752c0ca5c14e826d35d71b2b1f7aa9e02e41
Sahak <hakobyan.sahak88@gmail.com>
2018-05-31 16:15:52 +0400

    update

--- resources/units/unit_for_user_card/demo.blade.php
+++ resources/units/unit_for_user_card/demo.blade.php
@@ -1,24 +1,3 @@
-<div class="bty-user-card-1">
-    <div>
-        <img src="http://www.imagefully.com/wp-content/uploads/2015/10/Nice-Best-Photography-Alone-Picture.jpg"
-             alt="">
-    </div>
-    <div>
-        <h3>@if(isset($settings['name_type'])&& $settings['name_type']=='static' ) {!! $settings['name'] !!} @endif</h3>
-        <h4>profession</h4>
-        <p><i class="fa fa-map-marker"></i>location</p>
-        <h5>Info</h5>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy.</p>
-    </div>
-    <ul>
-        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-        <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-    </ul>
-</div>
-
-{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}
-{!! BBscript($_this->path.DS.'js'.DS.'custom.js',$_this) !!}
+@if(isset($settings['main_function']) && View::exists($_this->slug.'::view.'.$settings['main_function']))
+@include($_this->slug.'::view.'.$settings['main_function'])
+@endif
---
+++ resources/units/unit_for_user_card/view/authUser.blade.php
@@ -0,0 +1,24 @@
+<div class="bty-user-card-1">
+    <div>
+        <img src="http://www.imagefully.com/wp-content/uploads/2015/10/Nice-Best-Photography-Alone-Picture.jpg"
+             alt="">
+    </div>
+    <div>
+        <h3>{!! Auth::user()->username !!}</h3>
+        <h4>profession</h4>
+        <p><i class="fa fa-map-marker"></i>location</p>
+        <h5>Info</h5>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy.</p>
+    </div>
+    <ul>
+        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+    </ul>
+</div>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}
+{!! BBscript($_this->path.DS.'js'.DS.'custom.js',$_this) !!}

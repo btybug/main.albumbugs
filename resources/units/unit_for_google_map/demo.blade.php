76814bff286e767fc7935e7e364fc444dc83f242
Ahahit <hakobyan.sahak88@gmail.com>
2018-06-05 17:45:01 +0400

    my unit

--- resources/units/unit_for_google_map/demo.blade.php
+++ resources/units/unit_for_google_map/demo.blade.php
@@ -1,32 +1,115 @@
 <style>
     #map {
-        width: 100%;
-        height: 400px;
+        height: @if(isset($settings['height'])) {{$settings['height']}} @else {{'400px'}} @endif;
+        width: @if(isset($settings['width'])) {{$settings['width']}} @else {{'100%'}} @endif;
+        margin: auto;
         background-color: grey;
     }
+    h3{
+        width: 100%;
+        text-align: center;
+    }
 </style>
-<h3>My Google Maps Demo</h3>
+<h3>@if(isset($settings['header'])) {{$settings['header']}} @else {{'My Google Maps'}} @endif </h3>
 
 <div id="map"></div>
+<div style="display: none" id="main_function">@if(isset($settings['main_function'])) {{$settings['main_function']}} @else 1 @endif</div>
 <div style="display: none" id="lat">@if(isset($settings['lat'])) {{$settings['lat']}} @else {{10}} @endif</div>
 <div style="display: none" id="lng">@if(isset($settings['lng'])) {{$settings['lng']}} @else {{10}} @endif</div>
+<div style="display: none" id="from_lat">@if(isset($settings['from_lat'])) {{$settings['from_lat']}} @else {{41.85}} @endif</div>
+<div style="display: none" id="from_lng">@if(isset($settings['from_lng'])) {{$settings['from_lng']}} @else {{-87.65}} @endif</div>
+<div style="display: none" id="to_lat">@if(isset($settings['to_lat'])) {{$settings['to_lat']}} @else {{39.79}} @endif</div>
+<div style="display: none" id="to_lng">@if(isset($settings['to_lng'])) {{$settings['to_lng']}} @else {{-86.14}} @endif</div>
 <div style="display: none" id="styles">@if(isset($settings['style'])) {{$settings['style']}} @else {{'roadmap'}} @endif</div>
+<div style="display: none" id="zoom">@if(isset($settings['zoom'])) {{$settings['zoom']}} @else {{0}} @endif</div>
+<div style="display: none" id="mapType">@if(isset($settings['mapType'])) {{$settings['mapType']}} @else {{0}} @endif</div>
+<div style="display: none" id="scale">@if(isset($settings['scale'])) {{$settings['scale']}} @else {{0}} @endif</div>
+<div style="display: none" id="street">@if(isset($settings['street'])) {{$settings['street']}} @else {{0}} @endif</div>
+<div style="display: none" id="rotate">@if(isset($settings['rotate'])) {{$settings['rotate']}} @else {{0}} @endif</div>
+<div style="display: none" id="fullscreen">@if(isset($settings['fullscreen'])) {{$settings['fullscreen']}} @else {{0}} @endif</div>
+<div style="display: none" id="icon">@if(isset($settings['icon'])) {{$settings['icon']}} @else {{0}} @endif</div>
 <script>
     // Initialize and add the map
     function initMap() {
         // The location of Uluru
-        var lat = Number($('#lat').text());
-        var lng = Number($('#lng').text());
-        var styles = $('#styles').text();
-        var uluru = {lat: lat, lng: lng};
-        // The map, centered at Uluru
-        var map = new google.maps.Map(
+        var main_function = Number($('#main_function').text());
+        var styles = Number($('#styles').text());
+        var zoom;
+        var mapType;
+        var scale;
+        var streetView;
+        var rotate;
+        var fullscreen;
+        if(Number($('#zoom').text()) == 1) zoom = true; else zoom = false;
+        if(Number($('#mapType').text()) == 1) mapType = true; else mapTupe = false;
+        if(Number($('#scale').text()) == 1) scale = true; else scaleControl = false;
+        if(Number($('#street').text()) == 1) streetView = true; else streetView= false;
+        if(Number($('#rotate').text()) == 1) rotate = true; else rotate = false;
+        if(Number($('#fullscreen').text()) == 1) fullscreen = true; else fullscreen = false;
+        if(isNaN(main_function)) main_function = 1;
 
-            document.getElementById('map'), {zoom: 4, center: uluru, mapTypeId: "roadmap"});
-        console.log(styles);
-        // The marker, positioned at Uluru
-        var marker = new google.maps.Marker({position: uluru, map: map});
-        console.log(styles);
+        if(main_function == 1) {
+            var lat = Number($('#lat').text());
+            var lng = Number($('#lng').text());
+
+            var uluru = {lat: lat, lng: lng};
+            // The map, centered at Uluru
+            var map = new google.maps.Map(
+                document.getElementById('map'), {zoom: 4, center: uluru,
+                                                mapTypeId: "roadmap",
+                                                disableDefaultUI: true,
+                                                zoomControl: zoom,
+                                                mapTypeControl: mapType,
+                                                scaleControl: scale,
+                                                streetViewControl: streetView,
+                                                rotateControl: rotate,
+                                                fullscreenControl: fullscreen});
+            // The marker, positioned at Uluru
+            var url = null;
+            if(Number($("#icon").text()) !== 0) url = $("#icon").text();
+            var icon = {
+                url: url,
+                scaledSize: new google.maps.Size(50, 50),
+                //origin: new google.maps.Point(0,0),
+                //anchor: new google.maps.Point(0, 0)
+            };
+            var marker = new google.maps.Marker({position: uluru, map: map, icon: icon});
+        }
+        else if(main_function == 2){
+            var from = {lat: Number($('#from_lat').text()), lng: Number($('#from_lng').text())};
+            var to = {lat: Number($('#to_lat').text()), lng: Number($('#to_lng').text())};
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: from,
+                zoom: 7,
+                mapTypeId: "satellite",
+                disableDefaultUI: true,
+                zoomControl: zoom,
+                mapTypeControl: mapType,
+                scaleControl: scale,
+                streetViewControl: streetView,
+                rotateControl: rotate,
+                fullscreenControl: fullscreen
+            });
+            var directionsDisplay = new google.maps.DirectionsRenderer({
+                map: map
+            });
+            // Set destination, origin and travel mode.
+            var request = {
+                destination: to,
+                origin: from,
+                travelMode: "DRIVING",
+
+            };
+            // Pass the directions request to the directions service.
+            var directionsService = new google.maps.DirectionsService();
+            directionsService.route(request, function(response, status) {
+                if (status == 'OK') {
+                    // Display the route on the map.
+                    directionsDisplay.setDirections(response);
+                }
+            });
+        }
         if(styles == 0)
             map.setMapTypeId('roadmap');
         else if(styles == 1)
@@ -40,3 +123,4 @@
 <script async defer
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAud3CAgSUNCWBRlSZQjKz8a7M0RV7R_IA&callback=initMap">
 </script>
+
--- resources/units/unit_for_google_map/settings.blade.php
+++ resources/units/unit_for_google_map/settings.blade.php
@@ -7,36 +7,85 @@
             </div>
             <div class="col-md-8">
                 {!! Form::select('main_function',[
-                null=>'Main Function',
-                'position'=>'Position',
-                'destination'=>'Destination'],null,['class'=>'form-control double-select','data-value'=>'main']) !!}
-            </div>
-            <div class="clearfix"></div>
-        </div>
-    </div>
-</div>{!!Form::number('lng', 'value');!!}
-<div class="bty-settings-panel">
-    <div class="col-md-12">
-        <div class="form-group">
-            <div class="col-md-4">
-                    <label for="">Longitute</label>
-            </div>
-            <div class="col-md-8">
-                {!!Form::number('lng', 'value');!!}
-            </div>
-            <div class="clearfix"></div>
-        </div>
-        <div class="form-group">
-            <div class="col-md-4">
-                <label for="">Latitute</label>
-            </div>
-            <div class="col-md-8">
-                {!!Form::number('lat', 'value');!!}
+                '1'=>'Position',
+                '2'=>'Destination'],null,['class'=>'form-control double-select','data-value'=>'main']) !!}
             </div>
             <div class="clearfix"></div>
         </div>
     </div>
 </div>
+<div id="position">
+    <div class="bty-settings-panel">
+        <div class="col-md-12">
+            <div class="form-group">
+                <div class="col-md-4">
+                    <label for="">Position</label>
+                </div>
+                <div class="col-md-8">
+                    <label for="">Longitude</label>
+                    {!!Form::number('lng', null, ['class'=>'form-control']);!!}
+                    <label for="">Latitude</label>
+                    {!!Form::number('lat', null, ['class'=>'form-control']);!!}
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-4">
+                    <label for="">Icon Url</label>
+                </div>
+                <div class="col-md-8">
+                    {!!Form::text('icon', null, ['class'=>'form-control']);!!}
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<div id="destination" style="display:none;">
+    <div class="bty-settings-panel">
+        <div class="col-md-12">
+            <div class="form-group">
+                <div class="col-md-4">
+                    <label for="">From</label>
+                </div>
+                <div class="col-md-8">
+                    <label for="">Longitude</label>
+                    {!!Form::number('from_lng', 'value', ['class'=>'form-control']);!!}
+                    <label for="">Latitude</label>
+                    {!!Form::number('from_lat', 'value', ['class'=>'form-control']);!!}
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-4">
+                    <label for="">To</label>
+                </div>
+                <div class="col-md-8">
+                    <label for="">Longitude</label>
+                    {!!Form::number('to_lng', 'value',['class'=>'form-control']);!!}
+                    <label for="">Latitude</label>
+                    {!!Form::number('to_lat', 'value', ['class'=>'form-control']);!!}
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<script>
+    $( ".form-control" ).change(function() {
+        if($(this).val() == 2) {
+            $("#destination").show();
+            $("#position").hide();
+        }
+        else if($(this).val() == 1){
+            $("#position").show();
+            $("#destination").hide();
+        }
+    });
+
+</script>
 @endoption
 @option('general','s',$data)
 <div class="bty-settings-panel">
@@ -51,7 +100,54 @@
                    '1' => 'Satellite',
                    '2' => 'Hybrid',
                    '3' => 'Terrain',
-                ));!!}
+                ),null,['class'=>'form-control','data-value'=>'main']);!!}
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <div class="form-group">
+            <div class="col-md-4">
+                <label for="">Width</label>
+            </div>
+            <div class="col-md-8">
+                {!!Form::number('width', 'value', ['class'=>'form-control','data-value'=>'main']);!!}
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <div class="form-group">
+            <div class="col-md-4">
+                <label for="">Height</label>
+            </div>
+            <div class="col-md-8">
+                {!!Form::number('height', 'value', ['class'=>'form-control','data-value'=>'main']);!!}
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <div class="form-group">
+            <div class="col-md-4">
+                <label for="">Header</label>
+            </div>
+            <div class="col-md-8">
+                {!!Form::text('header', 'My Google Maps', ['class'=>'form-control']);!!}
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <div class="form-group">
+            <div class="col-md-4">
+                <label for="">Control Elements</label>
+            </div>
+            <div class="col-md-8">
+                <label for="">Zoom</label>
+                {!!Form::checkbox('zoom');!!}<br>
+                <label for="">Map Type</label>
+                {!!Form::checkbox('mapType', null, ['class'=>'form-control']);!!}<br>
+                <label for="">Scale</label>
+                {!!Form::checkbox('scale', null, ['class'=>'form-control']);!!}<br>
+                <label for="">Street</label>
+                {!!Form::checkbox('street', null, ['class'=>'form-control']);!!}<br>
+                <label for="">Rotate</label>
+                {!!Form::checkbox('rotate', null, ['class'=>'form-control']);!!}<br>
+                <label for="">Fullscreen</label>
+                {!!Form::checkbox('fullscreen');!!}
             </div>
             <div class="clearfix"></div>
         </div>
@@ -62,6 +158,7 @@
 {!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}
 {!! BBscript(public_path('js'.DS.'select2'.DS.'select2.full.min.js')) !!}
 {!! BBscript($_this->path.DS.'js'.DS.'settings.js') !!}
+
 
 
 

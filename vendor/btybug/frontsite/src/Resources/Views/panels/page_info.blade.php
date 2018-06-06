8bdf8a9fa6b5ece3e17d63c0f6eed5654db11f22
Sahak <hakobyan.sahak88@gmail.com>
2018-06-06 22:39:25 +0400

    update

--- vendor/btybug/frontsite/src/Resources/Views/panels/page_info.blade.php
+++ vendor/btybug/frontsite/src/Resources/Views/panels/page_info.blade.php
@@ -1,3 +1,6 @@
-{!! BBcustomize('layouts','page_layout','frontend',
-                                                        (isset($page->page_layout) && $page->page_layout)?'Change':'Select',
-                                                        'page_layout_'.$page->id,['class'=>'btn btn-default change-layout','model' =>$page]) !!}
+<a href="javascript:void(0)" class="btn btn-info">Change</a>
+
+
+{{--{!! BBcustomize('layouts','page_layout','frontend',--}}
+                                                        {{--(isset($page->page_layout) && $page->page_layout)?'Change':'Select',--}}
+                                                        {{--'page_layout_'.$page->id,['class'=>'btn btn-default change-layout','model' =>$page]) !!}--}}

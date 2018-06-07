678dc6a1c0d6125a0c922078f78b64f5cfc92ef2
terakyan <edo.terakyan@gmail.com>
2018-06-07 16:18:49 +0400

    fix base layout styles

--- resources/views/ContentLayouts/front_layout_with_3_9_col_2_row/settings.blade.php
+++ resources/views/ContentLayouts/front_layout_with_3_9_col_2_row/settings.blade.php
@@ -1,6 +1,4 @@
 <?php
-$container_styles = getDinamicStyle('containers');
-$container_styles = '<option value="0">Select Class</option>' . $container_styles;
 $placeholdersData = $model->placeholders;
 $placeholders = [];
 foreach ($placeholdersData as $key => $datum) {
@@ -34,9 +32,7 @@ foreach ($placeholdersData as $key => $datum) {
                 <label for="">Select</label>
             </div>
             <div class="col-md-8">
-                <select name="ls_style" id="" class="form-control">
-                    {!! $container_styles !!}
-                </select>
+                {!! Form::select('ls_style',['' => 'Select'] + getDinamicStyle('containers'),null,['class' => 'form-control']) !!}
             </div>
             <div class="clearfix"></div>
         </div>
@@ -161,9 +157,7 @@ foreach ($placeholdersData as $key => $datum) {
                 <label for="">Select</label>
             </div>
             <div class="col-md-8">
-                <select name="tr_style" id="" class="form-control">
-                    {!! $container_styles !!}
-                </select>
+                {!! Form::select('tr_style',['' => 'Select'] + getDinamicStyle('containers'),null,['class' => 'form-control']) !!}
             </div>
             <div class="clearfix"></div>
         </div>
@@ -177,9 +171,7 @@ foreach ($placeholdersData as $key => $datum) {
                 <label for="">Select</label>
             </div>
             <div class="col-md-8">
-                <select name="mr_style" id="" class="form-control">
-                    {!! $container_styles !!}
-                </select>
+                {!! Form::select('mr_style',['' => 'Select'] + getDinamicStyle('containers'),null,['class' => 'form-control']) !!}
             </div>
             <div class="clearfix"></div>
         </div>

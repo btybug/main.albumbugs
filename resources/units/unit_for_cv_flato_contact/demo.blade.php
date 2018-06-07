1235b6a849b7fbca0136d21361e7cb57d9d8b39d
sahakavatar <hakobyan.sahak88@gmail.com>
2018-06-07 14:51:36 +0400

    unit contact

---
+++ resources/units/unit_for_cv_flato_contact/demo.blade.php
@@ -0,0 +1,57 @@
+<section id="contact-flato">
+    <div class="container">
+        <div class="heading">
+            <h2 class="title">GET IN TOUCH</h2>
+            <p>Please feel free if you would like to have a chat.</p>
+        </div>
+    </div>
+    <div class="mapArea">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13007070.354009148!2d-104.65387033028972!3d37.25828124582162!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2z0KHQvtC10LTQuNC90LXQvdC90YvQtSDQqNGC0LDRgtGLINCQ0LzQtdGA0LjQutC4!5e0!3m2!1sru!2s!4v1528367375778" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+    <div class="content-contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 column_first">
+                    <div class="contact-info">
+                        <h4>CONTACT DETAILS</h4>
+                        <p><i class="fa fa-map-marker fa-lg"></i> Collins Street West Victoria 8007 Australia</p>
+                        <p><i class="fa fa-mobile fa-lg"></i>+1800 1234 56789</p>
+                        <p><i class="fa fa-envelope-o "></i> <a href="mailto:support@websitename.com">support@websitename.com</a>
+                        </p>
+                        <p><i class="fa fa-link "></i> <a href="http://www.websitename.com">www.websitename.com</a></p>
+                    </div>
+                </div>
+                <div class="col-md-8 column_last">
+                    <div class="conForm">
+                        <h4>Shoot a message!</h4>
+                        <form action="" method="post">
+                            <div class="form-group row">
+                                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                                    <input type="text" class="form-control" placeholder="Your name...">
+                                </div>
+                                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                                    <input type="email" class="form-control" placeholder="Your email...">
+                                </div>
+
+                            </div>
+                            <div class="form-group row">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                    <textarea name="" id="" cols="30" rows="10" class="form-control" placeholder="Your message..."></textarea>
+                                </div>
+
+
+                            </div>
+                            <div class="form-group text-right">
+                                <input type="submit" value="Send message" class="submitBnt"/>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

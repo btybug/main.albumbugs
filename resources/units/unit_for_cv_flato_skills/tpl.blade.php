902237447be1279ba6cf85178118b4eb9cb5a0d4
sahakavatar <hakobyan.sahak88@gmail.com>
2018-06-07 12:06:46 +0400

    unit skills

---
+++ resources/units/unit_for_cv_flato_skills/tpl.blade.php
@@ -0,0 +1,83 @@
+<section id="skills-flato">
+        <div class="skills">
+            <div class="heading">
+                <h2 class="title">TECHNICAL SKILLS</h2>
+                <p>I can say i’m quite good at</p>
+            </div>
+            <div class="content-skills">
+                <div class="row">
+                    <div class="col-md-4 skillsArea column_first">
+                        <div class=" col-xs-12 col-sm-6 col-md-6 col-lg-6 skills">
+
+                <span class="chart">
+                    <span class="percent">90</span>
+                </span>
+
+                            <h4>PHOTOSHOP</h4>
+                            <p>Donec accumsan ligula vitae mag na curabitur id</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 skillsArea">
+                        <div class=" col-xs-12 col-sm-6 col-md-6 col-lg-6 skills">
+
+                <span class="chart">
+                    <span class="percent">80</span>
+                </span>
+
+                            <h4>ILLUSTRATOR</h4>
+                            <p>Donec accumsan ligula vitae mag na curabitur id</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 skillsArea column_last">
+                        <div class=" col-xs-12 col-sm-6 col-md-6 col-lg-6 skills">
+
+                <span class="chart">
+                    <span class="percent">70</span>
+
+                </span>
+
+                            <h4>FLASH</h4>
+                            <p>Donec accumsan ligula vitae mag na curabitur id</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 skillsArea column_first">
+                        <div class=" col-xs-12 col-sm-6 col-md-6 col-lg-6 skills">
+
+                <span class="chart">
+                    <span class="percent">75</span>
+
+                </span>
+
+                            <h4>HTML5 / CSS3</h4>
+                            <p>Donec accumsan ligula vitae mag na curabitur id</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 skillsArea">
+                        <div class=" col-xs-12 col-sm-6 col-md-6 col-lg-6 skills">
+
+                <span class="chart" data-percent="60">
+                    <span class="percent">60</span>
+                </span>
+
+                            <h4>JQUERY</h4>
+                            <p>Donec accumsan ligula vitae mag na curabitur id</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 skillsArea column_last">
+                        <div class=" col-xs-12 col-sm-6 col-md-6 col-lg-6 skills">
+
+                <span class="chart" data-percent="65">
+                    <span class="percent">65</span>
+                </span>
+
+                            <h4>PHP / MYSQL</h4>
+                            <p>Donec accumsan ligula vitae mag na curabitur id</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+</section>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

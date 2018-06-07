fe99265165098bee369a0716050320d8dd205094
sahakavatar <hakobyan.sahak88@gmail.com>
2018-06-07 11:30:09 +0400

    about unit

---
+++ resources/units/unit_for_cv_flato_about_me/tpl.blade.php
@@ -0,0 +1,36 @@
+<section id="about-flato">
+        <div class="about">
+            <div class="heading">
+                <h2 class="title">ABOUT ME</h2>
+                <p>A small introduction about my self</p>
+            </div>
+            <div class="row aboutme">
+
+                <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+
+                    <h3>Jonathan Doe</h3>
+                    <h4 class="subHeading">Webdesigner &amp; Web Developer from Melbourne</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat. cillum dolore eu fugiat nulla pariatur. olore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                        ex ea commodo consequat.</p>
+                    <p>nulla pariatur.uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. olore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <a href="javascript:void(0);" class="bntDownload">Download Resume</a>
+                </div>
+
+
+                <div class=" col-xs-12 col-sm-12 col-md-4 col-lg-4 col-md-offset-1 col-lg-offset-1 proPic">
+
+                    <img src="http://themesquared.com/flato/wp-content/uploads/2014/08/me.jpg" alt=""
+                         class="img-circle">
+
+                </div>
+            </div>
+        </div>
+</section>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

38de5fbad4977e1990b95d1c4e52a3aa5a3caf18
sahakavatar <hakobyan.sahak88@gmail.com>
2018-06-07 13:44:23 +0400

    unit experience

---
+++ resources/units/unit_for_cv_flato_experience/tpl.blade.php
@@ -0,0 +1,54 @@
+<section id="experience-flato">
+    <div class="experience">
+            <div class="heading">
+                <h2 class="title">Work Experience</h2>
+                <p>My previous associations</p>
+            </div>
+            <div class="content-experience">
+                <div class="row details">
+                    <div class="col-xs-12 col-sm-3 col-md-2 col-lg-2">
+                        <div class="workYear">Mar,2011<br> Present</div>
+                    </div>
+                    <div class="col-xs-12 col-sm-9 col-md-10 col-lg-10 rightArea">
+                        <div class="arrowpart"></div>
+                        <div class="exCon">
+                            <h4>UIzards</h4>
+                            <h5>Senior UX Designer</h5>
+                            <p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quom
+                                placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in
+                                iis qui faorum claritatem. Investigationes demonstraverunt lectores legere me lius quod
+                                ii legunt saepius.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row details">
+                    <div class="col-xs-12 col-sm-3 col-md-2 col-lg-2">
+                        <div class="workYear">Mar,2011<br> Present</div>
+                    </div>
+                    <div class="col-xs-12 col-sm-9 col-md-10 col-lg-10 rightArea">
+                        <div class="arrowpart"></div>
+                        <div class="exCon"><h4>Lexind</h4><h5>Visual / UI Designer</h5>
+                            <p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quom
+                                placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in
+                                iis qui faorum claritatem. Investigationes demonstraverunt lectores legere me lius quod
+                                ii legunt saepius.</p></div>
+                    </div>
+                </div>
+                <div class="row details">
+                    <div class="col-xs-12 col-sm-3 col-md-2 col-lg-2">
+                        <div class="workYear">Mar,2011<br> Present</div>
+                    </div>
+                    <div class="col-xs-12 col-sm-9 col-md-10 col-lg-10 rightArea">
+                        <div class="arrowpart"></div>
+                        <div class="exCon"><h4>Matrix Media</h4><h5>Visual / UI Designer</h5>
+                            <p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quom
+                                placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in
+                                iis qui faorum claritatem. Investigationes demonstraverunt lectores legere me lius quod
+                                ii legunt saepius.</p></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

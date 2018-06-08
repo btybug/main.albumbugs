2b60d49c7915964a6b52e87c5941c1a805c1715b
sahakavatar <hakobyan.sahak88@gmail.com>
2018-06-08 17:29:53 +0400

    unit get in touch

---
+++ resources/units/unit_for_cv_abrar_get_in_touch/tpl.blade.php
@@ -0,0 +1,42 @@
+<section id="get-in-touch-abrar">
+    <div class="section-padding">
+        <div class="section-title text-capitalize text-center">
+            <h2 class="title-head">Get In Touch</h2>
+            <p>Contact with me</p>
+        </div>
+        <div class="get-in-touch-contact">
+            <div class="contact-area-form">
+                <form action="#" method="">
+                    <div class="form-group">
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" placeholder="Name :">
+                        </div>
+                        <div class="col-md-6">
+                            <input type="email" class="form-control" placeholder="Email :">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" placeholder="Contact :">
+                        </div>
+                        <div class="col-md-6">
+                            <input type="number" class="form-control" placeholder="Phone :">
+                        </div>
+                    </div>
+
+                    <div class="contact-info-msg form-group">
+                        <div class="col-md-12">
+                            <textarea id="message" class="form-control" name="message" placeholder="Type some text..."
+                                      rows="7" cols="30"></textarea>
+                        </div>
+
+                    </div>
+                    <div class="wall-btn text-center">
+                        <button class="white-clr">Send <i class="fa fa-arrow-right"></i></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

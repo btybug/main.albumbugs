98fe75077c3b8fd8fd1b22349c32a9df710bcf93
sahakavatar <hakobyan.sahak88@gmail.com>
2018-06-08 10:27:19 +0400

    unit social

---
+++ resources/units/unit_for_cv_flato_social/tpl.blade.php
@@ -0,0 +1,14 @@
+<section id="social-flato">
+    <div class="social">
+        <ul>
+            <li><a href="javascript:void(0);" title="facebook" target="_blank"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="javascript:void(0);" title="linkedin" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="javascript:void(0);" title="github" target="_blank"><i class="fa fa-github"></i></a></li>
+            <li><a href="javascript:void(0);" title="instagram" target="_blank"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="javascript:void(0);" title="skype" target="_blank"><i class="fa fa-skype"></i></a></li>
+            <li><a href="javascript:void(0);" title="vk" target="_blank"><i class="fa fa-vk"></i></a></li>
+        </ul>
+    </div>
+</section>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

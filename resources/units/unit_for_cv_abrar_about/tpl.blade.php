9207f9adb4358811b77341878ad767c8e6374f03
sahakavatar <hakobyan.sahak88@gmail.com>
2018-06-08 14:08:50 +0400

    unit about

---
+++ resources/units/unit_for_cv_abrar_about/tpl.blade.php
@@ -0,0 +1,142 @@
+<section id="about-me-abrar">
+    <div class=" section-padding">
+            <div class="section-title text-center">
+                <h2 class="title-head">About Me</h2>
+                <p>Introduce</p>
+            </div>
+            <div class="admin-about">
+                <div class="about-me-content">
+                    <div class="row">
+                        <div class="col-md-5">
+                            <div class="my-profile-pic">
+                                <img src="https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-7">
+                            <div class="admin-text">
+                                <div class="admin-quote">
+                                    <blockquote>
+                                        Donec tincidunt arcu a interdum efficitur. Nullam egestas bibendum tristique.
+                                        Donec tincidunt arcu a interdum efficitur. Nullam egestas bibendum tristique.
+                                        Quisque
+                                        Donec tincidunt arcu a interdum efficitur Donec tincidunt..
+                                    </blockquote>
+                                </div>
+
+                                <div class="admin-signature">
+                                    <img src="https://allaboutcloud.info/wp-content/uploads/2017/08/signature.png" alt="">
+                                </div>
+
+                                <div class="admin-sponsor">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="sponsor">
+                                                <img src="https://vignette.wikia.nocookie.net/central/images/9/93/YouTube-logo-dark.png/revision/latest?cb=20170516095638" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="sponsor">
+                                                <img src="https://angular.io/assets/images/logos/angular/angular_solidBlack.png" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="sponsor">
+                                                <img src="https://g1.iggcdn.com/assets/site/brand/IGG_Logo_Frame_Black_RGB-2-7c3ac82030a1a590f8f2337e419875f29bb44ff5929060b95158ccfcc92bc8f7.png" alt="">
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="about-me-description">
+                    <div class="row">
+
+                        <div class="admin-details-description">
+                            <div class="col-md-6">
+                                <div class="my-profile-details">
+                                    <ul>
+                                        <li><p><span class="pro-title"> Name : </span> <span
+                                                        class="pro-detail">Abrar Wall</span></p></li>
+                                        <li><p><span class="pro-title"> Age  : </span> <span
+                                                        class="pro-detail">27 Years</span></p></li>
+                                        <li><p><span class="pro-title"> Email : </span> <span
+                                                        class="pro-detail">info@example.com</span></p></li>
+                                        <li><p><span class="pro-title"> Address : </span> <span
+                                                        class="pro-detail">1234, Parkstreet Newyork City</span></p></li>
+                                        <li><p><span class="pro-title"> Phone : </span> <span
+                                                        class="pro-detail">(+91) 12345 95487</span></p></li>
+                                        <li><p><span class="pro-title"> Website : </span> <span
+                                                        class="pro-detail">info@example.com</span></p></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+
+                            <div class="col-md-6">
+
+                                <div class="skill_progress">
+                                    <div class="single_experties">
+                                        <div class="skilled-tittle">Photoshop
+                                        </div>
+                                        <div class="skilled-percent">75%
+                                        </div>
+                                        <div class="progress">
+                                            <div class="progress-bar width-75" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+                                                <span></span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="single_experties">
+                                        <div class="skilled-tittle">Photography</div>
+                                        <div class="skilled-percent">85%</div>
+                                        <div class="progress">
+                                            <div class="progress-bar width-85" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+                                                <span></span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="single_experties">
+                                        <div class="skilled-tittle">Web Development</div>
+                                        <div class="skilled-percent">90%</div>
+                                        <div class="progress">
+                                            <div class="progress-bar width-90" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+                                                <span></span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="single_experties">
+                                        <div class="skilled-tittle">Illustrator</div>
+                                        <div class="skilled-percent">65%</div>
+                                        <div class="progress">
+                                            <div class="progress-bar width-65" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+                                                <span></span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="wall-btn text-center">
+                    <button>Download CV</button>
+                </div>
+            </div>
+
+        </div>
+</section>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

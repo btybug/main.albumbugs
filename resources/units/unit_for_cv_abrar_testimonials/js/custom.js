50f9de20f52d116aae0d38bd244db61c5be61a82
sahakavatar <hakobyan.sahak88@gmail.com>
2018-06-08 16:54:32 +0400

    unit testimonial

---
+++ resources/units/unit_for_cv_abrar_testimonials/js/custom.js
@@ -0,0 +1,30 @@
+$('.testimonial-slide').owlCarousel({
+    margin:30,
+    responsiveClass:true,
+    nav: true,
+    navSpeed:500,
+    navText:["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+
+        },
+        400:{
+            items:1
+
+        },
+        600:{
+            items:1
+
+        },
+        700:{
+            items:1
+
+        },
+        1000:{
+            items:1
+
+
+        }
+    }
+});
---
+++ resources/units/unit_for_cv_abrar_testimonials/tpl.blade.php
@@ -0,0 +1,95 @@
+<section id="testimonial-abrar">
+    <div class="section-title text-capitalize text-center">
+        <h2 class="title-head">Clients Love</h2>
+        <p>What My Clients Say About Me</p>
+    </div>
+    <div class="client-testimonial-content">
+        <div class="testimonial-slide owl-carousel owl-theme owl-responsive-1000 owl-loaded">
+            <div class="testimonial-pic-text">
+                <div class="testimonial-text text-right">
+                    <div class="testimonial-qouate">
+                        <p>Sed ullamcorper mi sit amet porta aliquam. Phasellus non molestie augu
+                            scelerisque, ultricies erat tempor, finibus Sed ullamcorper mi sit amet porta
+                            Phasellus non molestie augue. Donec id sapien.</p>
+                    </div>
+                    <div class="client-name-designation pull-right">
+                        <span class="client-name">MD.Saiful Islam</span>
+                        <span class="client-designation">Director/S.S Studio</span>
+                    </div>
+                    <div class="client-rate text-left">
+                        <ul>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="testimonial-pic">
+                    <img src="https://images.askmen.com/1080x540/2017/01/06-044621-the_pitfalls_of_dating_a_married_woman.jpg"
+                         alt="">
+                </div>
+            </div>
+
+            <div class="testimonial-pic-text">
+                <div class="testimonial-text text-right">
+                    <div class="testimonial-qouate">
+                        <p>Sed ullamcorper mi sit amet porta aliquam. Phasellus non molestie augu
+                            scelerisque, ultricies erat tempor, finibus Sed ullamcorper mi sit amet porta
+                            Phasellus non molestie augue. Donec id sapien.</p>
+                    </div>
+                    <div class="client-name-designation pull-right">
+                        <span class="client-name">MD.Saiful Islam</span>
+                        <span class="client-designation">Director/S.S Studio</span>
+                    </div>
+                    <div class="client-rate text-left">
+                        <ul>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="testimonial-pic">
+                    <img src="https://images.askmen.com/1080x540/2017/01/06-044621-the_pitfalls_of_dating_a_married_woman.jpg"
+                         alt="">
+                </div>
+            </div>
+
+            <div class="testimonial-pic-text">
+                <div class="testimonial-text text-right">
+                    <div class="testimonial-qouate">
+                        <p>Sed ullamcorper mi sit amet porta aliquam. Phasellus non molestie augu
+                            scelerisque, ultricies erat tempor, finibus Sed ullamcorper mi sit amet porta
+                            Phasellus non molestie augue. Donec id sapien.</p>
+                    </div>
+                    <div class="client-name-designation pull-right">
+                        <span class="client-name">MD.Saiful Islam</span>
+                        <span class="client-designation">Director/S.S Studio</span>
+                    </div>
+                    <div class="client-rate pt20 text-left ul-li">
+                        <ul>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                            <li><i class="fa fa-star"></i></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="testimonial-pic">
+                    <img src="https://images.askmen.com/1080x540/2017/01/06-044621-the_pitfalls_of_dating_a_married_woman.jpg"
+                         alt="">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+{!! BBstyle($_this->path.DS.'css'.DS.'owl.carousel.css') !!}
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}
+{!! BBscript($_this->path.DS.'js'.DS.'owl.carousel.min.js') !!}
+{!! BBscript($_this->path.DS.'js'.DS.'custom.js') !!}

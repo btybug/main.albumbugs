d17b3103095a39537bb1dc184c4d77b56ad763bc
sahakavatar <hakobyan.sahak88@gmail.com>
2018-06-11 14:01:58 +0400

    unit contact

---
+++ resources/units/unit_for_cv_abrar_contact/tpl.blade.php
@@ -0,0 +1,26 @@
+
+<section id="contact-abrar">
+    <div class="contact-map">
+        <iframe src="https://www.google.com/maps/embed?pb" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+    <div class="contact-address">
+        <div class="address-title">
+            <h3 class="address-title-head">Contact Information</h3>
+        </div>
+        <div class="address-list">
+            <ul class="address-list-item">
+                <li><i class="fa fa-location-arrow"></i>31, Street Lane, 4th floor, NY 23450</li>
+                <li><i class="fa fa-envelope"></i>info@example.com</li>
+                <li><i class="fa fa-phone"></i>+123 456 789</li>
+            </ul>
+        </div>
+
+        <div class="help-line text-center">
+            <div class="address-title">
+                <h3 class="address-title-head text-uppercase">NEED HELP</h3>
+                <span>+ 000 123 456</span>
+            </div>
+        </div>
+    </div>
+</section>
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}
---
+++ resources/units/unit_for_cv_abrar_resume/tpl.blade.php
@@ -0,0 +1,225 @@
+<section id="resume-abrar">
+        <div class="section-padding">
+            <div class="section-title text-capitalize text-center">
+                <h2 class="title-head">My Resume</h2>
+                <p>My Education &amp; Experience</p>
+            </div>
+
+            <div class="resume-tab-item">
+                <div class="tab-button active">
+                    <ul class="tabs text-center">
+                        <li class="active"><a data-toggle="tab" href="#tab1"><i class="fa fa-briefcase"></i> Experience</a></li>
+                        <li class="tab_last"><a data-toggle="tab" href="#tab2"><i class="fa fa-newspaper-o"></i> Education</a></li>
+                    </ul>
+                </div>
+
+                <div class="tab-content">
+                    <div id="tab1" class="tab_content tab-pane fade in active">
+                        <div class="education-resume">
+                            <div class="resume-institute-description">
+                                <div class="resume-institute">
+                                    <div class="institute-icon-time text-center">
+                                        <div class="institute-icon">
+                                            <i class="fa fa-briefcase"></i>
+                                        </div>
+                                        <div class="resume-time">
+                                            <span>January 2015 - Present</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="resume-description">
+                                    <div class="resume-degree">
+                                        <h3 class="degree-title">Front End Developer</h3>
+                                        <span class="degree-institute">XsTheme</span>
+                                    </div>
+                                    <div class="degree-text">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                            tempor incididunt ut labore et dolore Lorem ipsum dolor sit amet,
+                                            consectetur adipisicing elit, sed do eiusmod tempor.</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="resume-institute-description">
+                                <div class="resume-institute">
+                                    <div class="institute-icon-time text-center">
+                                        <div class="institute-icon">
+                                            <i class="fa fa-pencil"></i>
+                                        </div>
+                                        <div class="resume-time">
+                                            <span>August 2014 - Jan 2015</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="resume-description">
+                                    <div class="resume-degree">
+                                        <h3 class="degree-title">Designer</h3>
+                                        <span class="degree-institute">HTMLMATE</span>
+                                    </div>
+                                    <div class="degree-text">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                            tempor incididunt ut labore et dolore Lorem ipsum dolor sit amet,
+                                            consectetur adipisicing elit, sed do eiusmod tempor.</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="resume-institute-description">
+                                <div class="resume-institute">
+                                    <div class="institute-icon-time text-center">
+                                        <div class="institute-icon">
+                                            <i class="fa fa-newspaper-o"></i>
+                                        </div>
+                                        <div class="resume-time">
+                                            <span>March 2014 - July 2014</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="resume-description">
+                                    <div class="resume-degree">
+                                        <h3 class="degree-title">Creative Dot</h3>
+                                        <span class="degree-institute">Visual &amp; Motion Graphics</span>
+                                    </div>
+                                    <div class="degree-text">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                            tempor incididunt ut labore et dolore Lorem ipsum dolor sit amet,
+                                            consectetur adipisicing elit, sed do eiusmod tempor.</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="resume-institute-description">
+                                <div class="resume-institute">
+                                    <div class="institute-icon-time text-center">
+                                        <div class="institute-icon">
+                                            <i class="fa fa-pencil-square-o"></i>
+                                        </div>
+                                        <div class="resume-time">
+                                            <span>May 2013 - March 2014</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="resume-description">
+                                    <div class="resume-degree">
+                                        <h3 class="degree-title">Freelance</h3>
+                                        <span class="degree-institute">UI/UX Designer</span>
+                                    </div>
+
+                                    <div class="degree-text">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                            tempor incididunt ut labore et dolore Lorem ipsum dolor sit amet,
+                                            consectetur adipisicing elit, sed do eiusmod tempor.</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div id="tab2" class="tab_content tab-pane fade">
+                        <div class="education-resume">
+
+                            <div class="resume-institute-description">
+                                <div class="resume-institute">
+                                    <div class="institute-icon-time text-center">
+                                        <div class="institute-icon">
+                                            <i class="fa fa-hand-o-up"></i>
+                                        </div>
+                                        <div class="resume-time">
+                                            <span>January 2015 - Present</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="resume-description">
+                                    <div class="resume-degree">
+                                        <h3 class="degree-title">M.B.A (Accounting)</h3>
+                                        <span class="degree-institute">National University</span>
+                                    </div>
+
+                                    <div class="degree-text">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                            tempor incididunt ut labore et dolore Lorem ipsum dolor sit amet,
+                                            consectetur adipisicing elit, sed do eiusmod tempor.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="resume-institute-description">
+                                <div class="resume-institute">
+                                    <div class="institute-icon-time text-center">
+                                        <div class="institute-icon">
+                                            <i class="fa fa-book"></i>
+                                        </div>
+                                        <div class="resume-time">
+                                            <span>August 2014 - Jan 2015</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="resume-description">
+                                    <div class="resume-degree">
+                                        <h3 class="degree-title">B.B.A (Honor’s)</h3>
+                                        <span class="degree-institute">National University</span>
+                                    </div>
+                                    <div class="degree-text">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                            tempor incididunt ut labore et dolore Lorem ipsum dolor sit amet,
+                                            consectetur adipisicing elit, sed do eiusmod tempor.</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="resume-institute-description">
+                                <div class="resume-institute">
+                                    <div class="institute-icon-time text-center">
+                                        <div class="institute-icon">
+                                            <i class="fa fa-address-book"></i>
+                                        </div>
+                                        <div class="resume-time">
+                                            <span>March 2014 - July 2014</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="resume-description">
+                                    <div class="resume-degree">
+                                        <h3 class="degree-title">Creative Dot</h3>
+                                        <span class="degree-institute">Visual &amp; Motion Graphics</span>
+                                    </div>
+
+                                    <div class="degree-text">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                            tempor incididunt ut labore et dolore Lorem ipsum dolor sit amet,
+                                            consectetur adipisicing elit, sed do eiusmod tempor.</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="resume-institute-description">
+                                <div class="resume-institute">
+                                    <div class="institute-icon-time text-center">
+                                        <div class="institute-icon">
+                                            <i class="fa fa-pencil-square"></i>
+                                        </div>
+                                        <div class="resume-time">
+                                            <span>May 2013 - March 2014</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="resume-description">
+                                    <div class="resume-degree">
+                                        <h3 class="degree-title">Freelance</h3>
+                                        <span class="degree-institute">UI/UX Designer</span>
+                                    </div>
+                                    <div class="degree-text">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                            tempor incididunt ut labore et dolore Lorem ipsum dolor sit amet,
+                                            consectetur adipisicing elit, sed do eiusmod tempor.</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

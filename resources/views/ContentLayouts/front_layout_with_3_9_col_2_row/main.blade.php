0086b6e2a93526de92c2a74990671ca3ed8e931c
sahakavatar <hakobyan.sahak88@gmail.com>
2018-06-13 17:03:12 +0400

    layouts

--- resources/views/ContentLayouts/front_layout_with_3_9_col_2_row/main.blade.php
+++ resources/views/ContentLayouts/front_layout_with_3_9_col_2_row/main.blade.php
@@ -1,7 +1,38 @@
 <div class="container-fluid">
+
     <div class="row row-no-gutter {{(isset($settings['main_page_style'])&& $settings['main_page_style']) ? $settings['main_page_style'] : ''}}">
         <div class="top-menu-container {{(isset($settings['style'])&& $settings['style'] ) ? $settings['style'] : 'demo-column'}}">
             <div class="top-menu">
+                <nav class="navbar navbar-inverse">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+
+                        </div>
+                        <div class="collapse navbar-collapse" id="myNavbar">
+                            <ul class="nav navbar-nav">
+                                <li class="active"><a href="#">Home</a></li>
+                                <li class="dropdown">
+                                    <a href="#">Page 1</a>
+
+                                    <a data-toggle="dropdown" class="dropdown-toggle"><span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Page 1-1</a></li>
+                                        <li><a href="#">Page 1-2</a></li>
+                                        <li><a href="#">Page 1-3</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Page 2</a></li>
+                                <li><a href="#">Page 3</a></li>
+                            </ul>
+
+                        </div>
+                    </div>
+                </nav>
                 <ul class="profile-menu {{isset($settings['menu_area_style']) ? $settings['menu_area_style'] : ''}}">
                     @php
                         $items = isset($settings['menu_area']) ? BBGetMenu($settings['menu_area']) : [];

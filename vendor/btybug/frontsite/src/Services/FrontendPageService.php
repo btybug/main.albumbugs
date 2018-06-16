a73788a1e75337b8103d83cb758b7bec060a97b6
Eduard <edo.terakyan@gmail.com>
2018-06-16 20:45:43 +0400

    up

--- vendor/btybug/frontsite/src/Services/FrontendPageService.php
+++ vendor/btybug/frontsite/src/Services/FrontendPageService.php
@@ -29,7 +29,7 @@ class FrontendPageService extends GeneralService
         'blade.php',
     ];
 
-    public function __construct(
+    public function __construct (
         FrontPagesRepository $frontPagesRepository,
         AdminsettingRepository $settingsRepository,
         PermissionRoleRepository $permissionRoleRepository
@@ -40,14 +40,14 @@ class FrontendPageService extends GeneralService
         $this->permissionRoleRepository = $permissionRoleRepository;
     }
 
-    public static function checkAccess($page_id, $user)
+    public static function checkAccess ($page_id, $user)
     {
         $frontPages = new FrontPagesRepository();
         $page = $frontPages->find($page_id);
         $memberships = $user->memberships;
 
         if ($page && $memberships) {
-            if (!$page->memberships) return true;
+            if (! $page->memberships) return true;
 
             $checkMembership = array_diff($page->memberships, $memberships);
             if (count($checkMembership) == 0) return true;
@@ -56,7 +56,7 @@ class FrontendPageService extends GeneralService
         return false;
     }
 
-    public static function FrontPagesParentPermissionWithRole($page_id, $role_id)
+    public static function FrontPagesParentPermissionWithRole ($page_id, $role_id)
     {
         $page = new FrontPagesRepository();
         $result = $page->find($page_id);
@@ -65,7 +65,7 @@ class FrontendPageService extends GeneralService
 
     }
 
-    public static function register(array $data)
+    public static function register (array $data)
     {
         if (
             isset($data['module_id'])
@@ -91,26 +91,35 @@ class FrontendPageService extends GeneralService
             $frontPageRepo = new FrontPagesRepository();
 
             return $frontPageRepo->create([
-                'user_id' => \Auth::id(),
-                'module_id' => (isset($data['module_id'])) ? $data['module_id'] : null,
-                'title' => (isset($data['title'])) ? $data['title'] : "New Page",
-                'slug' => (isset($data['slug'])) ? $data['slug'] : uniqid(),
-                'header' => (isset($data['header'])) ? $data['header'] : 0,
-                'footer' => (isset($data['footer'])) ? $data['footer'] : 0,
-                'status' => (isset($data['status'])) ? $data['status'] : "published",
-                'page_access' => (isset($data['page_access'])) ? $data['page_access'] : 1,
-                'page_layout' => null,
+                'user_id'              => \Auth::id(),
+                'module_id'            => (isset($data['module_id'])) ? $data['module_id'] : null,
+                'title'                => (isset($data['title'])) ? $data['title'] : "New Page",
+                'slug'                 => (isset($data['slug'])) ? $data['slug'] : uniqid(),
+                'header'               => (isset($data['header'])) ? $data['header'] : 0,
+                'footer'               => (isset($data['footer'])) ? $data['footer'] : 0,
+                'status'               => (isset($data['status'])) ? $data['status'] : "published",
+                'page_access'          => (isset($data['page_access'])) ? $data['page_access'] : 1,
+                'page_layout'          => null,
                 'page_layout_settings' => null,
-                'url' => (isset($data['prefix'])) ? $data['prefix'] . '/' . $data['url'] : $data['url'],
-                'parent_id' => (isset($data['parent_id'])) ? $data['parent_id'] : null,
-                'type' => 'plugin',
-                'content_type' => (isset($data['content_type'])) ? $data['content_type'] : 'editor',
-                'settings' => (isset($data['settings'])) ? json_encode($data['settings'], true) : null
+                'url'                  => (isset($data['prefix'])) ? $data['prefix'] . '/' . $data['url'] : $data['url'],
+                'parent_id'            => (isset($data['parent_id'])) ? $data['parent_id'] : null,
+                'type'                 => 'plugin',
+                'content_type'         => (isset($data['content_type'])) ? $data['content_type'] : 'editor',
+                'settings'             => (isset($data['settings'])) ? json_encode($data['settings'], true) : null
             ]);
         }
     }
 
-    public function saveSettings(
+    public static function getFirstParent ($page)
+    {
+        if ($page) {
+            return \DB::select('SELECT T2.* FROM (SELECT @r AS _id,(SELECT @r := parent_id FROM frontend_pages WHERE id = _id) AS parent_id, @l := @l + 1 AS lvl FROM (SELECT @r := ' . $page->id . ', @l := 0) vars, frontend_pages m WHERE @r <> 0) T1 JOIN frontend_pages T2 ON T1._id = T2.id ORDER BY T1.lvl DESC;');
+        }
+
+        return [];
+    }
+
+    public function saveSettings (
         Request $request
     )
     {
@@ -144,22 +153,23 @@ class FrontendPageService extends GeneralService
                 ]);
             }
         }
+
 //        $this->savePageMainContent($page);
         return $page;
     }
 
-    public function savePageMainContent(FrontendPage $page)
+    public function savePageMainContent (FrontendPage $page)
     {
         if ($page->content_type == 'template' && $page->page_layout) {
             $variation = ContentLayouts::findVariation($page->page_layout);
-            if($variation){
-                $variation->main_unit=$page->template;
+            if ($variation) {
+                $variation->main_unit = $page->template;
                 $variation->save();
             }
         }
     }
 
-    public function saveSpecialSettings(
+    public function saveSpecialSettings (
         Request $request
     )
     {
@@ -191,29 +201,29 @@ class FrontendPageService extends GeneralService
         return $page;
     }
 
-    public function saveGeneralSettings(
+    public function saveGeneralSettings (
         Request $request
     )
     {
         $page = $this->frontPagesRepository->findOrFail($request->id);
         $this->frontPagesRepository->update($page->id, [
-            'title' => $request->get('title'),
-            'url' => $request->get('url'),
-            'status' => $request->get('status'),
-            'page_access' => $request->get('page_access'),
-            'memberships' => $request->get('memberships', null),
+            'title'          => $request->get('title'),
+            'url'            => $request->get('url'),
+            'status'         => $request->get('status'),
+            'page_access'    => $request->get('page_access'),
+            'memberships'    => $request->get('memberships', null),
             'special_access' => $request->get('special_access', null)
         ]);
 
         return $page;
     }
 
-    public function addNewPage(int $parentID = null, $type = 'custom')
+    public function addNewPage (int $parentID = null, $type = 'custom')
     {
         $parent = null;
         if ($parentID) {
             $parent = $this->frontPagesRepository->find($parentID);
-            if (!$parent) {
+            if (! $parent) {
                 return false;
             }
         }
@@ -223,15 +233,15 @@ class FrontendPageService extends GeneralService
 //        $defaultPageLayoutSettings = $this->settingsRepository->findBy('settingkey', 'placeholders');
         $slug = uniqid();
         $new = $this->frontPagesRepository->create([
-            'user_id' => \Auth::id(),
-            'title' => 'New Page',
-            'slug' => $slug,
-            'header' => ($header_enabled) ? $header_enabled->val : 0,
-            'footer' => ($footer_enabled) ? $footer_enabled->val : 0,
-            'url' => '',
+            'user_id'     => \Auth::id(),
+            'title'       => 'New Page',
+            'slug'        => $slug,
+            'header'      => ($header_enabled) ? $header_enabled->val : 0,
+            'footer'      => ($footer_enabled) ? $footer_enabled->val : 0,
+            'url'         => '',
             'page_access' => ZERO,
-            'parent_id' => ($parent) ? $parent->id : null,
-            'type' => $type
+            'parent_id'   => ($parent) ? $parent->id : null,
+            'type'        => $type
         ]);
         $this->frontPagesRepository->update($new->id, [
             'url' => '/new-page(' . $new->id . ')',
@@ -240,14 +250,14 @@ class FrontendPageService extends GeneralService
         return $new;
     }
 
-    public function getPlaceholdersInUrl($pageLayoutSettings = [])
+    public function getPlaceholdersInUrl ($pageLayoutSettings = [])
     {
         if ($pageLayoutSettings) {
             return http_build_query($pageLayoutSettings);
         }
     }
 
-    public function sort(array $data)
+    public function sort (array $data)
     {
         $child = $this->frontPagesRepository->find($data['item']);
 
@@ -261,14 +271,5 @@ class FrontendPageService extends GeneralService
         }
 
         return false;
-    }
-
-    public static function getFirstParent($page)
-    {
-        if ($page) {
-            return \DB::select('SELECT T2.* FROM (SELECT @r AS _id,(SELECT @r := parent_id FROM frontend_pages WHERE id = _id) AS parent_id, @l := @l + 1 AS lvl FROM (SELECT @r := ' . $page->id . ', @l := 0) vars, frontend_pages m WHERE @r <> 0) T1 JOIN frontend_pages T2 ON T1._id = T2.id ORDER BY T1.lvl DESC;');
-        }
-
-        return [];
     }
 }

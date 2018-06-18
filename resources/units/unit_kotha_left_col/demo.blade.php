a8846883693f6ff6a4b063a53b19bf0f1ab55e43
sahakavatar <hakobyan.sahak88@gmail.com>
2018-06-18 17:04:34 +0400

    unit left

---
+++ resources/units/unit_kotha_left_col/demo.blade.php
@@ -0,0 +1,247 @@
+<div class="col-md-4">
+    <aside class="sidebar-left-col">
+        <div class="about-me-widget">
+            <div class="bg-img">
+                <img src="https://res.cloudinary.com/sagacity/image/upload/c_crop,h_1068,w_1600,x_0,y_438/c_limit,dpr_1.0,f_auto,fl_lossy,q_80,w_1100/0615_COVER_MNTNS_KICH000884_cmyk_iqrrxg_xkeo8p.jpg"
+                     alt="">
+            </div>
+            <div class="widget text-center">
+
+                <div class="about-me-content">
+                    <div class="about-me-img">
+                        <img src="https://jooinn.com/images/girl-162.jpg" alt="" class="img-me img-circle">
+                        <h2 class="text-uppercase">Kotha Smith</h2>
+                        <p>Kotha Smith is an enthusiastic and passionate Story Teller. He loves to do different
+                            home-made things
+                            and share to the world.</p>
+                    </div>
+                </div>
+                <div class="social-share">
+                    <ul class="list-inline">
+                        <li><a class="s-facebook" href=""><i class="fa fa-facebook"></i></a></li>
+                        <li><a class="s-twitter" href=""><i class="fa fa-twitter"></i></a></li>
+                        <li><a class="s-google-plus" href=""><i class="fa fa-google-plus"></i></a></li>
+                        <li><a class="s-linkedin" href=""><i class="fa fa-linkedin"></i></a></li>
+                        <li><a class="s-instagram" href=""><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="widget news-letter-widget">
+            <h2 class="widget-title text-uppercase text-center">Get Newsletter</h2>
+            <form action="#">
+                <input type="email" placeholder="Your email address" required="">
+                <input type="submit" value="Subscribe Now" class="text-uppercase text-center btn btn-subscribe">
+            </form>
+        </div>
+        <div class="widget widget-popular-post">
+            <h3 class="widget-title text-uppercase text-center">Popular Posts</h3>
+            <ul>
+                <li>
+                    <a href="#" class="popular-img"><img src="https://s.hswstatic.com/gif/animal-stereotype-orig.jpg"
+                                                         alt="">
+                    </a>
+                    <div class="p-content">
+                        <h4><a href="#" class="text-uppercase">A Mindblowing Interior Design</a></h4>
+                        <span class="p-date">February 15, 2018</span>
+                    </div>
+                </li>
+                <li><a href="#" class="popular-img"><img
+                                src="https://d1u4oo4rb13yy8.cloudfront.net/article/68223-ynavetrsmr-1505204516.jpg"
+                                alt="">
+                    </a>
+                    <div class="p-content">
+                        <h4><a href="#" class="text-uppercase">A Table with amazing books</a></h4>
+                        <span class="p-date">February 15, 2018</span>
+                    </div>
+                </li>
+                <li>
+                    <a href="#" class="popular-img"><img
+                                src="https://www.nationalgeographic.com/content/dam/photography/rights-exempt/best-of-photo-of-the-day/2017/animals/01_pod-best-animals.jpg"
+                                alt="">
+                    </a>
+                    <div class="p-content">
+                        <h4><a href="#" class="text-uppercase">Home is peaceful Place</a></h4>
+                        <span class="p-date">February 15, 2018</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="widget latest-post-widget">
+            <h2 class="widget-title text-uppercase text-center">Latest Posts</h2>
+            <ul>
+                <li class="media">
+                    <div class="row">
+                        <div class="col-sm-4 col-xs-12">
+                            <div class="media-left">
+                                <a href="#" class="popular-img"><img
+                                            src="https://secure.i.telegraph.co.uk/multimedia/archive/03379/animals-smile_3379238k.jpg"
+                                            alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-sm-8 col-xs-12">
+                            <div class="latest-post-content">
+                                <h2 class="text-uppercase"><a href="#">A Marine Dive From Sky</a></h2>
+                                <p>May 27, 2018</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </li>
+                <li class="media">
+                    <div class="row">
+                        <div class="col-sm-4 col-xs-12">
+                            <div class="media-left">
+                                <a href="#" class="popular-img"><img
+                                            src="https://www.sciencenews.org/sites/default/files/2018/03/main/articles/041418_reviews_animals_main.jpg"
+                                            alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-sm-8 col-xs-12">
+                            <div class="latest-post-content">
+                                <h2 class="text-uppercase"><a href="#">Small Meeting Room</a></h2>
+                                <p>April 27, 2017</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </li>
+                <li class="media">
+                    <div class="row">
+                        <div class="col-sm-4 col-xs-12">
+                            <div class="media-left">
+                                <a href="#" class="popular-img"><img
+                                            src="https://www.leesburganimalpark.com/wp-content/uploads/2016/10/slider-lemur-1140x499_c.jpg"
+                                            alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-sm-8 col-xs-12">
+                            <div class="latest-post-content">
+                                <h2 class="text-uppercase"><a href="#">Selfie from the beach point</a></h2>
+                                <p>April 20, 2017</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </li>
+                <li class="media">
+                    <div class="row">
+                        <div class="col-sm-4 col-xs-12">
+                            <div class="media-left">
+                                <a href="#" class="popular-img"><img
+                                            src="http://animals.sandiegozoo.org/sites/default/files/2016-09/animals_hero_monkey.jpg"
+                                            alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-sm-8 col-xs-12">
+                            <div class="latest-post-content">
+                                <h2 class="text-uppercase"><a href="#">Gather Some Artist Pencils.</a></h2>
+                                <p>May 27, 2017</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </li>
+            </ul>
+        </div>
+        <div class="widget insta-widget">
+            <h2 class="widget-title text-uppercase text-center">INSTAGRAM FEED</h2>
+            <div class="instagram-feed">
+                <div class="single-instagram">
+                    <a href="">
+                        <img src="https://static.boredpanda.com/blog/wp-content/uploads/2016/02/adorable-little-animals-that-i-make-from-wool__880.jpg" alt="">
+                    </a>
+                    <div class="insta-overlay">
+                        <div class="insta-meta">
+                            <ul class="list-inline text-center">
+                                <li><a href=""><i class="fa fa-heart-o"></i></a> 325</li>
+                                <li><a href=""><i class="fa fa-comment-o"></i></a> 20</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <a href="" class="insta-link"></a>
+                </div>
+                <div class="single-instagram">
+                    <a href="">
+                        <img src="https://i.pinimg.com/originals/6d/2f/14/6d2f140c61ac6b28b72a3c14caf1e4f4.jpg" alt="">
+                    </a>
+                    <div class="insta-overlay">
+                        <div class="insta-meta">
+                            <ul class="list-inline text-center">
+                                <li><a href=""><i class="fa fa-heart-o"></i></a> 325</li>
+                                <li><a href=""><i class="fa fa-comment-o"></i></a> 20</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <a href="" class="insta-link"></a>
+                </div>
+                <div class="single-instagram">
+                    <a href="">
+                        <img src="http://1.bp.blogspot.com/-mY4B-gt2dhQ/UeXGGiDOEiI/AAAAAAAACFg/mBleW2a004Y/s640/animales+tiernos+(1).jpg" alt="">
+                    </a>
+                    <div class="insta-overlay">
+                        <div class="insta-meta">
+                            <ul class="list-inline text-center">
+                                <li><a href=""><i class="fa fa-heart-o"></i></a> 325</li>
+                                <li><a href=""><i class="fa fa-comment-o"></i></a> 20</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <a href="" class="insta-link"></a>
+                </div>
+                <div class="single-instagram">
+                    <a href="">
+                        <img src="http://cdn.akc.org/content/article-body-image/cwc.jpg" alt="">
+                    </a>
+                    <div class="insta-overlay">
+                        <div class="insta-meta">
+                            <ul class="list-inline text-center">
+                                <li><a href=""><i class="fa fa-heart-o"></i></a> 325</li>
+                                <li><a href=""><i class="fa fa-comment-o"></i></a> 20</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <a href="" class="insta-link"></a>
+                </div>
+                <div class="single-instagram">
+                    <a href="">
+                        <img src="http://www.cutestpaw.com/wp-content/uploads/2013/08/Mother-dog-sitting-with-her-cute-puppies.jpg" alt="">
+                    </a>
+                    <div class="insta-overlay">
+                        <div class="insta-meta">
+                            <ul class="list-inline text-center">
+                                <li><a href=""><i class="fa fa-heart-o"></i></a> 325</li>
+                                <li><a href=""><i class="fa fa-comment-o"></i></a> 20</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <a href="" class="insta-link"></a>
+                </div>
+                <div class="single-instagram">
+                    <a href="">
+                        <img src="https://i.pinimg.com/originals/b5/5c/3d/b55c3d2cc20732509a53ab6a266c4c34.jpg" alt="">
+                    </a>
+                    <div class="insta-overlay">
+                        <div class="insta-meta">
+                            <ul class="list-inline text-center">
+                                <li><a href=""><i class="fa fa-heart-o"></i></a> 325</li>
+                                <li><a href=""><i class="fa fa-comment-o"></i></a> 20</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <a href="" class="insta-link"></a>
+                </div>
+            </div>
+        </div>
+    </aside>
+</div>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

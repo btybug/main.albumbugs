2734abed2a90856a0d5d7308f230b0f9137f66c2
sahakavatar <hakobyan.sahak88@gmail.com>
2018-06-19 09:46:16 +0400

    unit

---
+++ resources/units/unit_kotha_right_col_main/tpl.blade.php
@@ -0,0 +1,164 @@
+    <div class="more-blog">
+        <article class="single-blog">
+            <div class="post-thumb">
+                <a href=""><img src="http://listtop10s.com/wp-content/uploads/2013/12/Top-10-Most-Dangerous-Animals-Hippopotamus.jpg" alt=""></a>
+            </div>
+            <div class="post-content">
+                <div class="entry-header text-center text-uppercase">
+                    <a href="" class="post-cat">Travel</a>
+                    <h2><a href="#">Adventure to travel lonely</a></h2>
+                </div>
+                <div class="entry-content">
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+                        accusam
+                        et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+                        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                        diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+                        voluptua...</p>
+                </div>
+                <div class="continue-reading text-center text-uppercase">
+                    <a href="#">Continue Reading</a>
+                </div>
+                <div class="post-meta">
+                    <ul class="pull-left list-inline author-meta">
+                        <li class="author">By <a href="#">Jennifer </a></li>
+                        <li class="date"> On October 13, 2018</li>
+                    </ul>
+                    <ul class="pull-right list-inline social-share">
+                        <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                        <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                        <li><a href=""><i class="fa fa-pinterest"></i></a></li>
+                        <li><a href=""><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </article>
+        <article class="single-blog">
+            <div class="post-thumb">
+                <a href=""><img src="https://cosmos-images2.imgix.net/file/spina/photo/14772/GettyImages-691120979.jpg" alt=""></a>
+            </div>
+            <div class="post-content">
+                <div class="entry-header text-center text-uppercase">
+                    <a href="" class="post-cat">Travel</a>
+                    <h2><a href="#">Playing with kite at beach</a></h2>
+                </div>
+                <div class="entry-content">
+                    <p>Sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, Lorem
+                        ipsum dolor sit
+                        amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+                        dolore magna
+                        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+                        rebum. Stet clita
+                        kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. sed diam
+                        voluptua Lorem ipsum
+                        dolor sit amet, consetetur sadipscing elitr...</p>
+                </div>
+                <div class="continue-reading text-center text-uppercase">
+                    <a href="#">Continue Reading</a>
+                </div>
+                <div class="post-meta">
+                    <ul class="pull-left list-inline author-meta">
+                        <li class="author">By <a href="#">Jennifer </a></li>
+                        <li class="date"> On September 12, 2017</li>
+                    </ul>
+                    <ul class="pull-right list-inline social-share">
+                        <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                        <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                        <li><a href=""><i class="fa fa-pinterest"></i></a></li>
+                        <li><a href=""><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </article>
+        <article class="single-blog">
+            <div class="post-thumb">
+                <a href="#"><img src="http://www.abc.net.au/news/image/7477266-3x2-940x627.jpg" alt=""></a>
+            </div>
+            <div class="post-content">
+                <div class="entry-header text-center text-uppercase">
+                    <a href="" class="post-cat">Travel</a>
+                    <h2><a href="#">At vero eos etaccusam
+                            et justo duo </a></h2>
+                </div>
+                <div class="entry-content">
+                    <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Sed
+                        diam nonumy eirmod
+                        tempor invidunt ut labore et dolore magna aliquyam erat, Lorem ipsum dolor sit amet,
+                        consetetur
+                        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                        aliquyam erat, sed
+                        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. sed diam
+                        voluptua Lorem ipsum
+                        dolor sit sadipscing elitr amet, consetetur ...</p>
+                </div>
+                <div class="continue-reading text-center text-uppercase">
+                    <a href="#">Continue Reading</a>
+                </div>
+                <div class="post-meta">
+                    <ul class="pull-left list-inline author-meta">
+                        <li class="author">By <a href="#">Jennifer </a></li>
+                        <li class="date"> On December 29, 2017</li>
+                    </ul>
+                    <ul class="pull-right list-inline social-share">
+                        <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                        <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                        <li><a href=""><i class="fa fa-pinterest"></i></a></li>
+                        <li><a href=""><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </article>
+        <article class="single-blog">
+            <div class="post-thumb">
+                <a href="#"><img src="https://invisibledisabilities.org/wp-content/uploads/2018/03/Fotolia_177884381_Subscription_Monthly_M.jpg" alt=""></a>
+            </div>
+            <div class="post-content">
+                <div class="entry-header text-center text-uppercase">
+                    <a href="" class="post-cat">Travel</a>
+                    <h2><a href="#">An imaginary for travelers</a></h2>
+                </div>
+                <div class="entry-content">
+                    <p>Magna aliquyam erat, Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+                        dolor sit amet.
+                        Sed diam nonumy eirmod tempor invidunt ut labore et dolore. Lorem ipsum dolor sit amet,
+                        consetetur
+                        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                        aliquyam erat, sed
+                        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. sed diam
+                        voluptua Lorem ipsum
+                        dolor sit sadipscing elitr amet, consetetur ...</p>
+                </div>
+                <div class="continue-reading text-center text-uppercase">
+                    <a href="#">Continue Reading</a>
+                </div>
+                <div class="post-meta">
+                    <ul class="pull-left list-inline author-meta">
+                        <li class="author">By <a href="#">Jennifer </a></li>
+                        <li class="date"> On June 24, 2017</li>
+                    </ul>
+                    <ul class="pull-right list-inline social-share">
+                        <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                        <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                        <li><a href=""><i class="fa fa-pinterest"></i></a></li>
+                        <li><a href=""><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </article>
+        <div class="post-pagination clearfix">
+            <ul class="pagination text-left">
+                <li class="active"><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#"><i class="fa fa-angle-double-right"></i></a></li>
+            </ul>
+        </div>
+    </div>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}
---
+++ resources/units/unit_kotha_right_col_top/demo.blade.php
@@ -0,0 +1,7 @@
+<div class="col-md-8">
+    <div class="top-main-col">
+        <img src="http://www.tradexl.com/images/banner-ads.jpg" alt="">
+    </div>
+</div>
+
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css') !!}

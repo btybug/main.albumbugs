f364fe8012212b8a77820f68c452daf4debb75b4
sahakavatar <hakobyan.sahak88@gmail.com>
2018-06-20 15:25:22 +0400

    fix

---
+++ resources/units/unit_felicia_home/tpl.blade.php
@@ -0,0 +1,320 @@
+<section class="section-felic-about">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title">
+                    <h2 class="title text-uppercase separator"> About Onepage</h2>
+                    <div class="sub-title">
+                        We’re a creative agency located in New York.<br>
+                        we're very passionate about our work and love to create awesome products.
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+                <div class="services-list text-left">
+                    <div class="services-icon text-primary">
+                        <i class="fa fa-newspaper-o" aria-hidden="true"></i>
+                    </div>
+                    <div class="services-desk">
+                        <h4 class="services-title">
+                            Web Solution
+                        </h4>
+                        <p class="services-info">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="services-list text-left">
+                    <div class="services-icon text-danger">
+                        <i class="fa fa-credit-card" aria-hidden="true"></i>
+                    </div>
+                    <div class="services-desk">
+                        <h4 class="services-title">
+                            Corporate Branding
+                        </h4>
+                        <p class="services-info">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="services-list text-left">
+                    <div class="services-icon text-success">
+                        <i class="fa fa-globe" aria-hidden="true"></i>
+                    </div>
+                    <div class="services-desk">
+                        <h4 class="services-title">
+                            Web Security
+                        </h4>
+                        <p class="services-info">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="services-list text-left">
+                    <div class="services-icon text-warning">
+                        <i class="fa fa-tablet" aria-hidden="true"></i>
+                    </div>
+                    <div class="services-desk">
+                        <h4 class="services-title">
+                            App Development
+                        </h4>
+                        <p class="services-info">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="services-list text-left">
+                    <div class="services-icon text-info">
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                    </div>
+                    <div class="services-desk">
+                        <h4 class="services-title">
+                            Search Engine Optimaization
+                        </h4>
+                        <p class="services-info">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="services-list text-left">
+                    <div class="services-icon">
+                        <i class="fa fa-server" aria-hidden="true"></i>
+                    </div>
+                    <div class="services-desk">
+                        <h4 class="services-title">
+                            Data Analysis
+                        </h4>
+                        <p class="services-info">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<section class="section-felic-works">
+    <div class="section-title text-center">
+        <h2 class="title separator">Our Works</h2>
+        <div class="sub-title">Lorem Ipsum is simply dummy text of the printing.</div>
+    </div>
+    <!--portfolio-->
+    <div class="portfolio-box-">
+
+        <div class="text-center">
+            <ul class="portfolio-filter">
+                <li class="active"><a href="#" data-filter="*"> All</a></li>
+                <li class=""><a href="#" data-filter=".cat1">printing</a></li>
+                <li class=""><a href="#" data-filter=".cat2">Web</a></li>
+                <li class=""><a href="#" data-filter=".cat3">illustration</a></li>
+                <li class=""><a href="#" data-filter=".cat4">media</a></li>
+                <li class=""><a href="#" data-filter=".cat5">crafts</a></li>
+            </ul>
+        </div>
+        <div class="clearfix"></div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="portfolio ">
+
+
+                    <div class="col-md-3 col-sm-6 col-xs-12 cat1 cat3">
+                        <div class="portfolio-item">
+                            <a href="#" class="thumb" title="We are creative">
+                                <img src="https://i0.wp.com/www.planwallpaper.com/static/images/4438518-summer-wallpapers.jpg?resize=1060%2C596&ssl=1"
+                                     alt="">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-description">
+                                        <h4>even view</h4>
+                                        <p>Fit Woman Stretching Her Body in Morning Yoga Routine</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6 col-xs-12 cat2 cat4">
+                        <div class="portfolio-item">
+                            <a href="#" class="thumb" title="We are creative">
+                                <img src="http://gipsypixel.com/wp-content/uploads/2017/11/Nice-Summer-For-Everybody-Who-Loves-The-Backgrounds-Widescreen-Wallpaper-Of-Laptop-High-Quality.jpg"
+                                     alt="">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-description">
+                                        <h4>Even grids</h4>
+                                        <p>Fit Woman Stretching Her Body in Morning Yoga Routine</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6 col-xs-12 cat1 cat5">
+                        <div class="portfolio-item">
+                            <a href="#" class="thumb" title="We are creative">
+                                <img src="http://img1.indafoto.hu/2/7/70367_5acc291fb65d349ece286e28dd63d580/14922995_daa6fb3827af480c3a693b601ef513b3_l.jpg"
+                                     alt="">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-description">
+                                        <h4>Minimal design</h4>
+                                        <p>Fit Woman Stretching Her Body in Morning Yoga Routine</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6 col-xs-12 cat3">
+                        <div class="portfolio-item">
+                            <a href="#" class="thumb" title="We are creative">
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Zone_portuaire_calais_phare.jpg"
+                                     alt="">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-description">
+                                        <h4>Creative Looks</h4>
+                                        <p>Fit Woman Stretching Her Body in Morning Yoga Routine</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6 col-xs-12 cat1 cat2">
+                        <div class="portfolio-item">
+                            <a href="#" class="thumb" title="We are creative">
+                                <img src="https://moltopiccolo.files.wordpress.com/2012/01/cool-drinks.jpg" alt="">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-description">
+                                        <h4>Fully Responsive</h4>
+                                        <p>Fit Woman Stretching Her Body in Morning Yoga Routine</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6 col-xs-12 cat3 cat4">
+                        <div class="portfolio-item">
+                            <a href="#" class="thumb" title="We are creative">
+                                <img src="https://www.travelfashiongirl.com/wp-content/uploads/2017/07/how-to-pack-for-nice-france.jpg"
+                                     alt="">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-description">
+                                        <h4>Clean Code</h4>
+                                        <p>Fit Woman Stretching Her Body in Morning Yoga Routine</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6 col-xs-12 cat1 cat5">
+                        <div class="portfolio-item">
+                            <a href="#" class="thumb" title="We are creative">
+                                <img src="https://c1.staticflickr.com/9/8415/29516920583_213db8d232_b.jpg" alt="">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-description">
+                                        <h4>Bootstrap 3+</h4>
+                                        <p>Fit Woman Stretching Her Body in Morning Yoga Routine</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6 col-xs-12 cat1 cat4">
+                        <div class="portfolio-item">
+                            <a href="#" class="thumb" title="We are creative">
+                                <img src="http://www.bakkerij-wouters.be/wp-content/uploads/2016/06/wallpaper-desktop-fantasy-summer-backgrounds-150920.jpg"
+                                     alt="">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-description">
+                                        <h4>Easy to customize</h4>
+                                        <p>Fit Woman Stretching Her Body in Morning Yoga Routine</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+    <!--portfolio-->
+</section>
+
+<section class="section-felic-testimonial">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="section-title text-center">
+                    <h2 class="title separator">TESTIMONIAL</h2>
+                    <div class="sub-title">Lorem Ipsum is simply dummy text of the printing and typesetting industry
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div id="testimonial-list" class="owl-carousel owl-theme text-center testimonial-wrapper">
+                    <div class="item">
+                        <div class="text-center">
+                            <div class="thumb">
+                                <img src="http://www.parnassusbooks.net/sites/parnassusbooks.net/files/unnamed_1.jpg" alt="...">
+                            </div>
+                            <p class="testimonial-text">Lorem Ipsum is simply dummy text of the printing and
+                                typesetting industry. Lorem Ipsum has been the industry's standard dummy
+                                text ever since the 1500s</p>
+                        </div>
+                        <div class="testimonial-author">
+                            <span class="text-uppercase">Rose Merry</span>
+                            <small>CMO - SDF Company</small>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <div class="text-center">
+                            <div class="thumb">
+                                <img src="https://c1.staticflickr.com/8/7228/7073088833_95fdc29e21_m.jpg" alt="...">
+                            </div>
+                            <p class="testimonial-text">Lorem Ipsum is simply dummy text of the printing and
+                                typesetting industry. Lorem Ipsum has been the industry's standard dummy
+                                text ever since the 1500s</p>
+                        </div>
+                        <div class="testimonial-author">
+                            <span class="text-uppercase">Rose Merry</span>
+                            <small>CMO - SDF Company</small>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <div class="text-center">
+                            <div class="thumb">
+                                <img src="https://www.contemporarynomad.com/wp-content/uploads/2009/09/kinnauri-woman-1-240x240.jpg" alt="...">
+                            </div>
+                            <p class="testimonial-text">Lorem Ipsum is simply dummy text of the printing and
+                                typesetting industry. Lorem Ipsum has been the industry's standard dummy
+                                text ever since the 1500s</p>
+                        </div>
+                        <div class="testimonial-author">
+                            <span class="text-uppercase">Rose Merry</span>
+                            <small>CMO - SDF Company</small>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+{!! BBstyle($_this->path.DS.'css'.DS.'owl.carousel.min.css',$_this) !!}
+{!! BBstyle($_this->path.DS.'css'.DS.'owl.theme.default.css',$_this) !!}
+{!! BBstyle($_this->path.DS.'css'.DS.'style.css',$_this) !!}
+{!! BBscript($_this->path.DS.'js'.DS.'jquery.isotope.js',$_this) !!}
+{!! BBscript($_this->path.DS.'js'.DS.'owl.carousel.min.js',$_this) !!}
+{!! BBscript($_this->path.DS.'js'.DS.'custom.js',$_this) !!}

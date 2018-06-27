2343466bae7ee3d8941abc4c9684667cccc3e103
Sahak <hakobyan.sahak88@gmail.com>
2018-06-27 10:21:34 +0400

    media

--- vendor/btybug/media/src/Resources/Views/drive/galery.blade.php
+++ vendor/btybug/media/src/Resources/Views/drive/galery.blade.php
@@ -1,11 +1,13 @@
 <div style='height: 60px;display: flex;align-items: center;'><span
             style='width: 60px;height: 100%;display: flex;border: 4px solid #fff;'><img
                 style='width: 100%;height: 100%;object-fit: cover;'
+                class="tmp_{!!$name.'-'.$a !!}"
                 src='http://www.tourisme.fr/images/otf_offices/857/172313-203553606325436-5429368-o.jpg' alt=''></span>
     <button type='button'
             style='background-color: rgb(101, 101, 101); width: 210px;height: 100%;outline: none;border: none;'
             class='btnsettingsModal  media-modal-open' data-id='{!! $name.'-'.$a !!}'><img
                 style='width: 100%;height: 100%;object-fit: contain;'
+
                 src='http://dpskurukshetra.com/wp-content/uploads/2018/01/Camara-GALLERY-Icon.png' alt=''></button>
 </div>
 <input type='hidden' {!! $value !!} id='{!! $name.'-'.$a!!}' name='{!!$name!!}'>
